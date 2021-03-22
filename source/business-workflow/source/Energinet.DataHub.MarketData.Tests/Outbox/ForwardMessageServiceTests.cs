b4f0bb09632d0a4aa9303a957bbd58caf4c65607
René Nielsen <renetdev@gmail.com>
2021-03-22 20:37:52 +0100

    migrated solution from green-energy-hub

--- source/business-workflow/source/Energinet.DataHub.MarketData.Tests/Outbox/ForwardMessageServiceTests.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketData.Tests/Outbox/ForwardMessageServiceTests.cs
@@ -13,6 +13,9 @@
 // limitations under the License.
 
 using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Azure.Messaging.ServiceBus;
 using Energinet.DataHub.MarketData.Infrastructure.Outbox;
@@ -25,6 +28,7 @@ using Xunit.Sdk;
 
 namespace Energinet.DataHub.MarketData.Tests.Outbox
 {
+    [Trait("Category", "Unit")]
     [Trait("Category", "Unit")]
     public class ForwardMessageServiceTests
     {
