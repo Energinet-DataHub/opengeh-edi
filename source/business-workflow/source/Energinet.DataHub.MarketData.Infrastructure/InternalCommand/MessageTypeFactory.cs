725dc8408d1f720f28c5fb75484be80cb1d6cccd
Daniel <dph@energinet.dk>
2021-03-29 09:45:29 +0200

    Factory update

--- source/business-workflow/source/Energinet.DataHub.MarketData.Infrastructure/InternalCommand/MessageTypeFactory.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketData.Infrastructure/InternalCommand/MessageTypeFactory.cs
@@ -5,13 +5,13 @@ namespace Energinet.DataHub.MarketData.Infrastructure.InternalCommand
 {
     public class MessageTypeFactory
     {
-        public Type GetType(string type)
+        public Type? GetType(string type)
         {
             switch (type)
             {
                 case nameof(RequestChangeOfSupplier):
                     return typeof(RequestChangeOfSupplier);
-                case nameof(Master)
+                default: return null;
             }
         }
     }
