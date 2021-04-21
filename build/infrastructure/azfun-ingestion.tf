c97f165dd97d85b8a6f4571c916462d73f4ed96f
Christopher Maigaard <xchma@energinet.dk>
2021-04-21 09:12:34 +0200

    add dotnet 5 functions

--- build/infrastructure/azfun-ingestion.tf
+++ build/infrastructure/azfun-ingestion.tf
@@ -11,14 +11,14 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "azfun_asynchronousingestor" {
+module "azfun_ingestion" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//function-app?ref=1.2.0"
-  name                                      = "azfun-asynchronousingestor-${var.project}-${var.organisation}-${var.environment}"
+  name                                      = "azfun-ingestion-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name                       = data.azurerm_resource_group.main.name
   location                                  = data.azurerm_resource_group.main.location
-  storage_account_access_key                = module.azfun_asynchronousingestor_stor.primary_access_key
-  storage_account_name                      = module.azfun_asynchronousingestor_stor.name
-  app_service_plan_id                       = module.azfun_asynchronousingestor_plan.id
+  storage_account_access_key                = module.azfun_ingestion_stor.primary_access_key
+  storage_account_name                      = module.azfun_ingestion_stor.name
+  app_service_plan_id                       = module.azfun_ingestion_plan.id
   application_insights_instrumentation_key  = module.appi.instrumentation_key
   tags                                      = data.azurerm_resource_group.main.tags
   always_on                                 = true
@@ -27,45 +27,30 @@ module "azfun_asynchronousingestor" {
     WEBSITE_ENABLE_SYNC_UPDATE_SITE       = true
     WEBSITE_RUN_FROM_PACKAGE              = 1
     WEBSITES_ENABLE_APP_SERVICE_STORAGE   = true
-    FUNCTIONS_WORKER_RUNTIME              = "dotnet"
+    FUNCTIONS_WORKER_RUNTIME              = "dotnet-isolated"
     # Endregion: Default Values
-    KAFKA_SECURITY_PROTOCOL               = "SaslSsl"
-    KAFKA_SASL_MECHANISM                  = "Plain"
-    KAFKA_MESSAGE_SEND_MAX_RETRIES        = 5
-    KAFKA_MESSAGE_TIMEOUT_MS              = 1000
-    KAFKA_USERNAME                        = "$ConnectionString"
-    KAFKA_SSL_CA_LOCATION                 = "C:\\cacert\\cacert.pem"
-    MARKET_DATA_URL                       = "${module.sbn_marketroles.name}.servicebus.windows.net:9093"
+    # VALIDATION_REPORTS_QUEUE_TOPIC        = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_TOPIC.value
+    # VALIDATION_REPORTS_URL                = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_URL.value
+    # VALIDATION_REPORTS_CONNECTION_STRING  = data.azurerm_key_vault_secret.VALIDATION_REPORTS_CONNECTION_STRING.value
+    MARKET_DATA_QUEUE_URL                 = "${module.sbn_marketroles.name}.servicebus.windows.net:9093"
     MARKET_DATA_QUEUE_CONNECTION_STRING   = module.sbnar_marketroles_sender.primary_connection_string
-    MARKET_DATA_QUEUE_NAME                = module.sbq_marketroles.name
-    MARKET_DATA_DATABASE_CONNECTION_STRING= module.kvs_marketroles_db_connection_string.value
-    CHARGE_QUEUE_CONNECTION_STRING        = "TO_BE_REMOVED"
-    CHARGE_QUEUE_NAME                     = "TO_BE_REMOVED"
-    TIMESERIES_QUEUE_TOPIC                = "TO_BE_REMOVED"
-    TIMESERIES_QUEUE_URL                  = "TO_BE_REMOVED"
-    TIMESERIES_QUEUE_CONNECTION_STRING    = "TO_BE_REMOVED"
-    REQUEST_QUEUE_CONNECTION_STRING       = module.evhar_requestqueue_listener.primary_connection_string
-    REQUEST_QUEUE_NAME                    = module.evh_requestqueue.name
-    HUB_MRID                              = local.HUB_MRID
-    LOCAL_TIMEZONENAME                    = local.LOCAL_TIMEZONENAME
-    VALIDATION_REPORTS_QUEUE_TOPIC        = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_TOPIC.value
-    VALIDATION_REPORTS_URL                = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_URL.value
-    VALIDATION_REPORTS_CONNECTION_STRING  = data.azurerm_key_vault_secret.VALIDATION_REPORTS_CONNECTION_STRING.value
+    # MARKET_DATA_DB_CONNECTION_STRING      = module.kvs_marketroles_db_connection_string.value
+    MARKET_DATA_QUEUE_TOPIC_NAME          = module.sbq_marketroles.name
+    # POST_OFFICE_QUEUE_CONNECTION_STRING   = data.azurerm_key_vault_secret.POST_OFFICE_QUEUE_CONNECTION_STRING.value
+    # POST_OFFICE_QUEUE_TOPIC_NAME          = data.azurerm_key_vault_secret.POST_OFFICE_QUEUE_MARKETDATA_TOPIC_NAME.value
   }
   dependencies                              = [
     module.appi.dependent_on,
-    module.azfun_asynchronousingestor_plan.dependent_on,
-    module.azfun_asynchronousingestor_stor.dependent_on,
+    module.azfun_ingestion_plan.dependent_on,
+    module.azfun_ingestion_stor.dependent_on,
     module.sbnar_marketroles_sender.dependent_on,
     module.sbq_marketroles.dependent_on,
-    module.evhar_requestqueue_listener.dependent_on,
-    module.evh_requestqueue.dependent_on,
   ]
 }
 
-module "azfun_asynchronousingestor_plan" {
+module "azfun_ingestion_plan" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//app-service-plan?ref=1.2.0"
-  name                = "asp-asynchronousingestor-${var.project}-${var.organisation}-${var.environment}"
+  name                = "asp-ingestion-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
   kind                = "FunctionApp"
@@ -76,9 +61,9 @@ module "azfun_asynchronousingestor_plan" {
   tags                = data.azurerm_resource_group.main.tags
 }
 
-module "azfun_asynchronousingestor_stor" {
+module "azfun_ingestion_stor" {
   source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//storage-account?ref=1.2.0"
-  name                      = "stor${random_string.asynchronousingestor.result}"
+  name                      = "stor${random_string.ingestion.result}"
   resource_group_name       = data.azurerm_resource_group.main.name
   location                  = data.azurerm_resource_group.main.location
   account_replication_type  = "LRS"
@@ -88,7 +73,7 @@ module "azfun_asynchronousingestor_stor" {
 }
 
 # Since all functions need a storage connected we just generate a random name
-resource "random_string" "asynchronousingestor" {
+resource "random_string" "ingestion" {
   length  = 10
   special = false
   upper   = false
--- build/infrastructure/azfun-outbox.tf
+++ build/infrastructure/azfun-outbox.tf
@@ -11,62 +11,57 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-module "azfun_synchronousingestor" {
+module "azfun_outbox" {
   source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//function-app?ref=1.2.0"
-  name                                      = "azfun-synchronousingestor-${var.project}-${var.organisation}-${var.environment}"
+  name                                      = "azfun-outbox-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name                       = data.azurerm_resource_group.main.name
   location                                  = data.azurerm_resource_group.main.location
-  storage_account_access_key                = module.azfun_synchronousingestor_stor.primary_access_key
-  storage_account_name                      = module.azfun_synchronousingestor_stor.name
-  app_service_plan_id                       = module.azfun_synchronousingestor_plan.id
+  storage_account_access_key                = module.azfun_outbox_stor.primary_access_key
+  storage_account_name                      = module.azfun_outbox_stor.name
+  app_service_plan_id                       = module.azfun_outbox_plan.id
   application_insights_instrumentation_key  = module.appi.instrumentation_key
   tags                                      = data.azurerm_resource_group.main.tags
+  always_on                                 = true
   app_settings                              = {
     # Region: Default Values
     WEBSITE_ENABLE_SYNC_UPDATE_SITE       = true
     WEBSITE_RUN_FROM_PACKAGE              = 1
     WEBSITES_ENABLE_APP_SERVICE_STORAGE   = true
-    FUNCTIONS_WORKER_RUNTIME              = "dotnet"
+    FUNCTIONS_WORKER_RUNTIME              = "dotnet-isolated"
     # Endregion: Default Values
-    KAFKA_SECURITY_PROTOCOL               = "SaslSsl"
-    KAFKA_SASL_MECHANISM                  = "Plain"
-    KAFKA_SSL_CA_LOCATION                 = "C:\\cacert\\cacert.pem"
-    KAFKA_USERNAME                        = "$ConnectionString"
-    KAFKA_MESSAGE_SEND_MAX_RETRIES        = 5
-    KAFKA_MESSAGE_TIMEOUT_MS              = 1000
-    REQUEST_QUEUE_TOPIC                   = module.evh_requestqueue.name
-    REQUEST_QUEUE_URL                     = "${module.evhnm_requestqueue.name}.servicebus.windows.net:9093"
-    REQUEST_QUEUE_CONNECTION_STRING       = module.evhar_requestqueue_sender.primary_connection_string
-    VALIDATION_REPORTS_QUEUE_TOPIC        = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_TOPIC.value
-    VALIDATION_REPORTS_URL                = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_URL.value
-    VALIDATION_REPORTS_CONNECTION_STRING  = data.azurerm_key_vault_secret.VALIDATION_REPORTS_CONNECTION_STRING.value
+    # VALIDATION_REPORTS_QUEUE_TOPIC        = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_TOPIC.value
+    # VALIDATION_REPORTS_URL                = data.azurerm_key_vault_secret.VALIDATION_REPORTS_QUEUE_URL.value
+    # VALIDATION_REPORTS_CONNECTION_STRING  = data.azurerm_key_vault_secret.VALIDATION_REPORTS_CONNECTION_STRING.value
+    # MARKET_DATA_DB_CONNECTION_STRING      = module.kvs_marketroles_db_connection_string.value
+    MARKET_DATA_QUEUE_TOPIC_NAME          = module.sbq_marketroles.name
+    ACTOR_MESSAGE_DISPATCH_TRIGGER_TIMER  = "*/10 * * * * *"
+    # POST_OFFICE_QUEUE_CONNECTION_STRING   = data.azurerm_key_vault_secret.POST_OFFICE_QUEUE_CONNECTION_STRING.value
+    # POST_OFFICE_QUEUE_TOPIC_NAME          = data.azurerm_key_vault_secret.POST_OFFICE_QUEUE_MARKETDATA_TOPIC_NAME.value
   }
   dependencies                              = [
     module.appi.dependent_on,
-    module.azfun_synchronousingestor_plan.dependent_on,
-    module.azfun_synchronousingestor_stor.dependent_on,
-    module.evh_requestqueue.dependent_on,
-    module.evhnm_requestqueue.dependent_on,
-    module.evhar_requestqueue_sender.dependent_on,
+    module.azfun_outbox_plan.dependent_on,
+    module.azfun_outbox_stor.dependent_on,
+    module.sbq_marketroles.dependent_on,
   ]
 }
 
-module "azfun_synchronousingestor_plan" {
+module "azfun_outbox_plan" {
   source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//app-service-plan?ref=1.2.0"
-  name                = "asp-synchronousingestor-${var.project}-${var.organisation}-${var.environment}"
+  name                = "asp-outbox-${var.project}-${var.organisation}-${var.environment}"
   resource_group_name = data.azurerm_resource_group.main.name
   location            = data.azurerm_resource_group.main.location
   kind                = "FunctionApp"
   sku                 = {
-    tier  = "Free"
-    size  = "F1"
+    tier  = "Basic"
+    size  = "B1"
   }
   tags                = data.azurerm_resource_group.main.tags
 }
 
-module "azfun_synchronousingestor_stor" {
+module "azfun_outbox_stor" {
   source                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//storage-account?ref=1.2.0"
-  name                      = "stor${random_string.synchronousingestor.result}"
+  name                      = "stor${random_string.outbox.result}"
   resource_group_name       = data.azurerm_resource_group.main.name
   location                  = data.azurerm_resource_group.main.location
   account_replication_type  = "LRS"
@@ -76,7 +71,7 @@ module "azfun_synchronousingestor_stor" {
 }
 
 # Since all functions need a storage connected we just generate a random name
-resource "random_string" "synchronousingestor" {
+resource "random_string" "outbox" {
   length  = 10
   special = false
   upper   = false
