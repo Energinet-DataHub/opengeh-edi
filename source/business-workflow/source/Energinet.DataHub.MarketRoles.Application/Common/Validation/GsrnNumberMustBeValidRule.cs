aa74da3235354a33d3e1cc4eeb397946bb567587
Kristian F. Thomsen <kfthomsen@hotmail.com>
2021-05-10 16:45:17 +0200

    Add input validations for change of supplier

---
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Application/Common/Validation/GsrnNumberMustBeValidRule.cs
@@ -0,0 +1,41 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.MarketRoles.Domain.MeteringPoints;
+using Energinet.DataHub.MarketRoles.Domain.SeedWork;
+using FluentValidation;
+
+namespace Energinet.DataHub.MarketRoles.Application.Common.Validation
+{
+    public class GsrnNumberMustBeValidRule : AbstractValidator<string>
+    {
+        public GsrnNumberMustBeValidRule()
+        {
+            CascadeMode = CascadeMode.Stop;
+
+            RuleFor(gsrn => gsrn)
+                .NotEmpty()
+                .WithState(x => CreateValidationError("GSRN number cannot be empty."));
+
+            RuleFor(gsrn => GsrnNumber.CheckRules(gsrn))
+                .Must(x => x.Success)
+                .WithState(x => CreateValidationError("Invalid GSRN number"));
+        }
+
+        private static ValidationError CreateValidationError(string message)
+        {
+            return new ValidationError(message, typeof(GsrnNumberMustBeValidRule));
+        }
+    }
+}
---
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Tests/SampleData.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+
+namespace Energinet.DataHub.MarketRoles.Tests
+{
+    internal static class SampleData
+    {
+        internal static string GsrnNumber => "571234567891234568";
+
+        internal static string ConsumerId => "2601211234";
+
+        internal static string GlnNumber => "5790000555550";
+
+        internal static string TranactionId => Guid.NewGuid().ToString();
+    }
+}
