4303135ff9a372cff048798fd101b943f8581fe7
Kristian F. Thomsen <kfthomsen@hotmail.com>
2021-05-10 17:00:21 +0200

    Add input validation behaviour to business processing pipeline

---
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/BusinessRequestProcessing/Pipeline/InputValidationBehaviour.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.MarketRoles.Application.Common;
+using Energinet.DataHub.MarketRoles.Domain.SeedWork;
+using FluentValidation;
+using MediatR;
+
+namespace Energinet.DataHub.MarketRoles.Infrastructure.BusinessRequestProcessing.Pipeline
+{
+    public class InputValidationBehaviour<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
+        where TRequest : IBusinessRequest
+        where TResponse : BusinessProcessResult
+    {
+        private readonly IValidator<TRequest> _validator;
+
+        public InputValidationBehaviour(IValidator<TRequest> validator)
+        {
+            _validator = validator;
+        }
+
+        public Task<TResponse> Handle(TRequest request, CancellationToken cancellationToken, RequestHandlerDelegate<TResponse> next)
+        {
+            if (request == null) throw new ArgumentNullException(nameof(request));
+            if (next == null) throw new ArgumentNullException(nameof(next));
+
+            if (_validator is null)
+            {
+                return next();
+            }
+
+            var validationResult = _validator.Validate(request);
+            if (!validationResult.IsValid)
+            {
+                var validationErrors = validationResult
+                    .Errors
+                    .Select(error => (ValidationError)error.CustomState)
+                    .ToList()
+                    .AsReadOnly();
+
+                return Task.FromResult((TResponse)new BusinessProcessResult(request.TransactionId, validationErrors));
+            }
+
+            return next();
+        }
+    }
+}
