64ae11047aed9e3d602b0cfbfae10d8145e5e7ba
Kristian F. Thomsen <kfthomsen@hotmail.com>
2021-06-18 12:08:12 +0200

    Add business process handling logic for effectuating consumer move in

---
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.IntegrationTests/Application/MoveIn/EffectuateTests.cs
@@ -0,0 +1,62 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Threading.Tasks;
+using Energinet.DataHub.MarketRoles.Application.MoveIn;
+using Energinet.DataHub.MarketRoles.Application.MoveIn.Processing;
+using Energinet.DataHub.MarketRoles.Domain.MeteringPoints;
+using Energinet.DataHub.MarketRoles.Domain.SeedWork;
+using Energinet.DataHub.MarketRoles.Infrastructure.Integration.IntegrationEventDispatching.ChangeOfSupplier;
+using Energinet.DataHub.MarketRoles.Infrastructure.Integration.IntegrationEventDispatching.MoveIn;
+using Xunit;
+
+namespace Energinet.DataHub.MarketRoles.IntegrationTests.Application.MoveIn
+{
+    #pragma warning disable
+    public class EffectuateTests : TestHost
+    {
+        [Fact]
+        public async Task Effectuate_WhenEffectiveDateIsDue_IntegrationEventsArePublished()
+        {
+            var (accountingPoint, transaction) = await SetupScenarioAsync();
+            var command = new EffectuateConsumerMoveIn(accountingPoint.Id.Value, transaction.Value);
+
+            await InvokeCommandAsync(command);
+
+            await AssertOutboxMessage<ConsumerMovedInIntegrationEvent>();
+            await AssertOutboxMessage<EnergySupplierChanged>();
+        }
+
+        private async Task<(AccountingPoint AccountingPoint, Transaction Transaction)> SetupScenarioAsync()
+        {
+            var accountingPoint = CreateAccountingPoint();
+            CreateEnergySupplier();
+            SaveChanges();
+
+            var moveInDate = GetService<ISystemDateTimeProvider>().Now();
+            var requestMoveIn = new RequestMoveIn(
+                SampleData.Transaction,
+                SampleData.GlnNumber,
+                SampleData.ConsumerSSN,
+                string.Empty,
+                SampleData.ConsumerName,
+                SampleData.GsrnNumber,
+                moveInDate);
+
+            var result = await SendRequest(requestMoveIn);
+
+            return (accountingPoint, Transaction.Create(result.TransactionId));
+        }
+    }
+}
