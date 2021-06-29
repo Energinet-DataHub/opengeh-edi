aa6e48a9d98888b53fabf6927f0f0e21a1a1b568
Kristian F. Thomsen <kfthomsen@hotmail.com>
2021-06-29 14:21:28 +0200

    Add service for dispatching internal commands to Azure Service Bus queue

---
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/InternalCommands/InternalCommandServiceBusDispatcher.cs
@@ -0,0 +1,57 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+using Energinet.DataHub.MarketRoles.Infrastructure.Correlation;
+
+namespace Energinet.DataHub.MarketRoles.Infrastructure.InternalCommands
+{
+    public class InternalCommandServiceBusDispatcher : IInternalCommandDispatcher
+    {
+        private readonly ICorrelationContext _correlationContext;
+        private readonly ServiceBusSender _serviceBusSender;
+
+        public InternalCommandServiceBusDispatcher(ICorrelationContext correlationContext, ServiceBusSender serviceBusSender)
+        {
+            _correlationContext = correlationContext ?? throw new ArgumentNullException(nameof(correlationContext));
+            _serviceBusSender = serviceBusSender ?? throw new ArgumentNullException(nameof(serviceBusSender));
+        }
+
+        public async Task<DispatchResult> DispatchAsync(QueuedInternalCommand queuedInternalCommand)
+        {
+            if (queuedInternalCommand == null) throw new ArgumentNullException(nameof(queuedInternalCommand));
+            var message = CreateMessageFrom(queuedInternalCommand);
+
+            if (queuedInternalCommand.ScheduleDate.HasValue)
+            {
+                var sequenceNumber = await _serviceBusSender.ScheduleMessageAsync(message, queuedInternalCommand.ScheduleDate.Value.ToDateTimeOffset()).ConfigureAwait(false);
+                return DispatchResult.Create(sequenceNumber);
+            }
+
+            await _serviceBusSender.SendMessageAsync(message).ConfigureAwait(false);
+            return DispatchResult.Create(default);
+        }
+
+        private ServiceBusMessage CreateMessageFrom(QueuedInternalCommand queuedInternalCommand)
+        {
+            var message = new ServiceBusMessage(queuedInternalCommand.Data);
+            message.MessageId = queuedInternalCommand.Id.ToString();
+            message.CorrelationId = _correlationContext.GetCorrelationId();
+
+            return message;
+        }
+    }
+}
