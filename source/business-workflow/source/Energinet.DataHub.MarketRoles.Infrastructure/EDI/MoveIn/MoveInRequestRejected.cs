a8470697f4770eac120cdeba1a752c9513318ff4
Christopher Maigaard <xchma@energinet.dk>
2021-07-02 14:06:40 +0200

    missing license

--- source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/MoveIn/MoveInRequestRejected.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/MoveIn/MoveInRequestRejected.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright 2020 Energinet DataHub A/S
 //
 // Licensed under the Apache License, Version 2.0 (the "License2");
-// you may not use file except in compliance with the License.
+// you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     http://www.apache.org/licenses/LICENSE-2.0
