beb8705fb03e149d49d82e809638faf9631fb31d
Christopher Maigaard <xchma@energinet.dk>
2021-07-06 21:25:48 +0200

    fix build error

--- source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/MoveIn/RequestMoveInResultHandler.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/MoveIn/RequestMoveInResultHandler.cs
@@ -13,6 +13,7 @@
 // limitations under the License.
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Energinet.DataHub.MarketRoles.Application.Common;
 using Energinet.DataHub.MarketRoles.Application.MoveIn;
