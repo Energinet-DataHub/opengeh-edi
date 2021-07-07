11c435669f7502074c186f05bd5ee836e4495a3c
Daniel Pethjen <daniel@pethjen.dk>
2021-07-07 11:18:02 +0200

    init commit

--- source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/Users/UserIdentityFactory.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/Users/UserIdentityFactory.cs
@@ -14,30 +14,33 @@
 
 using System;
 using System.Linq;
+using System.Text.Json;
 using Energinet.DataHub.MarketRoles.Application.Common.Users;
 
 namespace Energinet.DataHub.MarketRoles.Infrastructure.Users
 {
-    #pragma warning disable CA1822 // Could be static, but we keep it non-static for now.
     public class UserIdentityFactory
     {
-        public UserIdentity FromDictionaryString(string inputText, string propertyKey)
-        {
-            if (string.IsNullOrWhiteSpace(inputText)) throw new ArgumentNullException(nameof(inputText));
-            if (string.IsNullOrWhiteSpace(propertyKey)) throw new ArgumentNullException(nameof(propertyKey));
-
-            var inputJsonDocument = System.Text.Json.JsonDocument.Parse(inputText);
-            var resultJsonProperty = inputJsonDocument.RootElement
-                .EnumerateObject()
-                .FirstOrDefault(e => e.Name.Equals(propertyKey, StringComparison.OrdinalIgnoreCase));
-
-            return FromString(resultJsonProperty.Value.ToString() ?? string.Empty);
-        }
-
         public UserIdentity FromString(string userIdentity)
         {
             if (string.IsNullOrWhiteSpace(userIdentity)) throw new ArgumentNullException(nameof(userIdentity));
             return System.Text.Json.JsonSerializer.Deserialize<UserIdentity>(userIdentity) ?? throw new System.Text.Json.JsonException(nameof(userIdentity));
+        }
+
+        public UserIdentity? FromDictionaryString(string inputText, string propertyKey)
+        {
+            if (string.IsNullOrWhiteSpace(inputText)) throw new ArgumentNullException(nameof(inputText));
+            if (string.IsNullOrWhiteSpace(propertyKey)) throw new ArgumentNullException(nameof(propertyKey));
+            var inputJsonDocument = System.Text.Json.JsonDocument.Parse(inputText);
+            var resultJsonProperty = inputJsonDocument.RootElement
+                .EnumerateObject()
+                .FirstOrDefault(e => e.Name.Equals(propertyKey));
+            if (resultJsonProperty.Value.ValueKind == JsonValueKind.Undefined)
+            {
+                return null;
+            }
+
+            return FromString(resultJsonProperty.Value.ToString() ?? string.Empty);
         }
     }
 }
