bdaf136dcec31cfd03a25dd3f62929e4d90424fd
Christopher <xchma@energinet.dk>
2021-07-07 11:21:53 +0200

    Update source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/Errors/Converters/CprNumberFormatRuleErrorConverter.cs
    Co-authored-by: erduna <ermin.d@gmail.com>

--- source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/Errors/Converters/CprNumberFormatRuleErrorConverter.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/Errors/Converters/CprNumberFormatRuleErrorConverter.cs
@@ -20,7 +20,7 @@ namespace Energinet.DataHub.MarketRoles.Infrastructure.EDI.Errors.Converters
     {
         protected override Error Convert(CprNumberFormatRuleError error)
         {
-            return new("TODO", $"Description");
+            return new("D17", $"Description");
         }
     }
 }
