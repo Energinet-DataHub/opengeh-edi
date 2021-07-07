89610e45c213831b111752ed4a2d9b49ac594941
Christopher <xchma@energinet.dk>
2021-07-07 11:22:52 +0200

    Update source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/Errors/Converters/StartOfSupplyMustBeValidRuleErrorConverter.cs
    Co-authored-by: erduna <ermin.d@gmail.com>

--- source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/Errors/Converters/StartOfSupplyMustBeValidRuleErrorConverter.cs
+++ source/business-workflow/source/Energinet.DataHub.MarketRoles.Infrastructure/EDI/Errors/Converters/StartOfSupplyMustBeValidRuleErrorConverter.cs
@@ -20,7 +20,7 @@ namespace Energinet.DataHub.MarketRoles.Infrastructure.EDI.Errors.Converters
     {
         protected override Error Convert(StartOfSupplyMustBeValidRuleError error)
         {
-            return new("TODO", $"Description");
+            return new("E17", $"Effectuation date (TODO: Insert date) incorrect: The information is not received within the correct time period (either too soon or too late)");
         }
     }
 }
