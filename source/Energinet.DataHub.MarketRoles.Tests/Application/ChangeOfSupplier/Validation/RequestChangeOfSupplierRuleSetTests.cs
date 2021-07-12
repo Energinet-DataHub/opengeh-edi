2fc4f265306a9c863cbfd4e9ee80df36a6ebfcaf
Ermin <ermin.d@gmail.com>
2021-07-12 13:22:26 +0200

    more test fix

--- source/Energinet.DataHub.MarketRoles.Tests/Application/ChangeOfSupplier/Validation/RequestChangeOfSupplierRuleSetTests.cs
+++ source/Energinet.DataHub.MarketRoles.Tests/Application/ChangeOfSupplier/Validation/RequestChangeOfSupplierRuleSetTests.cs
@@ -134,7 +134,8 @@ namespace Energinet.DataHub.MarketRoles.Tests.Application.ChangeOfSupplier.Valid
             return new RequestChangeOfSupplier(
                 transaction,
                 glnNumber,
-                "FakeCVROrCPR",
+                "1212120000",
+                "12345678",
                 gsrnNumber,
                 startDate);
         }
