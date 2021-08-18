d05920a575466128e2625d05d5a6b4937fece63a
Daniel Pethjen <daniel@pethjen.dk>
2021-08-18 10:06:53 +0200

    Clean up

--- build/infrastructure/main/variables.tf
+++ build/infrastructure/main/variables.tf
@@ -39,11 +39,6 @@ variable "sharedresources_resource_group_name" {
   type          = string
   description   = "Resource group name of the Core keyvaults location"
 }
-
-variable "sharedresources_integrationevents_service_bus_namespace_name" {
-  type          = string
-  description   = "Namespace of the shared integration event service bus"
-}  
   
 variable "sharedresources_sql_server_name" {
   type          = string
