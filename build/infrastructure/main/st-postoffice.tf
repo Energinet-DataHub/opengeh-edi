92596900c489edd428ddb21582cbb5e08858c71d
Dan Stenrøjl <XDAST@energinet.dk>
2022-01-28 19:09:45 +0100

    What: Use 6.0.0-storage-account module

--- build/infrastructure/main/st-postoffice.tf
+++ build/infrastructure/main/st-postoffice.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "st_postoffice" {
-    source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account?ref=6.0.0"
+    source                          = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/storage-account?ref=6.0.0-storage-account"
 
     name                            = "tmp"
     project_name                    = var.domain_name_short
