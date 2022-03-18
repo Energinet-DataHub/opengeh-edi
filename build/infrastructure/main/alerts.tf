53afde73f6e082d1d069b0297932e966076943ff
Daniel Pethjen <daniel@pethjen.dk>
2022-03-18 13:16:43 +0100

    add alert for ingestion

--- build/infrastructure/main/alerts.tf
+++ build/infrastructure/main/alerts.tf
@@ -50,6 +50,36 @@ exceptions
   time_window = 30
   trigger {
     operator  = "GreaterThan"
-    threshold = 0
+    threshold = 1
+  }
+}
+
+resource "azurerm_monitor_scheduled_query_rules_alert" "marketroles_ingestion" {
+  name                = "alert-ingestion-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}"
+  location            = azurerm_resource_group.this.location
+  resource_group_name = var.shared_resources_resource_group_name
+
+  action {
+    action_group           = [azurerm_monitor_action_group.marketroles.id]
+  }
+  data_source_id = data.azurerm_key_vault_secret.appi_shared_id.value
+  description    = "Alert when total results cross threshold"
+  enabled        = true
+  # Count all requests with server error result code grouped into 5-minute bins
+  query       = <<-QUERY
+  requests
+| where timestamp > ago(1h) and  success == false
+| join kind= inner (
+exceptions
+| where timestamp > ago(1h)  and cloud_RoleName == 'func-ingestion-${lower(var.domain_name_short)}-${lower(var.environment_short)}-${lower(var.environment_instance)}'
+) on operation_Id
+| project exceptionType = type, failedMethod = method, requestName = name, requestDuration = duration, function = cloud_RoleName
+  QUERY
+  severity    = 1
+  frequency   = 5
+  time_window = 30
+  trigger {
+    operator  = "GreaterThan"
+    threshold = 1
   }
 }
