a6cc8c5843f542940e910daec6e2ee2b9e82239d
Daniel Pethjen <daniel@pethjen.dk>
2022-03-29 12:52:54 +0200

    Remove unused usings

--- source/B2B.Transactions/DataAccess/Transaction/TransactionRepository.cs
+++ source/B2B.Transactions/DataAccess/Transaction/TransactionRepository.cs
@@ -12,9 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using B2B.Transactions.Transactions;
 
 namespace B2B.Transactions.DataAccess.Transaction
