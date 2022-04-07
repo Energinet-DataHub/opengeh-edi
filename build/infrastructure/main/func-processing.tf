79518d23397bfee69442928f83122878927eb6d2
Dan Stenrøjl <XDAST@energinet.dk>
2022-04-07 08:50:05 +0200

    TF update to function-app 5.12.0

--- build/infrastructure/main/func-processing.tf
+++ build/infrastructure/main/func-processing.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 module "func_processing" {
-  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=5.1.0"
+  source                                    = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/function-app?ref=5.12.0"
 
   name                                      = "processing"
   project_name                              = var.domain_name_short
@@ -22,6 +22,7 @@ module "func_processing" {
   location                                  = azurerm_resource_group.this.location
   app_service_plan_id                       = data.azurerm_key_vault_secret.plan_shared_id.value
   application_insights_instrumentation_key  = data.azurerm_key_vault_secret.appi_instrumentation_key.value
+  log_analytics_workspace_id                = data.azurerm_key_vault_secret.log_shared_id.value
   always_on                                 = true
   app_settings                              = {
     # Region: Default Values
@@ -37,6 +38,6 @@ module "func_processing" {
     INTEGRATION_EVENT_QUEUE                 = data.azurerm_key_vault_secret.sbq_event_forwarded_queue.value
     INTEGRATION_EVENT_QUEUE_CONNECTION      = data.azurerm_key_vault_secret.sb_domain_relay_listener_connection_string.value
   }
-  
+
   tags                                      = azurerm_resource_group.this.tags
 }
