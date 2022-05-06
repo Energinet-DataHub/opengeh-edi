206d6016fc4bd85b2965570f4f1337112301a2df
Daniel Bjerring Jørgensen <dbj1986dk@gmail.com>
2022-05-06 10:02:22 +0200

    remove private_dns_resource_group_name

--- build/infrastructure/main/st-postoffice.tf
+++ build/infrastructure/main/st-postoffice.tf
@@ -24,7 +24,6 @@ module "st_postoffice" {
     account_tier                    = "Standard"
 
     private_endpoint_subnet_id      = data.azurerm_key_vault_secret.snet_private_endpoints_id.value
-    private_dns_resource_group_name = data.azurerm_key_vault_secret.pdns_resource_group_name.value
     use_blob                        = false
     use_file                        = true
 
