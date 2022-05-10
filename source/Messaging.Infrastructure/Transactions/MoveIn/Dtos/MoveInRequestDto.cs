d8a13decd970cec6d233cde4ac3b226993c44329
Daniel Christensen <daniel@dchr.dk>
2022-05-10 13:38:51 +0200

    Added DTOs for data transport

---
+++ source/Messaging.Infrastructure/Transactions/MoveIn/Dtos/MoveInRequestDto.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Messaging.Infrastructure.Transactions.MoveIn.Dtos;
+
+public class MoveInRequestDto
+{
+    public MoveInRequestDto(string? consumerName, string? energySupplierGlnNumber, string? socialSecurityNumber, string? vatNumber, string accountingPointGsrnNumber, string startDate, string transactionId)
+    {
+        ConsumerName = consumerName;
+        EnergySupplierGlnNumber = energySupplierGlnNumber;
+        SocialSecurityNumber = socialSecurityNumber;
+        VATNumber = vatNumber;
+        AccountingPointGsrnNumber = accountingPointGsrnNumber;
+        StartDate = startDate;
+        TransactionId = transactionId;
+    }
+
+    public string TransactionId { get; }
+
+    public string? ConsumerName { get; }
+
+    public string? EnergySupplierGlnNumber { get; }
+
+    public string? SocialSecurityNumber { get; }
+
+    public string? VATNumber { get; }
+
+    public string AccountingPointGsrnNumber { get; }
+
+    public string StartDate { get; }
+}
--- source/Messaging.Infrastructure/Transactions/MoveIn/MoveInRequestAdapter.cs
+++ source/Messaging.Infrastructure/Transactions/MoveIn/MoveInRequestAdapter.cs
@@ -13,11 +13,14 @@
 // limitations under the License.
 
 using System;
+using System.Linq;
 using System.Net.Http;
 using System.Net.Http.Json;
 using System.Threading.Tasks;
 using Messaging.Application.Transactions;
 using Messaging.Application.Transactions.MoveIn;
+using Messaging.Infrastructure.Transactions.MoveIn.Dtos;
+using MoveInRequestDto = Messaging.Infrastructure.Transactions.MoveIn.Dtos.MoveInRequestDto;
 
 namespace Messaging.Infrastructure.Transactions.MoveIn;
 public sealed class MoveInRequestAdapter : IMoveInRequestAdapter
@@ -35,7 +38,24 @@ public sealed class MoveInRequestAdapter : IMoveInRequestAdapter
     {
         if (request == null) throw new ArgumentNullException(nameof(request));
 
-        var response = await _httpClient.PostAsJsonAsync(_moveInRequestUrl, request).ConfigureAwait(false);
-        return await response.Content.ReadFromJsonAsync<BusinessRequestResult>().ConfigureAwait(false) ?? throw new InvalidOperationException();
+        var moveInRequestDto = new MoveInRequestDto(
+            request.ConsumerName,
+            request.EnergySupplierGlnNumber,
+            request.SocialSecurityNumber,
+            request.VATNumber,
+            request.AccountingPointGsrnNumber,
+            request.StartDate,
+            request.TransactionId);
+
+        var response = await _httpClient.PostAsJsonAsync(_moveInRequestUrl, moveInRequestDto).ConfigureAwait(false);
+        var moveInResponseDto = await response.Content.ReadFromJsonAsync<MoveInResponseDto>().ConfigureAwait(false) ?? throw new InvalidOperationException();
+
+        if (moveInResponseDto.ValidationErrors.Count > 0)
+        {
+            var validationErrors = moveInResponseDto.ValidationErrors.Select(error => new ValidationError(error.Code, error.Message));
+            return BusinessRequestResult.Failure(validationErrors.ToArray());
+        }
+
+        return BusinessRequestResult.Succeeded();
     }
 }
