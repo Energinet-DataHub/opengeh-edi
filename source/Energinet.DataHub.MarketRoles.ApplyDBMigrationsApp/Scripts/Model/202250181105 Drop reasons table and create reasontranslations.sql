42bfe4b068717e93983e07be92af7e053034efa6
Kristian F. Thomsen <kft@energinet.dk>
2022-05-18 11:35:45 +0200

    Fix script

--- source/Energinet.DataHub.MarketRoles.ApplyDBMigrationsApp/Scripts/Model/202250181105 Drop reasons table and create reasontranslations.sql
+++ source/Energinet.DataHub.MarketRoles.ApplyDBMigrationsApp/Scripts/Model/202250181105 Drop reasons table and create reasontranslations.sql
@@ -6,9 +6,9 @@ CREATE TABLE [b2b].[ReasonTranslations](
     [ErrorCode] [nvarchar](250) NOT NULL,
     [Code] [nvarchar](3) NOT NULL,
     [Text] [nvarchar](max) NOT NULL,
-    [LanguageCode] [nvarchar](2) NOT NULL
+    [LanguageCode] [nvarchar](2) NOT NULL,
+    CONSTRAINT [UC_Code] UNIQUE (ErrorCode, LanguageCode),
     CONSTRAINT [PK_ReasonTranslations] PRIMARY KEY NONCLUSTERED
-    CONSTRAINT [UC_Code] UNIQUE (ErrorCode, LanguageCode)
 (
 [Id] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY],
