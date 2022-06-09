64b80175f663159294bece7ab52853acee31044d
Kristian F. Thomsen <kft@energinet.dk>
2022-06-09 08:35:39 +0200

    Remove script

--- source/Energinet.DataHub.MarketRoles.ApplyDBMigrationsApp/Scripts/Model/202260081220 Add reason code to outgoing messages.sql
+++
@@ -1,9 +0,0 @@
-﻿ALTER TABLE [b2b].[OutgoingMessages]
-    ADD [ReasonCode] [nvarchar](10) NOT NULL
-    GO
-UPDATE [b2b].[OutgoingMessages]
-SET ReasonCode =  CASE
-    WHEN DocumentType = 'ConfirmRequestChangeOfSupplier' THEN 'A01'
-    WHEN DocumentType = 'RejectRequestChangeOfSupplier' THEN 'A02'
-END
-WHERE DocumentType IN ('ConfirmRequestChangeOfSupplier', 'RejectRequestChangeOfSupplier')
