0d747405162ba1adf7b773b6169fb66ee659a655
Kristian F. Thomsen <kft@energinet.dk>
2022-06-09 09:02:31 +0200

    Add general notification

--- source/Messaging.Application/Common/HeaderWriter.cs
+++ source/Messaging.Application/Common/HeaderWriter.cs
@@ -63,6 +63,9 @@ public static class HeaderWriter
             .WriteElementStringAsync(documentDetails.Prefix, "receiver_MarketParticipant.marketRole.type", null, messageHeader.ReceiverRole)
             .ConfigureAwait(false);
         await writer.WriteElementStringAsync(documentDetails.Prefix, "createdDateTime", null, messageHeader.TimeStamp.ToString()).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(documentDetails.Prefix, "reason.code", null, messageHeader.ReasonCode).ConfigureAwait(false);
+        if (messageHeader.ReasonCode is not null)
+        {
+            await writer.WriteElementStringAsync(documentDetails.Prefix, "reason.code", null, messageHeader.ReasonCode).ConfigureAwait(false);
+        }
     }
 }
