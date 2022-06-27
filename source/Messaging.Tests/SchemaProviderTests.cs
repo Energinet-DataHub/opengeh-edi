191aca9577d7fdbfad668cae8e0c3ac034f1117a
Ib Kunz Mathiesen <kunzmathiesen@hotmail.com>
2022-06-27 13:55:26 +0200

    Pseudo test

---
+++ source/Messaging.Tests/SchemaProviderTests.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Mime;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+using Messaging.Application.SchemaStore;
+using Xunit;
+
+namespace Messaging.Tests;
+
+public class SchemaProviderTests
+{
+    [Fact]
+    public async Task SomeTest()
+    {
+        var xmlProvider = SchemaProviderFactory.GetProvider(MediaTypeNames.Application.Xml);
+        var schema = await xmlProvider.GetSchemaAsync("confirmrequestchangeofsupplier", "0.1").ConfigureAwait(false);
+        Assert.Equal(typeof(XmlSchema), schema?.GetType());
+    }
+
+    [Fact]
+    public async Task SomeTest2()
+    {
+        var xmlProvider = SchemaProviderFactory.GetProvider(MediaTypeNames.Application.Json);
+        var schema = await xmlProvider.GetSchemaAsync("confirmrequestchangeofsupplier", "0.1").ConfigureAwait(false);
+        Assert.Equal(typeof(XmlSchema), schema?.GetType());
+    }
+}
