04e1b1e80344701e96f54e4577bf8bb00ec205e4
Daniel Pethjen <daniel@pethjen.dk>
2022-06-28 14:48:51 +0200

    added queue name correctly

--- build/infrastructure/main/func-api.tf
+++ build/infrastructure/main/func-api.tf
@@ -54,7 +54,7 @@ module "func_receiver" {
     METERING_POINT_MASTER_DATA_RESPONSE_QUEUE_NAME                = data.azurerm_key_vault_secret.sbq_metering_point_master_data_response_name.value,
     ENERGY_SUPPLIER_CHANGED_TOPIC                                 = data.azurerm_key_vault_secret.sbt_energy_supplier_changed_name.value,
     ENERGY_SUPPLIER_CHANGED_SUBSCRIPTION                          = data.azurerm_key_vault_secret.sbs_energy_supplier_changed_to_messaging_name.value
-    MASTER_DATA_REQUEST_QUEUE_NAME                                = "metering-point-master-data-request"
+    MASTER_DATA_REQUEST_QUEUE_NAME                                = data.azurerm_key_vault_secret.sbq_metering_point_master_data_request_name.value,
     SHARED_SERVICE_BUS_SEND_CONNECTION_STRING                     = "@Microsoft.KeyVault(VaultName=${var.shared_resources_keyvault_name};SecretName=sb-domain-relay-send-connection-string)"
   }
 
