2db08be741fe9879ae600fab98c7de68ff79a689
Daniel Pethjen <daniel@pethjen.dk>
2022-07-06 13:38:03 +0200

    Assert master data message

--- source/Messaging.IntegrationTests/Application/Transactions/MoveIn/ForwardMeteringPointMasterDataTests.cs
+++ source/Messaging.IntegrationTests/Application/Transactions/MoveIn/ForwardMeteringPointMasterDataTests.cs
@@ -24,6 +24,7 @@ using Messaging.Application.OutgoingMessages.AccountingPointCharacteristics;
 using Messaging.Application.Transactions.MoveIn;
 using Messaging.IntegrationTests.Application.IncomingMessages;
 using Messaging.IntegrationTests.Fixtures;
+using NodaTime.Extensions;
 using Processing.IntegrationTests.Application;
 using Xunit;
 using Address = Messaging.Application.MasterData.Address;
@@ -54,20 +55,72 @@ public class ForwardMeteringPointMasterDataTests : TestBase
     public async Task Correct_metering_point_master_data_message_is_added_to_outgoing_message_store()
     {
         await SetupAnAcceptedMoveInTransaction().ConfigureAwait(false);
-
         var masterData = CreateMasterDataContent();
+
         var forwardMeteringPointMasterData = new ForwardMeteringPointMasterData(SampleData.TransactionId, masterData);
         await InvokeCommandAsync(forwardMeteringPointMasterData).ConfigureAwait(false);
 
         var marketActivityRecord = GetMarketActivityRecord("AccountingPointCharacteristics");
-        Assert.NotNull(marketActivityRecord);
+        AssertMasterData(masterData, marketActivityRecord.MarketEvaluationPt);
+    }
+
+    private static void AssertMasterData(MasterDataContent masterDataContent, MarketEvaluationPoint marketEvaluationPoint)
+    {
+        Assert.Equal(SampleData.MarketEvaluationPointId, marketEvaluationPoint.MRID.Id);
+        Assert.Equal(masterDataContent.MeteringPointResponsible, marketEvaluationPoint.MeteringPointResponsible.Id);
+        Assert.Equal(masterDataContent.Type, marketEvaluationPoint.Type);
+        Assert.Equal(masterDataContent.SettlementMethod, marketEvaluationPoint.SettlementMethod);
+        Assert.Equal(masterDataContent.MeteringMethod, marketEvaluationPoint.MeteringMethod);
+        Assert.Equal(masterDataContent.ConnectionState, marketEvaluationPoint.ConnectionState);
+        Assert.Equal(masterDataContent.ReadingPeriodicity, marketEvaluationPoint.ReadCycle);
+        Assert.Equal(masterDataContent.NetSettlementGroup, marketEvaluationPoint.NetSettlementGroup);
+        Assert.Equal(masterDataContent.ScheduledMeterReadingDate, marketEvaluationPoint.NextReadingDate);
+        Assert.Equal(masterDataContent.GridAreaDetails.Code, marketEvaluationPoint.MeteringGridAreaId.Id);
+        Assert.Equal(masterDataContent.GridAreaDetails.FromCode, marketEvaluationPoint.OutMeteringGridAreaId.Id);
+        Assert.Equal(masterDataContent.GridAreaDetails.ToCode, marketEvaluationPoint.InMeteringGridAreaId.Id);
+        Assert.Equal(masterDataContent.PowerPlantGsrnNumber, marketEvaluationPoint.LinkedMarketEvaluationPointId.Id);
+        Assert.Equal(
+            masterDataContent.Capacity.ToString(CultureInfo.InvariantCulture),
+            marketEvaluationPoint.PhysicalConnectionCapacity.Value);
+        Assert.Equal(masterDataContent.ConnectionType, marketEvaluationPoint.ConnectionType);
+        Assert.Equal(masterDataContent.DisconnectionType, marketEvaluationPoint.DisconnectionMethod);
+        Assert.Equal(masterDataContent.AssetType, marketEvaluationPoint.PsrType);
+        Assert.Equal(
+            masterDataContent.ProductionObligation.ToString(CultureInfo.InvariantCulture),
+            marketEvaluationPoint.ProductionObligation);
+        Assert.Equal(
+            masterDataContent.MaximumPower.ToString(CultureInfo.InvariantCulture),
+            marketEvaluationPoint.ContractedConnectionCapacity.Value);
+        Assert.Equal(
+            masterDataContent.MaximumCurrent.ToString(CultureInfo.InvariantCulture),
+            marketEvaluationPoint.RatedCurrent.Value);
+        Assert.Equal(masterDataContent.MeterNumber, marketEvaluationPoint.MeterId);
+        Assert.Equal(masterDataContent.Series.Product, marketEvaluationPoint.Series.Product);
+        Assert.Equal(masterDataContent.Series.UnitType, marketEvaluationPoint.Series.QuantityMeasureUnit);
+        Assert.Equal(masterDataContent.EffectiveDate.ToUniversalTime().ToInstant(), marketEvaluationPoint.SupplyStart);
+        Assert.Equal(masterDataContent.Address.LocationDescription, marketEvaluationPoint.Description);
+        Assert.Equal(masterDataContent.Address.GeoInfoReference.ToString(), marketEvaluationPoint.GeoInfoReference);
+        Assert.Equal(masterDataContent.Address.IsActualAddress.ToString(), marketEvaluationPoint.IsActualAddress);
+        Assert.Null(marketEvaluationPoint.ParentMarketEvaluationPoint);
+        Assert.Equal(masterDataContent.Address.StreetCode, marketEvaluationPoint.MainAddress.Street.Code);
+        Assert.Equal(masterDataContent.Address.StreetName, marketEvaluationPoint.MainAddress.Street.Name);
+        Assert.Equal(masterDataContent.Address.BuildingNumber, marketEvaluationPoint.MainAddress.Street.Number);
+        Assert.Equal(masterDataContent.Address.Floor, marketEvaluationPoint.MainAddress.Street.FloorIdentification);
+        Assert.Equal(masterDataContent.Address.Room, marketEvaluationPoint.MainAddress.Street.SuiteNumber);
+        Assert.Equal(
+            masterDataContent.Address.MunicipalityCode.ToString(CultureInfo.InvariantCulture),
+            marketEvaluationPoint.MainAddress.Town.Code);
+        Assert.Equal(masterDataContent.Address.City, marketEvaluationPoint.MainAddress.Town.Name);
+        Assert.Equal(masterDataContent.Address.CitySubDivision, marketEvaluationPoint.MainAddress.Town.Section);
+        Assert.Equal(masterDataContent.Address.CountryCode, marketEvaluationPoint.MainAddress.Town.Country);
     }
 
     private static IncomingMessageBuilder MessageBuilder()
     {
         return new IncomingMessageBuilder()
             .WithMessageId(SampleData.OriginalMessageId)
-            .WithTransactionId(SampleData.TransactionId);
+            .WithTransactionId(SampleData.TransactionId)
+            .WithMarketEvaluationPointId(SampleData.MarketEvaluationPointId);
     }
 
     private static MasterDataContent CreateMasterDataContent()
