1ebab9d104192a5bfee197a339f680ad4d39d9ca
Kristian F. Thomsen <kft@energinet.dk>
2022-07-06 11:01:07 +0200

    Inline fields

--- source/Messaging.Application/OutgoingMessages/AccountingPointCharacteristics/AccountingPointCharacteristicsDocumentWriter.cs
+++ source/Messaging.Application/OutgoingMessages/AccountingPointCharacteristics/AccountingPointCharacteristicsDocumentWriter.cs
@@ -22,13 +22,15 @@ namespace Messaging.Application.OutgoingMessages.AccountingPointCharacteristics;
 
 public class AccountingPointCharacteristicsDocumentWriter : DocumentWriter
 {
-    private const string Prefix = "cim";
-    private const string DocumentType = "AccountingPointCharacteristics_MarketDocument";
-    private const string XmlNamespace = "urn:ediel.org:structure:accountingpointcharacteristics:0:1";
-    private const string SchemaLocation = "urn:ediel.org:structure:accountingpointcharacteristics:0:1 urn-ediel-org-structure-accountingpointcharacteristics-0-1.xsd";
-
     public AccountingPointCharacteristicsDocumentWriter(IMarketActivityRecordParser parser)
-        : base(new DocumentDetails(DocumentType, SchemaLocation, XmlNamespace, Prefix, "E07"), parser)
+        : base(
+            new DocumentDetails(
+                "AccountingPointCharacteristics_MarketDocument",
+                "urn:ediel.org:structure:accountingpointcharacteristics:0:1 urn-ediel-org-structure-accountingpointcharacteristics-0-1.xsd",
+                "urn:ediel.org:structure:accountingpointcharacteristics:0:1",
+                "cim",
+                "E07"),
+            parser)
     {
     }
 
@@ -49,37 +51,37 @@ public class AccountingPointCharacteristicsDocumentWriter : DocumentWriter
 
     private static async Task WriteMarketEvaluationPointAsync(MarketEvaluationPoint marketEvaluationPoint, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, "MarketEvaluationPoint", null).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", "MarketEvaluationPoint", null).ConfigureAwait(false);
         await WriteMridAsync("mRID", marketEvaluationPoint.MRID, writer).ConfigureAwait(false);
         await WriteMridAsync("meteringPointResponsible_MarketParticipant.mRID", marketEvaluationPoint.MeteringPointResponsible, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "type", null, marketEvaluationPoint.Type).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "settlementMethod", null, marketEvaluationPoint.SettlementMethod).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "meteringMethod", null, marketEvaluationPoint.MeteringMethod).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "connectionState", null, marketEvaluationPoint.ConnectionState).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "readCycle", null, marketEvaluationPoint.ReadCycle).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "netSettlementGroup", null, marketEvaluationPoint.NetSettlementGroup).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "nextReadingDate", null, marketEvaluationPoint.NextReadingDate).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "type", null, marketEvaluationPoint.Type).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "settlementMethod", null, marketEvaluationPoint.SettlementMethod).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "meteringMethod", null, marketEvaluationPoint.MeteringMethod).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "connectionState", null, marketEvaluationPoint.ConnectionState).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "readCycle", null, marketEvaluationPoint.ReadCycle).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "netSettlementGroup", null, marketEvaluationPoint.NetSettlementGroup).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "nextReadingDate", null, marketEvaluationPoint.NextReadingDate).ConfigureAwait(false);
         await WriteMridAsync("meteringGridArea_Domain.mRID", marketEvaluationPoint.MeteringGridAreaId, writer).ConfigureAwait(false);
         await WriteMridAsync("inMeteringGridArea_Domain.mRID", marketEvaluationPoint.InMeteringGridAreaId, writer).ConfigureAwait(false);
         await WriteMridAsync("outMeteringGridArea_Domain.mRID", marketEvaluationPoint.OutMeteringGridAreaId, writer).ConfigureAwait(false);
         await WriteUnitValueAsync("physicalConnectionCapacity", marketEvaluationPoint.PhysicalConnectionCapacity, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "mPConnectionType", null, marketEvaluationPoint.ConnectionType).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "disconnectionMethod", null, marketEvaluationPoint.DisconnectionMethod).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "asset_MktPSRType.psrType", null, marketEvaluationPoint.PsrType).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "productionObligation", null, marketEvaluationPoint.ProductionObligation).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "mPConnectionType", null, marketEvaluationPoint.ConnectionType).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "disconnectionMethod", null, marketEvaluationPoint.DisconnectionMethod).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "asset_MktPSRType.psrType", null, marketEvaluationPoint.PsrType).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "productionObligation", null, marketEvaluationPoint.ProductionObligation).ConfigureAwait(false);
         await WriteUnitValueAsync("contractedConnectionCapacity", marketEvaluationPoint.ContractedConnectionCapacity, writer).ConfigureAwait(false);
         await WriteUnitValueAsync("ratedCurrent", marketEvaluationPoint.RatedCurrent, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "meter.mRID", null, marketEvaluationPoint.MeterId).ConfigureAwait(false);
-        await writer.WriteStartElementAsync(Prefix, "Series", null).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "product", null, marketEvaluationPoint.Series.Product).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "quantity_Measure_Unit.name", null, marketEvaluationPoint.Series.QuantityMeasureUnit).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "meter.mRID", null, marketEvaluationPoint.MeterId).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", "Series", null).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "product", null, marketEvaluationPoint.Series.Product).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "quantity_Measure_Unit.name", null, marketEvaluationPoint.Series.QuantityMeasureUnit).ConfigureAwait(false);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
         await WriteMridAsync("energySupplier_MarketParticipant.mRID", marketEvaluationPoint.EnergySupplier, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "supplyStart_DateAndOrTime.dateTime", null, marketEvaluationPoint.SupplyStart.ToString()).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "description", null, marketEvaluationPoint.Description).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "usagePointLocation.geoInfoReference", null, marketEvaluationPoint.GeoInfoReference).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "supplyStart_DateAndOrTime.dateTime", null, marketEvaluationPoint.SupplyStart.ToString()).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "description", null, marketEvaluationPoint.Description).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "usagePointLocation.geoInfoReference", null, marketEvaluationPoint.GeoInfoReference).ConfigureAwait(false);
         await WriteAddressAsync(marketEvaluationPoint.MainAddress, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "usagePointLocation.actualAddressIndicator", null, marketEvaluationPoint.IsActualAddress).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "usagePointLocation.actualAddressIndicator", null, marketEvaluationPoint.IsActualAddress).ConfigureAwait(false);
         await WriteRelatedMarketEvaluationPointAsync(marketEvaluationPoint.ParentMarketEvaluationPoint, "Parent_MarketEvaluationPoint", writer).ConfigureAwait(false);
         await WriteRelatedMarketEvaluationPointAsync(marketEvaluationPoint.ChildMarketEvaluationPoint, "Child_MarketEvaluationPoint", writer).ConfigureAwait(false);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
@@ -87,7 +89,7 @@ public class AccountingPointCharacteristicsDocumentWriter : DocumentWriter
 
     private static async Task WriteUnitValueAsync(string localName, UnitValue unitvalue, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, localName, null).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", localName, null).ConfigureAwait(false);
         await writer.WriteAttributeStringAsync(null, "unit", null, unitvalue.Unit).ConfigureAwait(false);
         writer.WriteValue(unitvalue.Value);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
@@ -95,7 +97,7 @@ public class AccountingPointCharacteristicsDocumentWriter : DocumentWriter
 
     private static async Task WriteMridAsync(string localName, Mrid mrid, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, localName, null).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", localName, null).ConfigureAwait(false);
         await writer.WriteAttributeStringAsync(null, "codingScheme", null, mrid.CodingScheme).ConfigureAwait(false);
         writer.WriteValue(mrid.Id);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
@@ -103,39 +105,39 @@ public class AccountingPointCharacteristicsDocumentWriter : DocumentWriter
 
     private static async Task WriteRelatedMarketEvaluationPointAsync(RelatedMarketEvaluationPoint childMktEvaluationPoint, string localName, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, localName, null).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", localName, null).ConfigureAwait(false);
         await WriteMridAsync("mRID", childMktEvaluationPoint.Id, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "description", null, childMktEvaluationPoint.Description).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "description", null, childMktEvaluationPoint.Description).ConfigureAwait(false);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
     }
 
     private static async Task WriteAddressAsync(Address address, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, "usagePointLocation.mainAddress", null).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", "usagePointLocation.mainAddress", null).ConfigureAwait(false);
         await WriteStreetDetailsAsync(address.Street, writer).ConfigureAwait(false);
         await WriteTownDetailsAsync(address.Town, writer).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "postalCode", null, address.PostalCode).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "postalCode", null, address.PostalCode).ConfigureAwait(false);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
     }
 
     private static async Task WriteStreetDetailsAsync(StreetDetail street, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, "streetDetail", null).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "code", null, street.Code).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "name", null, street.Name).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "number", null, street.Number).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "floorIdentification", null, street.FloorIdentification).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "suiteNumber", null, street.SuiteNumber).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", "streetDetail", null).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "code", null, street.Code).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "name", null, street.Name).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "number", null, street.Number).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "floorIdentification", null, street.FloorIdentification).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "suiteNumber", null, street.SuiteNumber).ConfigureAwait(false);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
     }
 
     private static async Task WriteTownDetailsAsync(TownDetail town, XmlWriter writer)
     {
-        await writer.WriteStartElementAsync(Prefix, "townDetail", null).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "code", null, town.Code).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "name", null, town.Name).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "section", null, town.Section).ConfigureAwait(false);
-        await writer.WriteElementStringAsync(Prefix, "country", null, town.Country).ConfigureAwait(false);
+        await writer.WriteStartElementAsync("cim", "townDetail", null).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "code", null, town.Code).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "name", null, town.Name).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "section", null, town.Section).ConfigureAwait(false);
+        await writer.WriteElementStringAsync("cim", "country", null, town.Country).ConfigureAwait(false);
         await writer.WriteEndElementAsync().ConfigureAwait(false);
     }
 }
