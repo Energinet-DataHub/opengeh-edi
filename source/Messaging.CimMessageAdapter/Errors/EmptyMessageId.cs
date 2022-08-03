956c714ac22f646ce18960cb0303dc499e7e79df
Ib Kunz Mathiesen <kunzmathiesen@hotmail.com>
2022-08-03 11:23:58 +0200

    Throw error when transaction or messageid is empty

---
+++ source/Messaging.CimMessageAdapter/Errors/EmptyMessageId.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Messaging.CimMessageAdapter.Errors;
+
+public class EmptyMessageId : ValidationError
+{
+    public EmptyMessageId()
+        : base($"Message id cannot be empty", "B2B-003")
+    {
+    }
+}
---
+++ source/Messaging.CimMessageAdapter/Errors/EmptyTransactionId.cs
@@ -0,0 +1,11 @@
+﻿using JetBrains.Annotations;
+
+namespace Messaging.CimMessageAdapter.Errors;
+
+public class EmptyTransactionId : ValidationError
+{
+    public EmptyTransactionId([NotNull] string message)
+        : base($"Transaction id may not be empty", "B2B-005")
+    {
+    }
+}
