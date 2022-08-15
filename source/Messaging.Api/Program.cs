8e3e4a63d3f5fe026132fa2376e3b6c012bffae7
Chris Brohus <CBS@energinet.dk>
2022-08-15 12:40:05 +0200

    Update NUGet package and call middleware

--- source/Messaging.Api/Program.cs
+++ source/Messaging.Api/Program.cs
@@ -65,6 +65,7 @@ namespace Messaging.Api
                 .ConfigureFunctionsWorkerDefaults(worker =>
                 {
                     worker.UseMiddleware<CorrelationIdMiddleware>();
+                    worker.UseMiddleware<RequestResponseLoggingMiddleware>();
                     worker.UseMiddleware<BearerAuthenticationMiddleware>();
                     worker.UseMiddleware<ClaimsEnrichmentMiddleware>();
                     worker.UseMiddleware<MarketActorAuthenticatorMiddleware>();
