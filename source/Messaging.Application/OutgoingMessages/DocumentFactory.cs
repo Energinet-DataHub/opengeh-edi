f55b0c48682ad7c86ffb2f9a526d8ce1bd42d19c
Chris Brohus <CBS@energinet.dk>
2022-08-18 13:16:05 +0200

    Added JSON schema for ConfirmRequestChangeOfSupplier Changed DocumentFactory to use interface instead of abstract class

--- source/Messaging.Application/OutgoingMessages/DocumentFactory.cs
+++ source/Messaging.Application/OutgoingMessages/DocumentFactory.cs
@@ -24,9 +24,9 @@ namespace Messaging.Application.OutgoingMessages;
 
 public class DocumentFactory
 {
-    private readonly IReadOnlyCollection<DocumentWriter> _documentWriters;
+    private readonly IReadOnlyCollection<IDocumentWriter> _documentWriters;
 
-    public DocumentFactory(IEnumerable<DocumentWriter> documentWriters)
+    public DocumentFactory(IEnumerable<IDocumentWriter> documentWriters)
     {
         _documentWriters = documentWriters.ToList();
     }
