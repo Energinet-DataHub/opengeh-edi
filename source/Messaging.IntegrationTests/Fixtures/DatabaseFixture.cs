773b3f9889a906b20ac6b17ef6a44d7fc5c97f69
Kristian F. Thomsen <kft@energinet.dk>
2022-08-19 11:06:37 +0200

    Cleanup table

--- source/Messaging.IntegrationTests/Fixtures/DatabaseFixture.cs
+++ source/Messaging.IntegrationTests/Fixtures/DatabaseFixture.cs
@@ -58,6 +58,7 @@ namespace Messaging.IntegrationTests.Fixtures
                 $"DELETE FROM [b2b].[TransactionIds]" +
                 $"DELETE FROM [b2b].[OutgoingMessages] " +
                 $"DELETE FROM [b2b].[ReasonTranslations] " +
+                $"DELETE FROM [b2b].[QueuedInternalCommands] " +
                 $"DELETE FROM [b2b].[MarketEvaluationPoints]";
 
             _context.Database.ExecuteSqlRaw(cleanupStatement);
