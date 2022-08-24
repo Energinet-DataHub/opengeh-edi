cfe29362895fa421680e25f31939c58e16f09d20
Ib Kunz Mathiesen <kunzmathiesen@hotmail.com>
2022-08-24 21:52:40 +0200

    Implement request customer master data

---
+++ source/Messaging.Infrastructure/Transactions/MoveIn/RequestCustomerMasterData.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using Messaging.Application.Transactions.MoveIn;
+
+namespace Messaging.Infrastructure.Transactions.MoveIn;
+
+public class RequestCustomerMasterData : IRequestCustomerMasterData
+{
+    private readonly IRequestDispatcher<FetchCustomerMasterData> _requestDispatcher;
+
+    public RequestCustomerMasterData(IRequestDispatcher<FetchCustomerMasterData> requestDispatcher)
+    {
+        _requestDispatcher = requestDispatcher;
+    }
+
+    public async Task RequestMasterDataForAsync(FetchCustomerMasterData fetchMeteringPointMasterData)
+    {
+        await _requestDispatcher.SendAsync(fetchMeteringPointMasterData).ConfigureAwait(false);
+    }
+}
