2175dc0b1ef3d6ced7ede69b8c9fc8ceec1a8b09
Kristian F. Thomsen <kft@energinet.dk>
2022-09-01 14:14:04 +0200

    Remove comment out code

--- source/Messaging.CimMessageAdapter/Messages/MessageParserResult.cs
+++ source/Messaging.CimMessageAdapter/Messages/MessageParserResult.cs
@@ -19,14 +19,16 @@ using Messaging.CimMessageAdapter.Errors;
 namespace Messaging.CimMessageAdapter.Messages
 {
     public class MessageParserResult<TMarketActivityRecordType>
-     where TMarketActivityRecordType : IMarketActivityRecord
+        where TMarketActivityRecordType : IMarketActivityRecord
     {
         public MessageParserResult(params ValidationError[] errors)
         {
             Errors = errors;
         }
 
-        public MessageParserResult(MessageHeader messageHeader, IReadOnlyCollection<TMarketActivityRecordType> marketActivityRecords)
+        public MessageParserResult(
+            MessageHeader messageHeader,
+            IReadOnlyCollection<TMarketActivityRecordType> marketActivityRecords)
         {
             MessageHeader = messageHeader;
             MarketActivityRecords = marketActivityRecords;
@@ -40,34 +42,5 @@ namespace Messaging.CimMessageAdapter.Messages
 
         public IReadOnlyCollection<TMarketActivityRecordType> MarketActivityRecords { get; } =
             new List<TMarketActivityRecordType>();
-        // public static MessageParserResult Failure(params ValidationError[] errors)
-        // {
-        //     return new MessageParserResult(errors);
-        // }
-        //
-        // public static MessageParserResult Succeeded(MessageHeader messageHeader, IReadOnlyCollection<MarketActivityRecord> marketActivityRecords)
-        // {
-        //     return new MessageParserResult(messageHeader, marketActivityRecords);
-        // }
     }
-
-    // #pragma warning disable
-    // public class MessageParserResult<TMarketActivityRecordType>
-    // {
-    //     private MessageParserResult(MessageHeader header, IReadOnlyCollection<TMarketActivityRecordType> marketActivityRecords)
-    //     {
-    //         Header = header;
-    //         MarketActivityRecords = marketActivityRecords;
-    //     }
-    //
-    //     public MessageHeader Header { get; }
-    //
-    //     public IReadOnlyCollection<TMarketActivityRecordType> MarketActivityRecords { get; }
-    //
-    //
-    //     public MessageParserResult<TMarketActivityRecordType> Succeeded(MessageHeader header, IReadOnlyCollection<TMarketActivityRecordType> marketActivityRecords)
-    //     {
-    //         return new MessageParserResult<TMarketActivityRecordType>(header, marketActivityRecords);
-    //     }
-    // }
 }
