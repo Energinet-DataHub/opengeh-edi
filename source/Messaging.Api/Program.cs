73eb71ffdddfe92088735b3cf002d32b1f18da84
Daniel Pethjen <daniel@pethjen.dk>
2022-09-16 13:57:52 +0200

    Extend healthchecks with new queues

--- source/Messaging.Api/Program.cs
+++ source/Messaging.Api/Program.cs
@@ -139,7 +139,9 @@ namespace Messaging.Api
                     services.AddInternalDomainServiceBusQueuesHealthCheck(
                         runtime.INCOMING_MESSAGE_QUEUE_MANAGE_CONNECTION_STRING!,
                         runtime.INCOMING_MESSAGE_QUEUE_NAME!,
-                        runtime.MESSAGE_REQUEST_QUEUE!);
+                        runtime.MESSAGE_REQUEST_QUEUE!,
+                        runtime.CUSTOMER_MASTER_DATA_RESPONSE_QUEUE_NAME!,
+                        runtime.CUSTOMER_MASTER_DATA_REQUEST_QUEUE_NAME!);
                     services.AddSqlServerHealthCheck(runtime.DB_CONNECTION_STRING!);
                 })
                 .Build();
