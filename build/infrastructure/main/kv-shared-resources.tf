f16a1850fc2b80d62f8e3102a037176379b81a26
Daniel Pethjen <daniel@pethjen.dk>
2022-09-19 10:50:52 +0200

    Update subscription TF

--- build/infrastructure/main/kv-shared-resources.tf
+++ build/infrastructure/main/kv-shared-resources.tf
@@ -101,3 +101,8 @@ data "azurerm_key_vault_secret" "mssql_data_elastic_pool_id" {
   name         = "mssql-data-elastic-pool-id"
   key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
 }
+
+data "azurerm_key_vault_secret" "sbt_domainrelay_integrationevent_received_name_id" {
+  name         = "sbt-sharedres-integrationevent-received-id"
+  key_vault_id = data.azurerm_key_vault.kv_shared_resources.id
+}
