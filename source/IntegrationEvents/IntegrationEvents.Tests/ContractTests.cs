d5d839f978acfbd515c8bd132aa1e3948a3b9063
Kristian F. Thomsen <kft@energinet.dk>
2022-09-21 09:42:16 +0200

    Ensure integration events define an identifier

---
+++ source/IntegrationEvents/IntegrationEvents.Tests/ContractTests.cs
@@ -0,0 +1,24 @@
+﻿using Energinet.DataHub.EnergySupplying.IntegrationEvents;
+using Google.Protobuf;
+
+namespace IntegrationEvents.Tests;
+
+public class ContractTests
+{
+    [Theory(DisplayName = nameof(Must_define_an_identifier))]
+    [MemberData(nameof(GetAllIntegrationEvents))]
+    public void Must_define_an_identifier(Type integrationEvent)
+    {
+        var hasIdField = integrationEvent?.GetField("IdFieldNumber") is not null;
+        Assert.True(hasIdField);
+    }
+
+    private static IEnumerable<object[]> GetAllIntegrationEvents()
+    {
+        return typeof(ConsumerMovedIn)
+            .Assembly
+            .GetTypes()
+            .Where(type => type.GetInterfaces().Contains(typeof(IMessage)))
+            .Select(integrationEvent => new[] { integrationEvent });
+    }
+}
