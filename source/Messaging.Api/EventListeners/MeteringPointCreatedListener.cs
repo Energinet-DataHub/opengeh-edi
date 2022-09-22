11ae922f3e2402668a518420723b777823b202b9
Chris Brohus <CBS@energinet.dk>
2022-09-22 14:32:18 +0200

    Added MeteringPointCreatedListener in B2B

---
+++ source/Messaging.Api/EventListeners/MeteringPointCreatedListener.cs
@@ -0,0 +1,41 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using Messaging.Infrastructure.Configuration.InternalCommands;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace Messaging.Api.EventListeners;
+
+public class MeteringPointCreatedListener
+{
+    private readonly CommandSchedulerFacade _commandSchedulerFacade;
+    private readonly ILogger<MeteringPointCreatedListener> _logger;
+
+    public MeteringPointCreatedListener(CommandSchedulerFacade commandSchedulerFacade, ILogger<MeteringPointCreatedListener> logger)
+    {
+        _commandSchedulerFacade = commandSchedulerFacade;
+        _logger = logger;
+    }
+
+    [Function("MeteringPointCreatedListener")]
+    public void Run(
+        [ServiceBusTrigger("%INTEGRATION_EVENT_TOPIC_NAME%", "%METERING_POINT_CREATED_EVENT_B2B_SUBSCRIPTION_NAME%", Connection = "SERVICE_BUS_CONNECTION_STRING_FOR_INTEGRATION_EVENTS_LISTENER")] byte[] data,
+        FunctionContext context)
+    {
+        if (context == null) throw new ArgumentNullException(nameof(context));
+        _logger.LogInformation($"Received MeteringPointCreated integration event in B2B");
+    }
+}
