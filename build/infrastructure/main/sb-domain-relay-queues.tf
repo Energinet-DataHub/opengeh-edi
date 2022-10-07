e05eff97451555cfa232cb664ad18f07d1fda1db
Daniel Pethjen <daniel@pethjen.dk>
2022-10-07 10:44:46 +0200

    Create new queue for req-change of acc point chars

---
+++ build/infrastructure/main/sb-domain-relay-queues.tf
@@ -0,0 +1,31 @@
+# Copyright 2020 Energinet DataHub A/S
+#
+# Licensed under the Apache License, Version 2.0 (the "License2");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Queue to request change of accounting point characteristics transactions
+module "sbq_req_change_acc_point_characteristics" {
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-queue?ref=7.0.0"
+
+  name                = "req-change-of-accounting-point-characteristics"
+  namespace_id        = data.sb_domain_relay_namespace_id.value
+}
+
+module "kvs_sbq_req_change_acc_point_characteristics" {
+  source        = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/key-vault-secret?ref=7.0.0"
+
+  name          = "sbq-req-change-of-acc-point-characteristics"
+  value         = module.sbq_req_change_acc_point_characteristics.name
+  key_vault_id  = data.azurerm_key_vault.kv_shared_resources.id
+
+  tags          = azurerm_resource_group.this.tags
+}
