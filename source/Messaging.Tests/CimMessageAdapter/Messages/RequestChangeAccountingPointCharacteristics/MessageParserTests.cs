33658ecfb7ce34549b96c6621ac31ea9d90b9abf
Daniel Pethjen <daniel@pethjen.dk>
2022-10-11 13:45:29 +0200

    Invalid and unknown format

--- source/Messaging.Tests/CimMessageAdapter/Messages/RequestChangeAccountingPointCharacteristics/MessageParserTests.cs
+++ source/Messaging.Tests/CimMessageAdapter/Messages/RequestChangeAccountingPointCharacteristics/MessageParserTests.cs
@@ -12,12 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 using Messaging.Application.IncomingMessages.RequestChangeAccountPointCharacteristics;
+using Messaging.CimMessageAdapter.Errors;
 using Messaging.CimMessageAdapter.Messages;
 using Messaging.CimMessageAdapter.Messages.RequestChangeAccountingPointCharacteristics;
 using Messaging.Domain.OutgoingMessages;
@@ -57,6 +59,14 @@ public class MessageParserTests
         };
     }
 
+    public static IEnumerable<object[]> CreateMessagesWithInvalidStructure()
+    {
+        return new List<object[]>
+        {
+            new object[] { CimFormat.Xml, CreateInvalidXmlMessage() },
+        };
+    }
+
     [Theory]
     [MemberData(nameof(CreateMessages))]
     public async Task Can_parse(CimFormat format, Stream message)
@@ -84,6 +94,24 @@ public class MessageParserTests
         Assert.Equal("578888883331812345", record2?.MarketEvaluationPoint.GsrnNumber);
     }
 
+    [Theory]
+    [MemberData(nameof(CreateMessagesWithInvalidStructure))]
+    public async Task Return_error_when_structure_is_invalid(CimFormat format, Stream message)
+    {
+        var result = await _messageParser.ParseAsync(message, format).ConfigureAwait(false);
+
+        Assert.False(result.Success);
+        Assert.Contains(result.Errors, error => error is InvalidMessageStructure);
+    }
+
+    [Fact]
+    public async Task Throw_if_message_format_is_not_known()
+    {
+        var parser = new MessageParser(new List<IMessageParser<MarketActivityRecord, RequestChangeAccountingPointCharacteristicsTransaction>>());
+
+        await Assert.ThrowsAsync<InvalidOperationException>(() => parser.ParseAsync(CreateXmlMessage(), CimFormat.Xml)).ConfigureAwait(false);
+    }
+
     private static Stream CreateXmlMessage()
     {
         var xmlDoc = XDocument.Load($"cimmessageadapter{Path.DirectorySeparatorChar}messages{Path.DirectorySeparatorChar}xml{Path.DirectorySeparatorChar}RequestChangeAccountingPointCharacteristics.xml");
@@ -100,6 +128,18 @@ public class MessageParserTests
         xmlDoc.Save(stream);
 
         return stream;
+    }
+
+    private static Stream CreateInvalidXmlMessage()
+    {
+        var messageStream = new MemoryStream();
+        using var writer = new StreamWriter(messageStream);
+        writer.Write("This is not XML");
+        writer.Flush();
+        messageStream.Position = 0;
+        var returnStream = new MemoryStream();
+        messageStream.CopyTo(returnStream);
+        return returnStream;
     }
 
     private static void AssertMarketActivityRecord(MarketActivityRecord? marketActivityRecord)
