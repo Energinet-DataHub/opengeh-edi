8f229759168a7ab82c95d7c913ed29daf978ca8d
Kristian F. Thomsen <kft@energinet.dk>
2022-10-14 10:26:54 +0200

    Add confirm request change of supplier message

--- source/Messaging.Infrastructure/Configuration/DataAccess/Outgoing/OutgoingMessageEntityConfiguration.cs
+++ source/Messaging.Infrastructure/Configuration/DataAccess/Outgoing/OutgoingMessageEntityConfiguration.cs
@@ -15,6 +15,7 @@
 using System;
 using Messaging.Domain.Actors;
 using Messaging.Domain.OutgoingMessages;
+using Messaging.Domain.OutgoingMessages.ConfirmRequestChangeOfSupplier;
 using Messaging.Domain.SeedWork;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
@@ -53,6 +54,12 @@ namespace Messaging.Infrastructure.Configuration.DataAccess.Outgoing
                     toDbValue => toDbValue.ToString(),
                     fromDbValue => EnumerationType.FromName<MarketRole>(fromDbValue));
             builder.Property(x => x.MarketActivityRecordPayload);
+
+            builder
+                .HasDiscriminator<string>("Discriminator")
+                .HasValue<OutgoingMessage>(nameof(OutgoingMessage))
+                .HasValue<ConfirmRequestChangeOfSupplierMessage>(DocumentType.ConfirmRequestChangeOfSupplier.Name)
+                .IsComplete(false);
         }
     }
 }
