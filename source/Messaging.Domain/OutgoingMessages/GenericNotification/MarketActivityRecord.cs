46cbc332a5f56987aa5915c4b000d3fb49a6b63c
Kristian F. Thomsen <kft@energinet.dk>
2022-10-14 09:47:16 +0200

    Move to domain layer

--- source/Messaging.Domain/OutgoingMessages/GenericNotification/MarketActivityRecord.cs
+++ source/Messaging.Domain/OutgoingMessages/GenericNotification/MarketActivityRecord.cs
@@ -12,10 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using NodaTime;
 
-namespace Messaging.Application.OutgoingMessages.GenericNotification
+namespace Messaging.Domain.OutgoingMessages.GenericNotification
 {
     public class MarketActivityRecord
     {
