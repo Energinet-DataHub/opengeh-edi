7bf1ae44704e92692bbbf968f7e92d05f1163f62
Chris Brohus <CBS@energinet.dk>
2022-10-27 10:37:06 +0200

    Added new service bus trigger Added new queue Added environment variable

--- build/infrastructure/main/sb-domain-relay-queues.tf
+++ build/infrastructure/main/sb-domain-relay-queues.tf
@@ -54,3 +54,9 @@ module "kvs_sbq_create_metering_point_transactions" {
 
   tags          = azurerm_resource_group.this.tags
 }
+
+module "sbq_customermasterdataupdatequeue" {
+  source              = "git::https://github.com/Energinet-DataHub/geh-terraform-modules.git//azure/service-bus-queue?ref=7.0.0"
+  name                = "customermasterdataupdatequeue"
+  namespace_id        = data.azurerm_key_vault_secret.sb_domain_relay_namespace_id.value
+}
