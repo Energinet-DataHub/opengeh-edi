ae48760778dd5aa49159886d9f048efea1adcfb4
Kristian Bank Erbou <contact@kbeconsult.dk>
2022-11-08 17:17:37 +0100

    Change from spn_name to deployagent_names

--- build/infrastructure/main/mssqldb-applicationaccess.tf
+++ build/infrastructure/main/mssqldb-applicationaccess.tf
@@ -21,8 +21,7 @@ module "mssql_database_application_access" {
                               module.func_receiver.name,
                               module.func_processing.name,
                             ]
-  spn_name                = "sp-rg-kv-sp-subscr-green-DataHub-U-002-contributor-U-d252b8da-contributor"
-
+  deployagent_names       = data.azurerm_virtual_machine.deployagent.*.name
   depends_on              = [
                               module.func_actor_sync.name,
                               module.func_receiver.name,
