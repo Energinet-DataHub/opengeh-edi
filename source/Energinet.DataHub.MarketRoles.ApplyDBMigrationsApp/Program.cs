500a2e8385a0ba9557627b02478a12a28d21cc3d
Kristian Bank Erbou <contact@kbeconsult.dk>
2022-11-11 08:39:52 +0100

    Remove Console.Writeline

--- source/Energinet.DataHub.MarketRoles.ApplyDBMigrationsApp/Program.cs
+++ source/Energinet.DataHub.MarketRoles.ApplyDBMigrationsApp/Program.cs
@@ -25,8 +25,7 @@ namespace Energinet.DataHub.MarketRoles.ApplyDBMigrationsApp
             var connectionString = ConnectionStringFactory.GetConnectionString(args);
             var filter = EnvironmentFilter.GetFilter(args);
             var isDryRun = args.Contains("dryRun");
-
-            Console.WriteLine($"Args have been resolved to: dryRun = ${isDryRun}");
+            
             var upgrader = UpgradeFactory.GetUpgradeEngine(connectionString, filter, isDryRun);
 
             var result = upgrader.PerformUpgrade();
