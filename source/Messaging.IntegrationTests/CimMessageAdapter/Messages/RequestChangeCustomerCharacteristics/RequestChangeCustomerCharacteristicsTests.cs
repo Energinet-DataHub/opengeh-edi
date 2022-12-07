92b669d2441bf6caf23aaec48acde3b3e4cddee0
Kristian F. Thomsen <kft@energinet.dk>
2022-12-07 21:20:32 +0100

    Remove unused method

--- source/Messaging.IntegrationTests/CimMessageAdapter/Messages/RequestChangeCustomerCharacteristics/RequestChangeCustomerCharacteristicsTests.cs
+++ source/Messaging.IntegrationTests/CimMessageAdapter/Messages/RequestChangeCustomerCharacteristics/RequestChangeCustomerCharacteristicsTests.cs
@@ -130,7 +130,6 @@ public class RequestChangeCustomerCharacteristicsTests : TestBase, IAsyncLifetim
     [Fact]
     public async Task Sender_id_must_match_the_organization_of_the_current_authenticated_user()
     {
-        await _marketActorAuthenticator.AuthenticateAsync(CreateIdentity("1234567890123"));
         await using var message = BusinessMessageBuilder
             .RequestChangeCustomerCharacteristics()
             .Message();
@@ -248,14 +247,6 @@ public class RequestChangeCustomerCharacteristicsTests : TestBase, IAsyncLifetim
     private ClaimsPrincipal CreateIdentity()
     {
         return new ClaimsPrincipal(new ClaimsIdentity(_claims));
-    }
-
-    private ClaimsPrincipal CreateIdentity(string actorIdentifier)
-    {
-        var claims = _claims.ToList();
-        claims.Remove(claims.Find(claim => claim.Type.Equals("actorid", StringComparison.OrdinalIgnoreCase))!);
-        claims.Add(new Claim("actorid", actorIdentifier));
-        return new ClaimsPrincipal(new ClaimsIdentity(claims));
     }
 
     private ClaimsPrincipal CreateIdentityWithoutRoles()
