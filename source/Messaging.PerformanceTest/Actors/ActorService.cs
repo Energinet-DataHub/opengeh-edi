aef26395d897dfa770b5028cc81f6f1819d6e53c
Stephan Raaschou <srn@broelstaerk.dk>
2022-12-13 15:10:44 +0100

    Initial peek/dequeue performance test service with option to get a unique actornumber with an http-get:

---
+++ source/Messaging.PerformanceTest/Actors/ActorService.cs
@@ -0,0 +1,60 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Concurrent;
+
+namespace Messaging.PerformanceTest.Actors;
+
+public class ActorService : IActorService
+{
+    private readonly object _actorNumberLock = new();
+    private readonly ConcurrentDictionary<string, bool> _actorNumberDictionary;
+
+    public ActorService()
+    {
+        _actorNumberDictionary = CreateActorNumbers();
+    }
+
+    public string? GetUniqueActorNumber()
+    {
+        lock (_actorNumberLock)
+        {
+            var actorNumber = _actorNumberDictionary.FirstOrDefault(x => x.Value == false).Key;
+            if (actorNumber == null)
+                return null;
+            _actorNumberDictionary[actorNumber] = true;
+            return actorNumber;
+        }
+    }
+
+    public int GetActorCount()
+    {
+        lock (_actorNumberLock)
+        {
+            return _actorNumberDictionary.Count;
+        }
+    }
+
+    private static ConcurrentDictionary<string, bool> CreateActorNumbers()
+    {
+        var actorNumberDictionary = new ConcurrentDictionary<string, bool>();
+        actorNumberDictionary.AddOrUpdate("45X000000000099K", false, (_, _) => false);
+        actorNumberDictionary.AddOrUpdate("7080010006509", false, (_, _) => false);
+        actorNumberDictionary.AddOrUpdate("5790002597695", false, (_, _) => false);
+        actorNumberDictionary.AddOrUpdate("7080005010788", false, (_, _) => false);
+        actorNumberDictionary.AddOrUpdate("5790002602245", false, (_, _) => false);
+        actorNumberDictionary.AddOrUpdate("5790001108212", false, (_, _) => false);
+        return actorNumberDictionary;
+    }
+}
---
+++ source/Messaging.PerformanceTest/Controllers/PeekDequeuePerformanceTestController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections.Concurrent;
+using Messaging.PerformanceTest.Actors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Messaging.PerformanceTest.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class PeekDequeuePerformanceTestController : ControllerBase
+{
+    private readonly ILogger<PeekDequeuePerformanceTestController> _logger;
+    private readonly IActorService _actorService;
+    private readonly IMoveInService _moveInService;
+
+    public PeekDequeuePerformanceTestController(
+        ILogger<PeekDequeuePerformanceTestController> logger,
+        IActorService actorService,
+        IMoveInService moveInService)
+    {
+        _logger = logger;
+        _actorService = actorService;
+        _moveInService = moveInService;
+    }
+
+    [HttpGet(Name = "GetUniqueActorNumber")]
+    public string? Get()
+    {
+        return _actorService.GetUniqueActorNumber();
+    }
+
+    [HttpPost(Name = "GenerateTestData")]
+    public void Post()
+    {
+         for (int i = 0; i < _actorService.GetActorCount(); i++)
+         {
+             var uniqueActorNumber = _actorService.GetUniqueActorNumber();
+
+             for (int j = 0; j < 10; j++)
+             {
+                 _moveInService.MoveIn(uniqueActorNumber);
+             }
+         }
+    }
+}
+
+/// <summary>
+/// Service for triggering a move in.
+/// </summary>
+public interface IMoveInService
+{
+    /// <summary>
+    /// Request a move in for a given actor.
+    /// </summary>
+    /// <param name="uniqueActorNumber"></param>
+    Task MoveInAsync(string? uniqueActorNumber);
+}
+
+internal class MoveInService : IMoveInService, IDisposable
+{
+    private readonly HttpClient _httpClient;
+
+    public MoveInService()
+    {
+        _httpClient = new HttpClient();
+    }
+
+    public async Task MoveInAsync(string? uniqueActorNumber)
+    {
+        await _httpClient.PostAsync(new Uri($"http://localhost:7071/api/MoveIn/{uniqueActorNumber}"), null).ConfigureAwait(false);
+    }
+
+    public void Dispose()
+    {
+        _httpClient.Dispose();
+    }
+}
