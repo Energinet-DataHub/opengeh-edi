8372ea53d733dac3c586426ac1f64d7ba7d956a3
Stephan Raaschou <srn@broelstaerk.dk>
2023-01-02 14:52:01 +0100

    Added powershell to create azure resources needed when running locally

---
+++ source/Messaging.Api/setup/setupazure.ps1
@@ -0,0 +1,16 @@
+
+param ($username, $subscription)
+. "./azure-resource-create-functions.ps1"
+
+Write-Host "Username = $username, Subscription = $subscription"
+
+$resourceGroupName = "rg-batman"
+$serviceBusNamespaceName = "servicebus-localtest-batman"
+
+az account set --name $subscription
+
+CreateQueue -queueName $username"-sbq-incoming_change_of_supplier_message_queue" -serviceBusNamespaceName $serviceBusNamespaceName
+
+CreateTopic -topicName $username"-sbt-integration_event_topic" -serviceBusNamespaceName $serviceBusNamespaceName
+
+CreateTopicSubscription -topicName $username"-sbt-integration_event_topic" -topicSubscriptionName $username"-sbts-integration_event_subscription" -serviceBusNamespaceName $serviceBusNamespaceName
