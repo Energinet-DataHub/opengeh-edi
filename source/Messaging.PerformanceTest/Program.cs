d01fef96871e290b0630bebe75269d9d30b78645
Stephan Raaschou <srn@broelstaerk.dk>
2023-01-10 13:53:24 +0100

    Enable Swagger - not only when running locally

--- source/Messaging.PerformanceTest/Program.cs
+++ source/Messaging.PerformanceTest/Program.cs
@@ -36,11 +36,9 @@ builder.Services.AddHealthChecks();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+
+app.UseSwaggerUI();
 
 app.UseAuthorization();
 
