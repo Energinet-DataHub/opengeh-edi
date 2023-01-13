71f8f172d0483ee76c4f15a9288c6dab733d5e20
Stephan Raaschou <srn@broelstaerk.dk>
2023-01-13 19:18:14 +0100

    Update source/Messaging.Api/setup/setupazure.ps1

--- source/Messaging.Api/setup/setupazure.ps1
+++ source/Messaging.Api/setup/setupazure.ps1
@@ -14,3 +14,8 @@ CreateQueue -queueName $username"-sbq-incoming_change_of_supplier_message_queue"
 CreateTopic -topicName $username"-sbt-integration_event_topic" -serviceBusNamespaceName $serviceBusNamespaceName
 
 CreateTopicSubscription -topicName $username"-sbt-integration_event_topic" -topicSubscriptionName $username"-sbts-integration_event_subscription" -serviceBusNamespaceName $serviceBusNamespaceName
+
+CreateTopicSubscription -topicName $username"-sbt-integration_event_topic" -topicSubscriptionName $username"-sbts-market_participant_changed_actor_created" -serviceBusNamespaceName $serviceBusNamespaceName
+
+CreateTopicSubscription -topicName $username"-sbt-integration_event_topic" -topicSubscriptionName $username"-sbts-balance_result_available_event" -serviceBusNamespaceName $serviceBusNamespaceName
+
