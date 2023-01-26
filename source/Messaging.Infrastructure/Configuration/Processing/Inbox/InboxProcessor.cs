7ece8c079e18f9598e8a41a70d30137bbade86e7
Kristian F. Thomsen <kft@energinet.dk>
2023-01-26 13:57:08 +0100

    Move to a new home

---
+++ source/Messaging.Infrastructure/Configuration/Processing/Inbox/InboxProcessor.cs
@@ -0,0 +1,88 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper;
+using MediatR;
+using Messaging.Application.Configuration;
+using Messaging.Application.Configuration.DataAccess;
+using Messaging.Infrastructure.Configuration.MessageBus;
+
+namespace Messaging.Infrastructure.Configuration.Processing.Inbox;
+
+public class InboxProcessor
+{
+    private readonly IDatabaseConnectionFactory _connectionFactory;
+    private readonly IMediator _mediator;
+    private readonly ISystemDateTimeProvider _dateTimeProvider;
+    private readonly List<IIntegrationEventMapper> _mappers;
+
+    public InboxProcessor(IDatabaseConnectionFactory connectionFactory, IMediator mediator, ISystemDateTimeProvider dateTimeProvider, IEnumerable<IIntegrationEventMapper> mappers)
+    {
+        _connectionFactory = connectionFactory;
+        _mediator = mediator;
+        _dateTimeProvider = dateTimeProvider;
+        _mappers = mappers.ToList();
+    }
+
+    public async Task ProcessMessagesAsync()
+    {
+        var messages = await FindPendingMessagesAsync().ConfigureAwait(false);
+
+        foreach (var message in messages)
+        {
+            await _mediator.Publish(
+                    MapperFor(message.EventType)
+                        .MapFrom(message.EventPayload))
+                .ConfigureAwait(false);
+
+            await MarkAsProcessedAsync(message).ConfigureAwait(false);
+        }
+    }
+
+    private async Task MarkAsProcessedAsync(InboxMessage message)
+    {
+        var updateStatement = $"UPDATE b2b.InboxMessages " +
+                              "SET ProcessedDate = @Now " +
+                              "WHERE Id = @Id";
+        using var connection = await _connectionFactory.GetConnectionAndOpenAsync().ConfigureAwait(false);
+        await connection.ExecuteAsync(updateStatement, new
+        {
+            Id = message.Id,
+            Now = _dateTimeProvider.Now().ToDateTimeUtc(),
+        }).ConfigureAwait(false);
+    }
+
+    private async Task<IReadOnlyList<InboxMessage>> FindPendingMessagesAsync()
+    {
+        using var connection = await _connectionFactory.GetConnectionAndOpenAsync().ConfigureAwait(false);
+        var sql = "SELECT " +
+                  $"Id AS {nameof(InboxMessage.Id)}, " +
+                  $"EventType AS {nameof(InboxMessage.EventType)}, " +
+                  $"EventPayload AS {nameof(InboxMessage.EventPayload)} " +
+                  "FROM b2b.InboxMessages " +
+                  "WHERE ProcessedDate IS NULL " +
+                  "ORDER BY OccurredOn";
+
+        var messages = await connection.QueryAsync<InboxMessage>(sql).ConfigureAwait(false);
+        return messages.ToList();
+    }
+
+    private IIntegrationEventMapper MapperFor(string eventType)
+    {
+        return _mappers.First(mapper => mapper.CanHandle(eventType));
+    }
+}
