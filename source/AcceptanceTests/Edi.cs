345a058750dedeb83409c9b22b571c1f88bd7f22
Kristian F. Thomsen <kft@energinet.dk>
2023-02-22 11:12:30 +0100

    Move response check

--- source/AcceptanceTests/Edi.cs
+++ source/AcceptanceTests/Edi.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http.Headers;
+﻿using System.Net;
+using System.Net.Http.Headers;
 using AcceptanceTest.Factories;
 
 namespace AcceptanceTest;
@@ -20,7 +21,8 @@ public class Edi : IDisposable
     public async Task AssertTotalProductionResultIsAvailableAsync(string gridOperatorNumber, DocumentFormat documentFormat)
     {
         var peekResponse = await _driver.PeekAsync(TokenBuilder.ForGridOperator(gridOperatorNumber)).ConfigureAwait(false);
-        peekResponse.EnsureSuccessStatusCode();
+
+        Assert.Equal(HttpStatusCode.OK, peekResponse.StatusCode);
     }
 
     public void Dispose()
@@ -48,11 +50,13 @@ internal class EdiDriver : IDisposable
         _httpClient.BaseAddress = new Uri("http://localhost:7071/api/");
     }
 
-    public Task<HttpResponseMessage> PeekAsync(string token)
+    public async Task<HttpResponseMessage> PeekAsync(string token)
     {
         using var request = new HttpRequestMessage(HttpMethod.Get, "peek/aggregations");
         request.Headers.Authorization = new AuthenticationHeaderValue("bearer", token);
-        return _httpClient.SendAsync(request);
+        var peekResponse = await _httpClient.SendAsync(request).ConfigureAwait(false);
+        peekResponse.EnsureSuccessStatusCode();
+        return peekResponse;
     }
 
     public void Dispose()
