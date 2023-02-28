bd29204a682fe468089f45028e0749eb5e828343
Kristian F. Thomsen <kft@energinet.dk>
2023-02-28 09:08:14 +0100

    Move document schemas and handling to separate package (#64)
    * Add project
    
    * Add package
    
    * Move to a new home
    
    * Remove unused package reference
    
    * Remove unused package reference
    
    * Remove unused package reference
    
    * Remove unused package

--- source/DocumentValidation/JsonSchemaProvider.cs
+++ source/DocumentValidation/JsonSchemaProvider.cs
@@ -12,10 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Threading.Tasks;
 using Json.Schema;
 
-namespace Infrastructure.IncomingMessages.SchemaStore;
+namespace DocumentValidation;
 
 public class JsonSchemaProvider : SchemaProvider
 {
