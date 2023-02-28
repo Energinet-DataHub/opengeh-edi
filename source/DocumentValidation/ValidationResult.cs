3c47b856807fede0bc29bbcdfd72392396f248ea
Kristian F. Thomsen <kft@energinet.dk>
2023-02-28 15:09:47 +0100

    Move to a new home

--- source/DocumentValidation/ValidationResult.cs
+++ source/DocumentValidation/ValidationResult.cs
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-namespace DocumentValidation.Xml
+namespace DocumentValidation
 {
     public class ValidationResult
     {
