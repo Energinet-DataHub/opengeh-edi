f13b00df7bea06ebe0ea00b22b4736f1ad506dfe
Kristian F. Thomsen <kft@energinet.dk>
2023-03-30 12:47:28 +0200

    Add documentation

--- source/Tests/Infrastructure/OutgoingMessages/AggregationResult/IAssertAggregationResultDocument.cs
+++ source/Tests/Infrastructure/OutgoingMessages/AggregationResult/IAssertAggregationResultDocument.cs
@@ -17,24 +17,108 @@ using System.Threading.Tasks;
 
 namespace Tests.Infrastructure.OutgoingMessages.AggregationResult;
 
-#pragma warning disable
+/// <summary>
+/// Assertion helper for aggregation result documents
+/// </summary>
 public interface IAssertAggregationResultDocument
 {
+    /// <summary>
+    /// Asserts message id
+    /// </summary>
+    /// <param name="expectedMessageId"></param>
     IAssertAggregationResultDocument HasMessageId(string expectedMessageId);
+
+    /// <summary>
+    /// Assert sender id
+    /// </summary>
+    /// <param name="expectedSenderId"></param>
     IAssertAggregationResultDocument HasSenderId(string expectedSenderId);
+
+    /// <summary>
+    /// Asserts receiver id
+    /// </summary>
+    /// <param name="expectedReceiverId"></param>
     IAssertAggregationResultDocument HasReceiverId(string expectedReceiverId);
+
+    /// <summary>
+    /// Asserts time stamp
+    /// </summary>
+    /// <param name="expectedTimestamp"></param>
     IAssertAggregationResultDocument HasTimestamp(string expectedTimestamp);
+
+    /// <summary>
+    /// Asserts transaction id
+    /// </summary>
+    /// <param name="expectedTransactionId"></param>
     IAssertAggregationResultDocument HasTransactionId(Guid expectedTransactionId);
+
+    /// <summary>
+    /// Asserts grid area code
+    /// </summary>
+    /// <param name="expectedGridAreaCode"></param>
     IAssertAggregationResultDocument HasGridAreaCode(string expectedGridAreaCode);
+
+    /// <summary>
+    /// Asserts balance supplier number
+    /// </summary>
+    /// <param name="expectedBalanceResponsibleNumber"></param>
     IAssertAggregationResultDocument HasBalanceResponsibleNumber(string expectedBalanceResponsibleNumber);
+
+    /// <summary>
+    /// Asserts energy supplier number
+    /// </summary>
+    /// <param name="expectedEnergySupplierNumber"></param>
     IAssertAggregationResultDocument HasEnergySupplierNumber(string expectedEnergySupplierNumber);
+
+    /// <summary>
+    /// Asserts product code
+    /// </summary>
+    /// <param name="expectedProductCode"></param>
     IAssertAggregationResultDocument HasProductCode(string expectedProductCode);
+
+    /// <summary>
+    /// Asserts period
+    /// </summary>
+    /// <param name="expectedStartOfPeriod"></param>
+    /// <param name="expectedEndOfPeriod"></param>
     IAssertAggregationResultDocument HasPeriod(string expectedStartOfPeriod, string expectedEndOfPeriod);
+
+    /// <summary>
+    /// Asserts a point
+    /// </summary>
+    /// <param name="position"></param>
+    /// <param name="quantity"></param>
     IAssertAggregationResultDocument HasPoint(int position, int quantity);
+
+    /// <summary>
+    /// Asserts document validity
+    /// </summary>
     Task<IAssertAggregationResultDocument> DocumentIsValidAsync();
+
+    /// <summary>
+    /// Asserts the settlement method is not present
+    /// </summary>
     IAssertAggregationResultDocument SettlementMethodIsNotPresent();
+
+    /// <summary>
+    /// Asserts the energy supplier number is not present
+    /// </summary>
     IAssertAggregationResultDocument EnergySupplierNumberIsNotPresent();
+
+    /// <summary>
+    /// Asserts the balance responsible number is not present
+    /// </summary>
     IAssertAggregationResultDocument BalanceResponsibleNumberIsNotPresent();
+
+    /// <summary>
+    /// Asserts the quantity is not present
+    /// </summary>
+    /// <param name="position"></param>
     IAssertAggregationResultDocument QuantityIsNotPresentForPosition(int position);
+
+    /// <summary>
+    /// Asserts the quality is not present
+    /// </summary>
+    /// <param name="position"></param>
     IAssertAggregationResultDocument QualityIsNotPresentForPosition(int position);
 }
