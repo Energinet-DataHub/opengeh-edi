4f2c5720b115e2d2505fbadbc24e76b86ad2f6cf
Kristian F. Thomsen <kft@energinet.dk>
2023-04-11 09:29:30 +0200

    Encapsulate format rules of period dates

--- source/Domain/Transactions/Aggregations/Period.cs
+++ source/Domain/Transactions/Aggregations/Period.cs
@@ -1,5 +1,22 @@
-﻿using NodaTime;
+﻿using System.Globalization;
+using NodaTime;
 
 namespace Domain.Transactions.Aggregations;
 
-public record Period(Instant Start, Instant End);
+public record Period(Instant Start, Instant End)
+{
+    public string StartToString()
+    {
+        return ParsePeriodDateFrom(Start);
+    }
+
+    public string EndToString()
+    {
+        return ParsePeriodDateFrom(End);
+    }
+
+    private static string ParsePeriodDateFrom(Instant instant)
+    {
+        return instant.ToString("yyyy-MM-ddTHH:mm'Z'", CultureInfo.InvariantCulture);
+    }
+}
