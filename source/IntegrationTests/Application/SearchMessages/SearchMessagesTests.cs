5828bc28ee3d5a9521511fac6a7dca4a4f3643b4
Kristian F. Thomsen <kft@energinet.dk>
2023-04-12 13:32:19 +0200

    Can query for archived messages

---
+++ source/IntegrationTests/Application/SearchMessages/SearchMessagesTests.cs
@@ -0,0 +1,79 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Application.Configuration.Queries;
+using IntegrationTests.Fixtures;
+using Xunit;
+
+namespace IntegrationTests.Application.SearchMessages;
+
+public class SearchMessagesTests : TestBase
+{
+    private readonly ArchivedMessageRepository _repository;
+
+    public SearchMessagesTests(DatabaseFixture databaseFixture)
+        : base(databaseFixture)
+    {
+        _repository = new ArchivedMessageRepository();
+    }
+
+    [Fact]
+    public async Task Can_fetch_messages()
+    {
+        var messageId = Guid.NewGuid();
+        _repository.Add(new ArchivedMessage(messageId));
+
+        var result = await InvokeQueryAsync(new GetMessagesQuery()).ConfigureAwait(false);
+
+        Assert.NotNull(result);
+        Assert.Contains(result.Messages, message => message.MessageId == messageId);
+    }
+
+    #pragma warning disable
+    private async Task<MessageSearchResult?> InvokeQueryAsync(GetMessagesQuery query)
+    {
+        var messages = await _repository.GetAllAsync().ConfigureAwait(false);
+        return new MessageSearchResult(messages.Select(message => new MessageInfo(message.MessageId)).ToList().AsReadOnly());
+    }
+}
+
+public record ArchivedMessage(Guid MessageId);
+
+public class ArchivedMessageRepository
+{
+    private List<ArchivedMessage> _messages = new();
+
+    public void Add(ArchivedMessage message)
+    {
+        _messages.Add(message);
+    }
+
+    public Task<ReadOnlyCollection<ArchivedMessage>> GetAllAsync()
+    {
+        return Task.FromResult(_messages.AsReadOnly());
+    }
+}
+
+internal sealed record MessageSearchResult(ReadOnlyCollection<MessageInfo> Messages);
+
+public record MessageInfo(Guid MessageId);
+
+public sealed record GetMessagesQuery : IQuery<MessageSearchResult>
+{
+}
