02966088d7e157a1404aaec7b38901f78818b208
Kristian F. Thomsen <kft@energinet.dk>
2023-05-02 12:07:15 +0200

    Query database direct instead of using write models repository

--- source/Application/SearchMessages/GetMessageQueryHandler.cs
+++ source/Application/SearchMessages/GetMessageQueryHandler.cs
@@ -18,19 +18,16 @@ using System.Threading;
 using System.Threading.Tasks;
 using Application.Configuration.DataAccess;
 using Dapper;
-using Domain.ArchivedMessages;
 using MediatR;
 
 namespace Application.SearchMessages;
 
 public class GetMessageQueryHandler : IRequestHandler<GetMessagesQuery, MessageSearchResult>
 {
-    private readonly IArchivedMessageRepository _archivedMessageRepository;
     private readonly IDatabaseConnectionFactory _connectionFactory;
 
-    public GetMessageQueryHandler(IArchivedMessageRepository archivedMessageRepository, IDatabaseConnectionFactory connectionFactory)
+    public GetMessageQueryHandler(IDatabaseConnectionFactory connectionFactory)
     {
-        _archivedMessageRepository = archivedMessageRepository;
         _connectionFactory = connectionFactory;
     }
 
@@ -38,22 +35,26 @@ public class GetMessageQueryHandler : IRequestHandler<GetMessagesQuery, MessageS
     {
         ArgumentNullException.ThrowIfNull(request);
 
+        var selectStatement =
+            "SELECT Id AS MessageId, DocumentType, SenderNumber, ReceiverNumber, CreatedAt FROM dbo.ArchivedMessages";
+        object queryParameters = new();
+
         if (request.CreationPeriod is not null)
         {
-            using var connection = await _connectionFactory.GetConnectionAndOpenAsync().ConfigureAwait(false);
-            var archivedMessages =
-                await connection.QueryAsync<MessageInfo>(
-                    "SELECT Id AS MessageId, DocumentType, SenderNumber, ReceiverNumber, CreatedAt FROM dbo.ArchivedMessages WHERE CreatedAt BETWEEN @StartOfPeriod AND @EndOfPeriod",
-                    new
-                    {
-                        StartOfPeriod = request.CreationPeriod.DateToSearchFrom.ToString(),
-                        EndOfPeriod = request.CreationPeriod.DateToSearchTo.ToString(),
-                    })
-                    .ConfigureAwait(false);
-            return new MessageSearchResult(archivedMessages.ToList().AsReadOnly());
+            selectStatement += " WHERE CreatedAt BETWEEN @StartOfPeriod AND @EndOfPeriod";
+            queryParameters = new
+            {
+                StartOfPeriod = request.CreationPeriod.DateToSearchFrom.ToString(),
+                EndOfPeriod = request.CreationPeriod.DateToSearchTo.ToString(),
+            };
         }
 
-        var messages = await _archivedMessageRepository.GetAllAsync().ConfigureAwait(false);
-        return new MessageSearchResult(messages.Select(message => new MessageInfo(message.Id, message.DocumentType.Name, message.SenderNumber.Value, message.ReceiverNumber.Value, message.CreatedAt)).ToList().AsReadOnly());
+        using var connection = await _connectionFactory.GetConnectionAndOpenAsync().ConfigureAwait(false);
+        var archivedMessages =
+            await connection.QueryAsync<MessageInfo>(
+                    selectStatement,
+                    queryParameters)
+                .ConfigureAwait(false);
+        return new MessageSearchResult(archivedMessages.ToList().AsReadOnly());
     }
 }
