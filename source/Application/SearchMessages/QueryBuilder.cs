0064f1664e2a0a01f819b4001bdbb254ba2779f0
Alexander <122119488+AHdevelopment@users.noreply.github.com>
2023-05-04 13:19:47 +0200

    Updates

--- source/Application/SearchMessages/QueryBuilder.cs
+++ source/Application/SearchMessages/QueryBuilder.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using Dapper;
 
 namespace Application.SearchMessages;
@@ -7,30 +7,33 @@ internal static class QueryBuilder
 {
     public static QueryInput BuildFrom(GetMessagesQuery request)
     {
-        ArgumentNullException.ThrowIfNull(request);
-
-        var selectStatement =
-            "SELECT Id AS MessageId, DocumentType, SenderNumber, ReceiverNumber, CreatedAt FROM dbo.ArchivedMessages";
         var queryParameters = new DynamicParameters();
-
-        if (request.CreationPeriod is null && request.MessageId is not null)
-        {
-            selectStatement += " WHERE CreatedAt = CreatedAt";
-        }
+        var statement = new List<string>();
 
         if (request.CreationPeriod is not null)
         {
-            selectStatement += " WHERE CreatedAt BETWEEN @StartOfPeriod AND @EndOfPeriod";
+            statement.Add("CreatedAt BETWEEN @StartOfPeriod AND @EndOfPeriod");
             queryParameters.Add("StartOfPeriod", request.CreationPeriod.DateToSearchFrom.ToString());
             queryParameters.Add("EndOfPeriod", request.CreationPeriod.DateToSearchTo.ToString());
         }
 
         if (request.MessageId is not null)
         {
-            selectStatement += " AND Id = @MessageId";
+            statement.Add("Id=@MessageId");
             queryParameters.Add("MessageId", request.MessageId.Value.ToString());
         }
 
-        return new QueryInput(selectStatement, queryParameters);
+        return new QueryInput(BuildStatement(statement), queryParameters);
+    }
+
+    private static string BuildStatement(IReadOnlyCollection<string> dic)
+    {
+        var statement = "SELECT Id AS MessageId, DocumentType, SenderNumber, ReceiverNumber, CreatedAt FROM dbo.ArchivedMessages";
+        if (dic.Count > 0)
+        {
+            statement += " WHERE " + string.Join(" AND ", dic);
+        }
+
+        return statement;
     }
 }
