15b7c0514e4d62552d9f0f6e13ece2c9b284522f
Alexander <122119488+AHdevelopment@users.noreply.github.com>
2023-05-10 11:27:47 +0200

    Update AggregationResultDocumentWriterTests.cs

--- source/Tests/Infrastructure/OutgoingMessages/AggregationResult/AggregationResultDocumentWriterTests.cs
+++ source/Tests/Infrastructure/OutgoingMessages/AggregationResult/AggregationResultDocumentWriterTests.cs
@@ -153,16 +153,18 @@ public class AggregationResultDocumentWriterTests : IClassFixture<DocumentValida
     }
 
     [Theory]
-    [InlineData(nameof(DocumentFormat.Xml))]
-    [InlineData(nameof(DocumentFormat.Json))]
-    public async Task Process_type_is_translated(string documentFormat)
+    [InlineData(nameof(DocumentFormat.Xml), nameof(ProcessType.PreliminaryAggregation))]
+    [InlineData(nameof(DocumentFormat.Json), nameof(ProcessType.PreliminaryAggregation))]
+    [InlineData(nameof(DocumentFormat.Xml), nameof(ProcessType.BalanceFixing))]
+    [InlineData(nameof(DocumentFormat.Json), nameof(ProcessType.BalanceFixing))]
+    public async Task Process_type_is_translated(string documentFormat, string processType)
     {
-        _timeSeries.WithProcessType(ProcessType.PreliminaryAggregation);
+        _timeSeries.WithProcessType(ProcessType.From(processType));
 
         var document = await CreateDocument(_timeSeries, DocumentFormat.From(documentFormat)).ConfigureAwait(false);
 
         AssertDocument(document, DocumentFormat.From(documentFormat))
-            .HasProcessType(ProcessType.PreliminaryAggregation);
+            .HasProcessType(ProcessType.From(processType));
     }
 
     private Task<Stream> CreateDocument(TimeSeriesBuilder resultBuilder, DocumentFormat documentFormat)
