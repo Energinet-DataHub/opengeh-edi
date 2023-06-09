878dcaf49f2e81bfd729aa6e1070c28494c5ee1c
Mads Due <madsmagnusdue@hotmail.com>
2023-06-09 14:29:01 +0200

    refac: ensure sql idempotent

--- source/ApplyDBMigrationsApp/Scripts/Model/202305221044 Add Document to ArchivedMessage.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202305221044 Add Document to ArchivedMessage.sql
@@ -1,2 +1,9 @@
-﻿ALTER TABLE [dbo].[ArchivedMessages]
-    ADD [Document] [varbinary](max) NULL 
+﻿IF NOT EXISTS (
+    SELECT *
+    FROM   sys.columns
+    WHERE  object_id = OBJECT_ID(N'[dbo].[ArchivedMessages]')
+      AND name = 'Document'
+)
+    BEGIN
+        ALTER TABLE [dbo].[ArchivedMessages] ADD [Document] [varbinary](max) NULL
+    END
