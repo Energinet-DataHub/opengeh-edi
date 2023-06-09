6a7652663cbbbe9b4530a98c598dcf0bb29d6858
Mads Due <madsmagnusdue@hotmail.com>
2023-06-09 13:09:49 +0200

    upcase sql statament

--- source/ApplyDBMigrationsApp/Scripts/Model/202306090924 Change archived id type to nvarchar.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202306090924 Change archived id type to nvarchar.sql
@@ -6,7 +6,7 @@ DROP INDEX IX_FindMessages ON [dbo].[ArchivedMessages];
 ALTER TABLE [dbo].[ArchivedMessages]
     ALTER COLUMN Id [nvarchar](50) NOT NULL
 
-alter table [dbo].[ArchivedMessages]
+ALTER TABLE [dbo].[ArchivedMessages]
     ADD CONSTRAINT PK_ArchivedMessages_Id PRIMARY KEY (id);
 
 CREATE INDEX IX_FindMessages ON [dbo].[ArchivedMessages] (Id, DocumentType, ReceiverNumber, SenderNumber, CreatedAt);
