38544a507568f8923f947fa1545bec168d4fdd6c
Mads Due <madsmagnusdue@hotmail.com>
2023-06-09 12:00:52 +0200

    removed excepted guid type on request for getting the document

--- source/Api/ArchivedMessages/ArchivedMessageGetDocumentListener.cs
+++ source/Api/ArchivedMessages/ArchivedMessageGetDocumentListener.cs
@@ -34,7 +34,7 @@ namespace Api.ArchivedMessages
 
         [Function("ArchivedMessages_GetDocument")]
         public async Task<HttpResponseData> GetDocumentAsync(
-        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/archived-messages/{id:Guid}/document")]
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "v1/archived-messages/{id}/document")]
         HttpRequestData request,
         string id,
         FunctionContext executionContext,
