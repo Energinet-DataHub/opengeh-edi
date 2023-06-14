1d3f6fe28e612863085f1404f3c0efc1ac5b6060
Martin Hvidberg <mhv@mjolner.dk>
2023-06-14 10:53:47 +0200

    Unittests for BalanceResponsible

--- source/Tests/Domain/Transactions/Aggregations/AggregationResultForwardingTests.cs
+++ source/Tests/Domain/Transactions/Aggregations/AggregationResultForwardingTests.cs
@@ -89,6 +89,37 @@ public class AggregationResultForwardingTests
         Assert.Equal(SettlementType.NonProfiled.Name, message.Series.SettlementType);
     }
 
+    [Fact]
+    public void Create_message_for_balance_responsible_when_result_is_total_production()
+    {
+        var result = _aggregationResult
+            .ForProduction()
+            .WithGrouping(ActorNumber.Create("1234567890123"), ActorNumber.Create("1234567890124"))
+            .Build();
+
+        var message = CreateMessage(result);
+
+        Assert.Equal(MarketRole.BalanceResponsible, message.ReceiverRole);
+        Assert.Equal(result.ActorGrouping?.BalanceResponsibleNumber, message.ReceiverId.Value);
+        //TODO: Hvad betyder nettab (kun for den balance ansvarlig som har den el-leverandør som har nettabsmålepunktet)
+    }
+
+    [Fact]
+    public void Create_message_for_balance_responsible_when_result_is_flex()
+    {
+        var result = _aggregationResult
+            .ForConsumption(SettlementType.Flex)
+            .WithGrouping(ActorNumber.Create("1234567890123"), ActorNumber.Create("1234567890124"))
+            .Build();
+
+        var message = CreateMessage(result);
+
+        Assert.Equal(MarketRole.BalanceResponsible, message.ReceiverRole);
+        Assert.Equal(result.ActorGrouping?.BalanceResponsibleNumber, message.ReceiverId.Value);
+        Assert.Equal(SettlementType.Flex.Name, message.Series.SettlementType);
+        //TODO: Hvad betyder nettab (kun for den balance ansvarlig som har den el-leverandør som har nettabsmålepunktet)
+    }
+
     private static AggregationResultMessage CreateMessage(Aggregation result)
     {
         var transaction = CreateTransaction();
