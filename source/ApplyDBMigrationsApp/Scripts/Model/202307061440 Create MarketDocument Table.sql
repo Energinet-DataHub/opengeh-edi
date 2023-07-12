f8573322c1ee6dcc09b07671ec1b21abc660a5f2
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-07-12 12:45:32 +0200

    Update 202307061440 Create MarketDocument Table.sql

--- source/ApplyDBMigrationsApp/Scripts/Model/202307061440 Create MarketDocument Table.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202307061440 Create MarketDocument Table.sql
@@ -2,7 +2,7 @@
 (
     [RecordId]                         [int] IDENTITY (1,1) NOT NULL,
     [Id]                               [uniqueidentifier]   NOT NULL,
-    [BundleId]                         [nvarchar](100)      NOT NULL,
+    [BundleId]                         [uniqueidentifier]   NOT NULL,
     [Payload]                          [varbinary](MAX)     NOT NULL,
     CONSTRAINT [PK_MarketDocuments] PRIMARY KEY NONCLUSTERED
         ([Id] ASC) ON [PRIMARY]
