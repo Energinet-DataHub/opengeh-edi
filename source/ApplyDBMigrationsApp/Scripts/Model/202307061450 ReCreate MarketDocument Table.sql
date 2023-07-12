daea9cef4062971cebf6a9fcba09c02e6ad7727a
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-07-12 13:36:27 +0200

    Recreate MarketDocuments

--- source/ApplyDBMigrationsApp/Scripts/Model/202307061450 ReCreate MarketDocument Table.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202307061450 ReCreate MarketDocument Table.sql
@@ -1,8 +1,11 @@
-﻿CREATE TABLE [dbo].[MarketDocuments]
+﻿DROP TABLE [dbo].[MarketDocuments];
+GO
+
+CREATE TABLE [dbo].[MarketDocuments]
 (
     [RecordId]                         [int] IDENTITY (1,1) NOT NULL,
     [Id]                               [uniqueidentifier]   NOT NULL,
-    [BundleId]                         [nvarchar](100)      NOT NULL,
+    [BundleId]                         [uniqueidentifier]   NOT NULL,
     [Payload]                          [varbinary](MAX)     NOT NULL,
     CONSTRAINT [PK_MarketDocuments] PRIMARY KEY NONCLUSTERED
         ([Id] ASC) ON [PRIMARY]
