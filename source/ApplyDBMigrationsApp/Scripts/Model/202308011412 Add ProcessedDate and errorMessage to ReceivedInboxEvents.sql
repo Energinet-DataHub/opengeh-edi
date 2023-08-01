30e607e8fd116166e8b7b67ab3183dce1b1a06d0
Mads Due <madsmagnusdue@hotmail.com>
2023-08-01 16:27:34 +0200

    alter table instead of drop all data

--- source/ApplyDBMigrationsApp/Scripts/Model/202308011412 Add ProcessedDate and errorMessage to ReceivedInboxEvents.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202308011412 Add ProcessedDate and errorMessage to ReceivedInboxEvents.sql
@@ -1,15 +1,3 @@
-﻿DROP TABLE [dbo].[ReceivedInboxEvents];
+﻿ALTER TABLE [dbo].[ReceivedInboxEvents] ADD ProcessedDate datetime2(7)   NULL
+ALTER TABLE [dbo].[ReceivedInboxEvents] ADD ErrorMessage nvarchar(MAX)   NULL
 GO
-
-CREATE TABLE [dbo].[ReceivedInboxEvents]
-(
-    Id nvarchar(50)                 NOT NULL,
-    RecordId Int IDENTITY (1,1)     NOT NULL,
-    OccurredOn datetime2(7)         NOT NULL,
-    EventType nvarchar(100)         NOT NULL,
-    EventPayload nvarchar(MAX)      NOT NULL,
-    ReferenceId [uniqueidentifier]  NOT NULL,
-    ProcessedDate datetime2(7)      NULL,
-    ErrorMessage nvarchar(MAX)      NULL,
-    CONSTRAINT PK_ReceivedInboxEvents PRIMARY KEY NONCLUSTERED (Id)
-)
