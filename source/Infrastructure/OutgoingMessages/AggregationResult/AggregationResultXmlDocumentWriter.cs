e56c431218bf44098ded927d2bc2692a61125364
Mads Magnus Due <xmadu@energinet.dk>
2023-08-15 15:30:33 +0200

    Fix: RSM should only contain 1 static product and balanceId+energyId should match reveiverRole (#395)
    * updated after PO feedback
    
    * updated after PO feedback

--- source/Infrastructure/OutgoingMessages/AggregationResult/AggregationResultXmlDocumentWriter.cs
+++ source/Infrastructure/OutgoingMessages/AggregationResult/AggregationResultXmlDocumentWriter.cs
@@ -59,7 +59,6 @@ public class AggregationResultXmlDocumentWriter : DocumentWriter
                 "marketEvaluationPoint.settlementMethod", timeSeries.SettlementType is null ? null : CimCode.Of(SettlementType.From(timeSeries.SettlementType)), writer).ConfigureAwait(false);
             await WriteElementIfHasValueAsync("settlement_Series.version", timeSeries.SettlementVersion, writer).ConfigureAwait(false);
             await WriteElementIfHasValueAsync("originalTransactionIDReference_Series.mRID", timeSeries.OriginalTransactionIdReference, writer).ConfigureAwait(false);
-            await WriteElementIfHasValueAsync("product", timeSeries.Product, writer).ConfigureAwait(false);
 
             await writer.WriteStartElementAsync(DocumentDetails.Prefix, "meteringGridArea_Domain.mRID", null).ConfigureAwait(false);
             await writer.WriteAttributeStringAsync(null, "codingScheme", null, "NDK").ConfigureAwait(false);
