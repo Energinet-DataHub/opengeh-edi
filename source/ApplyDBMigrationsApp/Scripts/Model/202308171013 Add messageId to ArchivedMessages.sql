a55c90b78fb499d6304cfc18c65e3e73b2f8818c
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2023-08-18 14:57:12 +0200

    Added messageId to archived messages

--- source/ApplyDBMigrationsApp/Scripts/Model/202308171013 Add messageId to ArchivedMessages.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202308171013 Add messageId to ArchivedMessages.sql
@@ -1,2 +1,7 @@
-﻿ALTER TABLE [dbo].[ArchivedMessages] ADD MessageId nvarchar(36) NULL
+﻿ALTER TABLE [dbo].[ArchivedMessages]
+ADD MessageId nvarchar(36) null
+GO
+
+UPDATE [dbo].[ArchivedMessages]
+SET [MessageId] = [dbo].[ArchivedMessages].[Id]
 GO
