b8335fa9c8495e3ba3cd33dfccdb076ac9038f16
Martin Hvidberg <xmahv@energinet.dk>
2023-08-29 09:11:50 +0200

    Used Assert.collection instead

--- source/Tests/Domain/Transactions/Aggregations/CalculationResultCompletedEventMapperTests.cs
+++ source/Tests/Domain/Transactions/Aggregations/CalculationResultCompletedEventMapperTests.cs
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Domain.OutgoingMessages;
+using Domain.Transactions.Aggregations;
 using Energinet.DataHub.Wholesale.Contracts.Events;
 using Xunit;
 using Enum = System.Enum;
@@ -28,6 +30,20 @@ public class CalculationResultCompletedEventMapperTests
     [Fact]
     public void Calculation_result_completed_processType_from_wholesale_is_not_changed()
     {
+        var processTypeTheirs = (ProcessType[])Enum.GetValues(typeof(ProcessType));
+
+        Assert.Collection(
+            processTypeTheirs,
+            item => Assert.Equal(ProcessType.Unspecified, item),
+            item => Assert.Equal(ProcessType.BalanceFixing, item),
+            item => Assert.Equal(ProcessType.Aggregation, item),
+            item => Assert.Equal(ProcessType.WholesaleFixing, item),
+            item => Assert.Equal(ProcessType.FirstCorrectionSettlement, item),
+            item => Assert.Equal(ProcessType.SecondCorrectionSettlement, item),
+            item => Assert.Equal(ProcessType.ThirdCorrectionSettlement, item));
+
+        Assert.Equal(7, processTypeTheirs.Length);
+        var businessReasons = BusinessReason.GetAll();
         //mapped to
         //ProcessType.Aggregation => BusinessReason.PreliminaryAggregation.Name,
         // ProcessType.BalanceFixing => BusinessReason.BalanceFixing.Name,
@@ -36,62 +52,73 @@ public class CalculationResultCompletedEventMapperTests
         // ProcessType.SecondCorrectionSettlement => BusinessReason.Correction.Name, // TODO: Check if this is correct
         // ProcessType.ThirdCorrectionSettlement => BusinessReason.Correction.Name, // TODO: Check if this is correct
         // ProcessType.Unspecified => throw new InvalidOperationException("Process type is not specified from Wholesales"),
-        var processTypeTheirs = (ProcessType[])Enum.GetValues(typeof(ProcessType));
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.Unspecified);
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.Aggregation);
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.BalanceFixing);
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.SecondCorrectionSettlement);
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.WholesaleFixing);
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.FirstCorrectionSettlement);
-        Assert.Contains(processTypeTheirs, type => type == ProcessType.ThirdCorrectionSettlement);
     }
 
     [Fact]
     public void Calculation_result_completed_resolution_from_wholesale_is_not_changed()
     {
         var resolutionTheirs = (Resolution[])Enum.GetValues(typeof(Resolution));
-        Assert.Contains(resolutionTheirs, resolution => resolution == Resolution.Quarter);
-        Assert.Contains(resolutionTheirs, resolution => resolution == Resolution.Unspecified);
+        Assert.Collection(
+            resolutionTheirs,
+            item => Assert.Equal(Resolution.Unspecified, item),
+            item => Assert.Equal(Resolution.Quarter, item));
+
+        Assert.Equal(2, resolutionTheirs.Length);
+        var resolutions = global::Domain.Transactions.Aggregations.Resolution.GetAll();
     }
 
     [Fact]
     public void Calculation_result_completed_quantityQuality_from_wholesale_is_not_changed()
     {
         var quantityQualityTheirs = (QuantityQuality[])Enum.GetValues(typeof(QuantityQuality));
-        Assert.Contains(quantityQualityTheirs, quality => quality == QuantityQuality.Unspecified);
-        Assert.Contains(quantityQualityTheirs, quality => quality == QuantityQuality.Calculated);
-        Assert.Contains(quantityQualityTheirs, quality => quality == QuantityQuality.Estimated);
-        Assert.Contains(quantityQualityTheirs, quality => quality == QuantityQuality.Incomplete);
-        Assert.Contains(quantityQualityTheirs, quality => quality == QuantityQuality.Measured);
-        Assert.Contains(quantityQualityTheirs, quality => quality == QuantityQuality.Missing);
 
-        //var businessReason = Quality.GetAll();
+        Assert.Collection(
+            quantityQualityTheirs,
+            item => Assert.Equal(QuantityQuality.Unspecified, item),
+            item => Assert.Equal(QuantityQuality.Estimated, item),
+            item => Assert.Equal(QuantityQuality.Measured, item),
+            item => Assert.Equal(QuantityQuality.Missing, item),
+            item => Assert.Equal(QuantityQuality.Incomplete, item),
+            item => Assert.Equal(QuantityQuality.Calculated, item));
+
+        Assert.Equal(6, quantityQualityTheirs.Length);
+
+        var quantityQuality = Quality.GetAll();
     }
 
     [Fact]
     public void Calculation_result_completed_timeSeriesType_from_wholesale_is_not_changed()
     {
         var timeSeriesTypeTheirs = (TimeSeriesType[])Enum.GetValues(typeof(TimeSeriesType));
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.Production);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.Production);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.NonProfiledConsumption);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.FlexConsumption);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.NetExchangePerGa);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.NetExchangePerNeighboringGa);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.GridLoss);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.NegativeGridLoss);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.PositiveGridLoss);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.TotalConsumption);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.TempFlexConsumption);
-        Assert.Contains(timeSeriesTypeTheirs, type => type == TimeSeriesType.TempProduction);
+        Assert.Collection(
+            timeSeriesTypeTheirs,
+            item => Assert.Equal(TimeSeriesType.Unspecified, item),
+            item => Assert.Equal(TimeSeriesType.Production, item),
+            item => Assert.Equal(TimeSeriesType.NonProfiledConsumption, item),
+            item => Assert.Equal(TimeSeriesType.FlexConsumption, item),
+            item => Assert.Equal(TimeSeriesType.NetExchangePerGa, item),
+            item => Assert.Equal(TimeSeriesType.NetExchangePerNeighboringGa, item),
+            item => Assert.Equal(TimeSeriesType.GridLoss, item),
+            item => Assert.Equal(TimeSeriesType.NegativeGridLoss, item),
+            item => Assert.Equal(TimeSeriesType.PositiveGridLoss, item),
+            item => Assert.Equal(TimeSeriesType.TotalConsumption, item),
+            item => Assert.Equal(TimeSeriesType.TempFlexConsumption, item),
+            item => Assert.Equal(TimeSeriesType.TempProduction, item));
+        Assert.Equal(12, timeSeriesTypeTheirs.Length);
+
+        var timeSeriesTypes = MeteringPointType.GetAll();
     }
 
     [Fact]
     public void Calculation_result_completed_quantity_from_wholesale_is_not_changed()
     {
-        //var measurementUnits = MeasurementUnit.GetAll();
         var measurementUnitTheirs = (QuantityUnit[])Enum.GetValues(typeof(QuantityUnit));
-        Assert.Contains(measurementUnitTheirs, unit => unit == QuantityUnit.Unspecified);
-        Assert.Contains(measurementUnitTheirs, unit => unit == QuantityUnit.Kwh);
+        Assert.Collection(
+            measurementUnitTheirs,
+            item => Assert.Equal(QuantityUnit.Unspecified, item),
+            item => Assert.Equal(QuantityUnit.Kwh, item));
+        Assert.Equal(2, measurementUnitTheirs.Length);
+
+        var measurementUnits = MeasurementUnit.GetAll();
     }
 }
