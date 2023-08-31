e076e3f10d785c826cb08f20555ed6e483e7801e
Mads Magnus Due <madsmagnusdue@hotmail.com>
2023-08-31 11:34:10 +0200

    new test to prove we can not handle two peek requests at same time for same actor

--- source/IntegrationTests/Application/OutgoingMessages/WhenAPeekIsRequestedTests.cs
+++ source/IntegrationTests/Application/OutgoingMessages/WhenAPeekIsRequestedTests.cs
@@ -23,12 +23,15 @@ using Domain.Actors;
 using Domain.Documents;
 using Domain.OutgoingMessages;
 using Domain.OutgoingMessages.Peek;
+using Infrastructure.Configuration.InternalCommands;
 using Infrastructure.OutgoingMessages;
 using IntegrationTests.Application.IncomingMessages;
 using IntegrationTests.Assertions;
 using IntegrationTests.Factories;
 using IntegrationTests.Fixtures;
+using IntegrationTests.Infrastructure.Configuration.InternalCommands;
 using MediatR;
+using Microsoft.Extensions.DependencyInjection;
 using Xunit;
 using MessageCategory = Domain.OutgoingMessages.Queueing.MessageCategory;
 
@@ -87,6 +90,33 @@ public class WhenAPeekIsRequestedTests : TestBase
 
         var result = await PeekMessage(MessageCategory.MasterData).ConfigureAwait(false);
 
+        await AssertMessageIsArchived(result.MessageId);
+    }
+
+    [Fact]
+    public async Task Two_peek_request_at_same_time_from_same_actor()
+    {
+        // Arrange
+        await GivenAMoveInTransactionHasBeenAccepted().ConfigureAwait(false);
+
+        var serviceScopeFactory = GetService<IServiceScopeFactory>();
+        using var newScope = serviceScopeFactory.CreateScope();
+        var mediatr = newScope.ServiceProvider.GetRequiredService<IMediator>();
+
+        // Act
+        var task01 = PeekMessage(MessageCategory.MasterData);
+        // NEW SCOPE for second task.
+        var task02 = mediatr.Send(
+            new PeekCommand(
+                ActorNumber.Create(SampleData.NewEnergySupplierNumber),
+                MessageCategory.MasterData,
+                MarketRole.EnergySupplier,
+                DocumentFormat.Xml));
+
+        await Task.WhenAll(task01, task02).ConfigureAwait(false);
+        var result = await task01;
+
+        // Assert
         await AssertMessageIsArchived(result.MessageId);
     }
 
