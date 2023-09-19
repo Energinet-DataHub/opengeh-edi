2144149f6fc75db1050dd3297eb2ac8fd28870fc
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2023-09-19 14:15:26 +0200

    added test for failing service bus commit

--- source/IntegrationTests/Infrastructure.CimMessageAdapter/Messages/RequestAggregatedMeasureData/RequestAggregatedMeasureDataReceiverTests.cs
+++ source/IntegrationTests/Infrastructure.CimMessageAdapter/Messages/RequestAggregatedMeasureData/RequestAggregatedMeasureDataReceiverTests.cs
@@ -49,6 +49,7 @@ public class RequestAggregatedMeasureDataReceiverTests : TestBase, IAsyncLifetim
     private readonly MessageTypeValidator _messageTypeValidator;
     private readonly CalculationResponsibleReceiverVerification _calculationResponsibleReceiverVerification;
     private readonly MessageQueueDispatcherStub<RequestAggregatedMeasureDataTransactionQueues> _messageQueueDispatcherSpy = new();
+    private readonly MessageQueueDispatcherThatFailsStub<RequestAggregatedMeasureDataTransactionQueues> _messageQueueDispatcherThatFailsStub = new();
     private readonly List<Claim> _claims = new()
     {
         new(ClaimsMap.UserId, new CreateActor(Guid.NewGuid().ToString(), SampleData.StsAssignedUserId, SampleData.SenderId).B2CId),
@@ -458,6 +459,30 @@ public class RequestAggregatedMeasureDataReceiverTests : TestBase, IAsyncLifetim
         var transaction = _messageQueueDispatcherSpy.CommittedItems.FirstOrDefault();
         Assert.True(result.Success);
         Assert.NotNull(transaction);
+
+        var document = messageParserResult!.IncomingMarketDocument!;
+        await AssertTransactionIdIsStoredAsync(document.Header.SenderId, document.MarketActivityRecords.First().Id).ConfigureAwait(false);
+        await AssertMessageIdIsStoredAsync(document.Header.SenderId, document.Header.MessageId).ConfigureAwait(false);
+    }
+
+    // This is a unpleasant implementation, but it's the current implementation.
+    // Since it is not possible to make a database transaction and a service bus commit
+    // in the same transaction scope.
+    [Fact]
+    public async Task Transaction_and_message_ids_are_stored_when_commit_to_queue_fails()
+    {
+        await CreateIdentityWithRoles(new List<MarketRole> { MarketRole.EnergySupplier })
+            .ConfigureAwait(false);
+        var knownReceiverId = "5790001330552";
+        var knownReceiverRole = "DGL";
+        await using var message = BusinessMessageBuilder
+            .RequestAggregatedMeasureData()
+            .WithReceiverRole(knownReceiverRole)
+            .WithReceiverId(knownReceiverId)
+            .Message();
+
+        var messageParserResult = await ParseMessageAsync(message).ConfigureAwait(false);
+        await Assert.ThrowsAsync<ServiceBusCommitException>(() => CreateMessageReceiver(_messageQueueDispatcherThatFailsStub).ReceiveAsync(messageParserResult, CancellationToken.None)).ConfigureAwait(false);
 
         var document = messageParserResult!.IncomingMarketDocument!;
         await AssertTransactionIdIsStoredAsync(document.Header.SenderId, document.MarketActivityRecords.First().Id).ConfigureAwait(false);
