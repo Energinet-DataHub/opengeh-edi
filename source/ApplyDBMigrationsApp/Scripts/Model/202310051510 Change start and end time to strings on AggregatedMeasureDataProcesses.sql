f4b3cf874734c113b97f5f57fcbd8b360be91900
Mads Magnus Due <madsmagnusdue@hotmail.com>
2023-10-05 14:41:45 +0200

    refac: RequestAggregatedMeasureData period to string values

---
+++ source/ApplyDBMigrationsApp/Scripts/Model/202310051510 Change start and end time to strings on AggregatedMeasureDataProcesses.sql
@@ -0,0 +1,5 @@
+﻿ALTER TABLE [dbo].[AggregatedMeasureDataProcesses]
+ALTER COLUMN StartOfPeriod VARCHAR(32) NULL
+GO
+ALTER TABLE [dbo].[AggregatedMeasureDataProcesses]
+ALTER COLUMN EndOfPeriod VARCHAR(32) NULL
