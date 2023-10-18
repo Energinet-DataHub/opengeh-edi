7433a4fc7f086550129f72a7a0bb597e8ea41b73
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-10-18 12:58:47 +0200

    Update 202310181233 Add SettlemenVersion to AggregatedMeasuredDataProcess.sql

--- source/ApplyDBMigrationsApp/Scripts/Model/202310181233 Add SettlemenVersion to AggregatedMeasuredDataProcess.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202310181233 Add SettlemenVersion to AggregatedMeasuredDataProcess.sql
@@ -1,9 +1,1 @@
-﻿IF NOT EXISTS (
-    SELECT *
-    FROM   sys.columns
-    WHERE  object_id = OBJECT_ID(N'[dbo].[AggregatedMeasureDataProcesses]')
-      AND name = 'SettlementVersion'
-)
-BEGIN
-ALTER TABLE [dbo].[AggregatedMeasureDataProcesses] ADD [SettlementVersion] [varbinary](3) NULL
-END
+﻿ALTER TABLE [dbo].[AggregatedMeasureDataProcesses] ADD [SettlementVersion] [varbinary](3) NULL
