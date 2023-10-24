537e56778071deed9088f7a3d61147dafa4e3fd0
Mads Magnus Due <xmadu@energinet.dk>
2023-10-24 08:54:20 +0200

    fix: wrong date format for b2c requests (#588)
    * fix: wrong date format for b2c requests
    
    * fix noda parser

---
+++ source/B2CWebApi/Factories/InstantFormatFactory.cs
@@ -0,0 +1,31 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using NodaTime;
+using NodaTime.Text;
+
+namespace Energinet.DataHub.EDI.B2CWebApi.Factories;
+
+public static class InstantFormatFactory
+{
+    public static string SetInstantToMidnight(string dateString, DateTimeZone dateTimeZone)
+    {
+        var instant = InstantPattern.ExtendedIso.Parse(dateString).Value;
+        var zonedDateTime = new ZonedDateTime(instant, dateTimeZone);
+        var dateTimeZoneAtMidnight = zonedDateTime.Date
+            .At(LocalTime.Midnight)
+            .InZoneStrictly(dateTimeZone);
+        return dateTimeZoneAtMidnight.ToInstant().ToString();
+    }
+}
