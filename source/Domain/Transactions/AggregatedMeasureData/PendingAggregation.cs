84063a6f67c9b7a4b7a31badc52cab14b0fafd70
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2023-10-31 13:39:01 +0100

    Update PendingAggregation.cs

--- source/Domain/Transactions/AggregatedMeasureData/PendingAggregation.cs
+++ source/Domain/Transactions/AggregatedMeasureData/PendingAggregation.cs
@@ -13,7 +13,6 @@
 // limitations under the License.
 
 using Energinet.DataHub.EDI.Domain.Actors;
-using Energinet.DataHub.EDI.Domain.GridAreas;
 using Energinet.DataHub.EDI.Domain.OutgoingMessages;
 using Energinet.DataHub.EDI.Domain.Transactions.Aggregations;
 
