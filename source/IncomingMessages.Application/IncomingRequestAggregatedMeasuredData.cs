3cf0e5b2e511327cddb3df998f768f8a03b7685a
Lasse Ringgren Nielsen <sixxth@gmail.com>
2023-11-13 12:26:58 +0100

    Create IncomingRequestAggregatedMeasuredData.cs

---
+++ source/IncomingMessages.Application/IncomingRequestAggregatedMeasuredData.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+namespace Energinet.DataHub.EDI.IncomingMessages.Application;
+
+public class IncomingRequestAggregatedMeasuredData
+{//TODO: LRN move back to infrastructure
+    // private readonly IServiceBusSenderAdapter _senderCreator;
+    //
+    // public IncomingRequestAggregatedMeasuredData(
+    //     IServiceBusSenderFactory serviceBusSenderFactory,
+    //     IncommingMessagesServiceBusClientConfiguration wholeSaleServiceBusClientConfiguration)
+    // {
+    //     if (serviceBusSenderFactory == null) throw new ArgumentNullException(nameof(serviceBusSenderFactory));
+    //     if (wholeSaleServiceBusClientConfiguration == null) throw new ArgumentNullException(nameof(wholeSaleServiceBusClientConfiguration));
+    //
+    //     _senderCreator = serviceBusSenderFactory.GetSender(wholeSaleServiceBusClientConfiguration.QueueName);
+    // }
+    //
+    // public async Task SendAsync(
+    //     ServiceBusMessage request,
+    //     CancellationToken cancellationToken)
+    // {
+    //     await _senderCreator.SendAsync(request, cancellationToken).ConfigureAwait(false);
+    // }
+}
