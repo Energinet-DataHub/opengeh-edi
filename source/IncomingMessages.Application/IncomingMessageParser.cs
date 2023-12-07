06c88afc439eb134c9b6a8a0f8efe553ea217f15
Mads Magnus Due <madsmagnusdue@hotmail.com>
2023-12-07 17:46:36 +0100

    fix: avoid empty document in archived message

--- source/IncomingMessages.Application/IncomingMessageParser.cs
+++ source/IncomingMessages.Application/IncomingMessageParser.cs
@@ -63,6 +63,7 @@ public class IncomingMessageParser : IIncomingMessageParser
         CancellationToken cancellationToken,
         DocumentFormat responseFormat = null!)
     {
+        if (message == null) throw new ArgumentNullException(nameof(message));
         var requestAggregatedMeasureDataMarketMessageParserResult =
             await _marketMessageParser.ParseAsync(message, documentFormat, documentType, cancellationToken).ConfigureAwait(false);
 
@@ -72,6 +73,7 @@ public class IncomingMessageParser : IIncomingMessageParser
             return _responseFactory.From(res, responseFormat ?? documentFormat);
         }
 
+        EnsureStreamIsRewound(message);
         await _archivedMessagesClient.CreateAsync(
             new ArchivedMessage(
                 Guid.NewGuid().ToString(),
@@ -106,5 +108,10 @@ public class IncomingMessageParser : IIncomingMessageParser
         }
 
         return _responseFactory.From(result, responseFormat ?? documentFormat);
+    }
+
+    private static void EnsureStreamIsRewound(Stream message)
+    {
+        message.Seek(0, SeekOrigin.Begin);
     }
 }
