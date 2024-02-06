4fd6691d4c3798a52e43c1104101226c39624c60
Ebbe Knudsen <xedkn@energinet.dk>
2024-02-06 10:11:57 +0100

    Fix sql migration script deletion order

--- source/ApplyDBMigrationsApp/Scripts/Model/202401291447 Clear MarketDocuments and ActorMessageQueues tables and change MarketDocument.Payload columnn to FileStorageReference.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202401291447 Clear MarketDocuments and ActorMessageQueues tables and change MarketDocument.Payload columnn to FileStorageReference.sql
@@ -1,10 +1,11 @@
+TRUNCATE TABLE [dbo].[Bundles]
+GO
+
 -- DELETE FROM since Bundles foreign key is referencing ActorMessageQueues
 -- noinspection SqlWithoutWhere
 DELETE FROM [dbo].[ActorMessageQueues]
 GO
 
-TRUNCATE TABLE [dbo].[Bundles]
-GO
 
 TRUNCATE TABLE [dbo].[MarketDocuments]
 GO
