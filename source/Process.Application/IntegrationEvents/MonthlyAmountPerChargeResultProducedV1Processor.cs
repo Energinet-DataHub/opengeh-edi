d3dae1693e11f7c5ab21ffc9b54002796e11e6a2
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-02-07 11:44:37 +0100

    yes

---
+++ source/Process.Application/IntegrationEvents/MonthlyAmountPerChargeResultProducedV1Processor.cs
@@ -0,0 +1,56 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.Core.Messaging.Communication;
+using Energinet.DataHub.EDI.Infrastructure.Configuration.IntegrationEvents.IntegrationEventMappers;
+using Energinet.DataHub.EDI.OutgoingMessages.Interfaces;
+using Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models;
+using Energinet.DataHub.EDI.Process.Application.Transactions.WholesaleCalculation;
+using Energinet.DataHub.Wholesale.Contracts.IntegrationEvents;
+using Microsoft.Extensions.Logging;
+
+namespace Energinet.DataHub.EDI.Process.Application.IntegrationEvents;
+
+public class MonthlyAmountPerChargeResultProducedV1Processor : IIntegrationEventProcessor
+{
+    private readonly WholesaleCalculationFactory _wholesaleFactory;
+    private readonly IOutgoingMessagesClient _outgoingMessagesClient;
+    private readonly ILogger<MonthlyAmountPerChargeResultProducedV1Processor> _logger;
+
+    public MonthlyAmountPerChargeResultProducedV1Processor(
+        WholesaleCalculationFactory wholesaleFactory,
+        IOutgoingMessagesClient outgoingMessagesClient,
+        ILogger<MonthlyAmountPerChargeResultProducedV1Processor> logger)
+    {
+        _wholesaleFactory = wholesaleFactory;
+        _outgoingMessagesClient = outgoingMessagesClient;
+        _logger = logger;
+    }
+
+    public string EventTypeToHandle => MonthlyAmountPerChargeResultProducedV1.EventName;
+
+    public async Task ProcessAsync(IntegrationEvent integrationEvent, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(integrationEvent);
+
+        var monthlyAmountPerChargeResultProducedV1 = (MonthlyAmountPerChargeResultProducedV1)integrationEvent.Message;
+
+        var message = _wholesaleFactory.CreateAsync(monthlyAmountPerChargeResultProducedV1, cancellationToken)
+            .ConfigureAwait(false);
+        await _outgoingMessagesClient.EnqueueAndCommitAsync(message!, cancellationToken).ConfigureAwait(false);
+    }
+}
