05322314b186d88ab547d9fda1967f83e899e8e9
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2024-02-20 07:47:36 +0100

    some of it

---
+++ source/OutgoingMessages.Application/MarketDocuments/WholesaleCalculations/WholesaleCalculationJsonDocumentWriter.cs
@@ -0,0 +1,193 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Globalization;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
+using Energinet.DataHub.EDI.OutgoingMessages.Application.MarketDocuments.Json;
+using Energinet.DataHub.EDI.OutgoingMessages.Domain.MarketDocuments;
+using Energinet.DataHub.EDI.OutgoingMessages.Domain.OutgoingMessages;
+using Energinet.DataHub.EDI.OutgoingMessages.Domain.OutgoingMessages.Queueing;
+
+namespace Energinet.DataHub.EDI.OutgoingMessages.Application.MarketDocuments.WholesaleCalculations;
+
+public sealed class WholesaleCalculationJsonDocumentWriter : IDocumentWriter
+{
+    // TODO: FIX THIS!
+    private const string DocumentTypeName = "NotifyWholesaleServices_MarketDocument";
+    private const string TypeCode = "E31";
+    private readonly IMessageRecordParser _parser;
+
+    public WholesaleCalculationJsonDocumentWriter(IMessageRecordParser parser)
+    {
+        _parser = parser;
+    }
+
+    public bool HandlesFormat(DocumentFormat format)
+    {
+        return format == DocumentFormat.Json;
+    }
+
+    public bool HandlesType(DocumentType documentType)
+    {
+        return documentType == DocumentType.NotifyWholesaleServices;
+    }
+
+    public async Task<MarketDocumentStream> WriteAsync(
+        OutgoingMessageHeader header,
+        IReadOnlyCollection<string> marketActivityRecords)
+    {
+        var stream = new MarketDocumentWriterMemoryStream();
+        var options = new JsonWriterOptions { Indented = true };
+
+        using var writer = new Utf8JsonWriter(stream, options);
+        JsonHeaderWriter.Write(header, DocumentTypeName, TypeCode, null, writer);
+        WriteSeries(marketActivityRecords, writer);
+        writer.WriteEndObject();
+        writer.WriteEndObject();
+        await writer.FlushAsync().ConfigureAwait(false);
+
+        stream.Position = 0;
+        return new MarketDocumentStream(stream);
+    }
+
+    private void WriteSeries(IReadOnlyCollection<string> marketActivityRecords, Utf8JsonWriter writer)
+    {
+        ArgumentNullException.ThrowIfNull(marketActivityRecords);
+        ArgumentNullException.ThrowIfNull(writer);
+
+        writer.WritePropertyName("Series");
+        writer.WriteStartArray();
+        {
+            foreach (var series in ParseFrom(marketActivityRecords))
+            {
+                writer.WriteStartObject();
+                {
+                    writer.WriteProperty("mRID", series.TransactionId.ToString());
+                    writer.WriteProperty("version", series.CalculationVersion.ToString(NumberFormatInfo.InvariantInfo));
+
+                    if (series.SettlementVersion is not null)
+                    {
+                        writer.WriteObject(
+                            "settlement_Series.version",
+                            KeyValuePair.Create("value", series.SettlementVersion.Code));
+                    }
+
+                    // TODO (MWO): These lines have to be converted from xml to json when the time comes
+                    // TODO (MWO): These are there for later use, but are not used as of right now
+                    //await WriteElementIfHasValueAsync("originalTransactionIDReference_Series.mRID", wholesaleCalculationSeries.OriginalTransactionIdReference, writer).ConfigureAwait(false);
+                    //await writer.WriteElementStringAsync(DocumentDetails.Prefix, "marketEvaluationPoint.type", null, "E17").ConfigureAwait(false);
+                    //await WriteElementIfHasValueAsync("marketEvaluationPoint.settlementMethod", wholesaleCalculationSeries.SettlementType is null ? null : CimCode.Of(SettlementType.From(wholesaleCalculationSeries.SettlementType)), writer).ConfigureAwait(false);
+
+                    writer.WriteObject(
+                        "chargeType.mRID",
+                        KeyValuePair.Create("value", series.ChargeCode));
+
+                    writer.WriteObject(
+                        "chargeType.type",
+                        KeyValuePair.Create("value", series.ChargeType.Code));
+
+                    writer.WriteObject(
+                        "chargeType.chargeTypeOwner_MarketParticipant.mRID",
+                        new KeyValuePair<string, string>("codingScheme", CimCode.CodingSchemeOf(series.ChargeOwner)),
+                        new KeyValuePair<string, string>("value", series.ChargeOwner.Value));
+
+                    writer.WriteObject(
+                        "meteringGridArea_Domain.mRID",
+                        new KeyValuePair<string, string>("codingScheme", "NDK"),
+                        new KeyValuePair<string, string>("value", series.GridAreaCode));
+
+                    writer.WriteObject(
+                        "energySupplier_MarketParticipant.mRID",
+                        new KeyValuePair<string, string>("codingScheme", CimCode.CodingSchemeOf(series.EnergySupplier)),
+                        new KeyValuePair<string, string>("value", series.EnergySupplier.Value));
+
+                    writer.WriteProperty("product", GeneralValues.ProductCode);
+
+                    writer.WriteObject(
+                        "quantity_Measure_Unit.name",
+                        KeyValuePair.Create("value", series.QuantityUnit.Code));
+
+                    writer.WriteObject(
+                        "price_Measure_Unit.name",
+                        KeyValuePair.Create("value", series.PriceMeasureUnit.Code));
+
+                    writer.WriteObject(
+                        "currency_Unit.name",
+                        KeyValuePair.Create("value", series.Currency.Code));
+
+                    writer.WritePropertyName("Period");
+                    writer.WriteStartObject();
+                    {
+                        writer.WriteProperty("resolution", series.Resolution.Code);
+
+                        writer.WritePropertyName("timeInterval");
+                        writer.WriteStartObject();
+                        {
+                            writer.WriteObject(
+                                "start",
+                                KeyValuePair.Create("value", series.Period.StartToString()));
+                            writer.WriteObject(
+                                "end",
+                                KeyValuePair.Create("value", series.Period.EndToString()));
+                        }
+
+                        writer.WriteEndObject();
+
+                        writer.WritePropertyName("Point");
+                        writer.WriteStartArray();
+                        {
+                            writer.WriteStartObject();
+                            {
+                                writer.WriteObject(
+                                    "position",
+                                    KeyValuePair.Create("value", "1"));
+                                writer.WriteObject(
+                                    "energySum_Quantity.quantity",
+                                    KeyValuePair.Create(
+                                        "value",
+                                        series.Quantity?.ToString(NumberFormatInfo.InvariantInfo) ?? "0"));
+                            }
+
+                            writer.WriteEndObject();
+                        }
+
+                        writer.WriteEndArray();
+                    }
+
+                    writer.WriteEndObject();
+                }
+
+                writer.WriteEndObject();
+            }
+        }
+
+        writer.WriteEndArray();
+    }
+
+    private ReadOnlyCollection<WholesaleCalculationMarketActivityRecord> ParseFrom(IReadOnlyCollection<string> payloads)
+    {
+        ArgumentNullException.ThrowIfNull(payloads);
+
+        return payloads
+            .Select(payload => _parser.From<WholesaleCalculationMarketActivityRecord>(payload))
+            .ToList()
+            .AsReadOnly();
+    }
+}
---
+++ source/Tests/Infrastructure/OutgoingMessages/WholesaleCalculations/AssertWholesaleCalculationResultJsonDocument.cs
@@ -0,0 +1,412 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
+using Energinet.DataHub.EDI.OutgoingMessages.Domain.MarketDocuments;
+using FluentAssertions;
+using IncomingMessages.Infrastructure.DocumentValidation;
+using Json.Schema;
+
+namespace Energinet.DataHub.EDI.Tests.Infrastructure.OutgoingMessages.WholesaleCalculations;
+
+public sealed class AssertWholesaleCalculationResultJsonDocument : IAssertWholesaleCalculationResultDocument
+{
+    private readonly JsonSchemaProvider _schemas = new(new CimJsonSchemas());
+    private readonly JsonDocument _document;
+    private readonly JsonElement _root;
+
+    public AssertWholesaleCalculationResultJsonDocument(Stream document)
+    {
+        _document = JsonDocument.Parse(document);
+        _root = _document.RootElement.GetProperty("NotifyWholesaleServices_MarketDocument");
+    }
+
+    public async Task<IAssertWholesaleCalculationResultDocument> DocumentIsValidAsync()
+    {
+        var schema = await _schemas
+            .GetSchemaAsync<JsonSchema>("NOTIFYWHOLESALESERVICES", "0", CancellationToken.None)
+            .ConfigureAwait(false);
+
+        schema.Should().NotBeNull("Cannot validate document without a schema");
+
+        var validationOptions = new EvaluationOptions { OutputFormat = OutputFormat.List };
+        var validationResult = schema!.Evaluate(_document, validationOptions);
+        var errors = validationResult.Details.Where(detail => detail.HasErrors)
+            .Select(x => x.Errors)
+            .SelectMany(e => e!.Values);
+
+        validationResult.IsValid.Should().BeTrue(string.Join("\n", errors));
+
+        return this;
+    }
+
+    #region header validation
+
+    public IAssertWholesaleCalculationResultDocument HasMessageId(string expectedMessageId)
+    {
+        _root.GetProperty("mRID").GetString().Should().Be(expectedMessageId);
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasBusinessReason(BusinessReason expectedBusinessReason)
+    {
+        _root.GetProperty("process.processType")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(CimCode.Of(expectedBusinessReason));
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasSenderId(ActorNumber expectedSenderId)
+    {
+        ArgumentNullException.ThrowIfNull(expectedSenderId);
+        _root.GetProperty("sender_MarketParticipant.mRID")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedSenderId.Value);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasSenderRole(ActorRole expectedSenderRole)
+    {
+        ArgumentNullException.ThrowIfNull(expectedSenderRole);
+        _root.GetProperty("sender_MarketParticipant.marketRole.type")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedSenderRole.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasReceiverId(ActorNumber expectedReceiverId)
+    {
+        ArgumentNullException.ThrowIfNull(expectedReceiverId);
+        _root.GetProperty("receiver_MarketParticipant.mRID")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedReceiverId.Value);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasReceiverRole(ActorRole expectedReceiverRole)
+    {
+        ArgumentNullException.ThrowIfNull(expectedReceiverRole);
+        _root.GetProperty("receiver_MarketParticipant.marketRole.type")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedReceiverRole.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasTimestamp(string expectedTimestamp)
+    {
+        _root.GetProperty("createdDateTime").GetString().Should().Be(expectedTimestamp);
+        return this;
+    }
+
+    #endregion
+
+    public IAssertWholesaleCalculationResultDocument HasTransactionId(Guid expectedTransactionId)
+    {
+        FirstTimeSeriesElement().GetProperty("mRID").GetString().Should().Be(expectedTransactionId.ToString());
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasCalculationVersion(int expectedVersion)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("version")
+            .GetString()
+            .Should()
+            .Be(expectedVersion.ToString(CultureInfo.InvariantCulture));
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasSettlementVersion(SettlementVersion expectedSettlementVersion)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("settlement_Series.version")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(CimCode.Of(expectedSettlementVersion));
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasOriginalTransactionIdReference(
+        string expectedOriginalTransactionIdReference)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("originalTransactionIDReference_Series.mRID")
+            .GetString()
+            .Should()
+            .Be(expectedOriginalTransactionIdReference);
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasEvaluationType(string expectedMarketEvaluationType)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("marketEvaluationPoint.type")
+            .GetString()
+            .Should()
+            .Be(expectedMarketEvaluationType);
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasSettlementMethod(SettlementType expectedSettlementMethod)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("marketEvaluationPoint.settlementMethod")
+            .GetString()
+            .Should()
+            .Be(CimCode.Of(expectedSettlementMethod));
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasChargeCode(string expectedChargeTypeNumber)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("chargeType.mRID")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedChargeTypeNumber);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasChargeType(ChargeType expectedChargeType)
+    {
+        ArgumentNullException.ThrowIfNull(expectedChargeType);
+        FirstTimeSeriesElement()
+            .GetProperty("chargeType.type")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedChargeType.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasChargeTypeOwner(ActorNumber expectedChargeTypeOwner)
+    {
+        ArgumentNullException.ThrowIfNull(expectedChargeTypeOwner);
+        FirstTimeSeriesElement()
+            .GetProperty("chargeType.chargeTypeOwner_MarketParticipant.mRID")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedChargeTypeOwner.Value);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasGridAreaCode(string expectedGridAreaCode)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("meteringGridArea_Domain.mRID")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedGridAreaCode);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasEnergySupplierNumber(ActorNumber expectedEnergySupplierNumber)
+    {
+        ArgumentNullException.ThrowIfNull(expectedEnergySupplierNumber);
+        FirstTimeSeriesElement()
+            .GetProperty("energySupplier_MarketParticipant.mRID")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedEnergySupplierNumber.Value);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasProductCode(string expectedProductCode)
+    {
+        FirstTimeSeriesElement().GetProperty("product").GetString().Should().Be(expectedProductCode);
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasMeasurementUnit(MeasurementUnit expectedMeasurementUnit)
+    {
+        ArgumentNullException.ThrowIfNull(expectedMeasurementUnit);
+        FirstTimeSeriesElement()
+            .GetProperty("quantity_Measure_Unit.name")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedMeasurementUnit.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasPriceMeasurementUnit(
+        MeasurementUnit expectedPriceMeasurementUnit)
+    {
+        ArgumentNullException.ThrowIfNull(expectedPriceMeasurementUnit);
+        FirstTimeSeriesElement()
+            .GetProperty("price_Measure_Unit.name")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedPriceMeasurementUnit.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasCurrency(Currency expectedPriceUnit)
+    {
+        ArgumentNullException.ThrowIfNull(expectedPriceUnit);
+        FirstTimeSeriesElement()
+            .GetProperty("currency_Unit.name")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedPriceUnit.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasPeriod(Period expectedPeriod)
+    {
+        ArgumentNullException.ThrowIfNull(expectedPeriod);
+        FirstTimeSeriesElement()
+            .GetProperty("Period")
+            .GetProperty("timeInterval")
+            .GetProperty("start")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedPeriod.StartToString());
+
+        FirstTimeSeriesElement()
+            .GetProperty("Period")
+            .GetProperty("timeInterval")
+            .GetProperty("end")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedPeriod.EndToString());
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasResolution(Resolution resolution)
+    {
+        ArgumentNullException.ThrowIfNull(resolution);
+        FirstTimeSeriesElement()
+            .GetProperty("Period")
+            .GetProperty("resolution")
+            .GetString()
+            .Should()
+            .Be(resolution.Code);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument HasPositionAndQuantity(int expectedPosition, int expectedQuantity)
+    {
+        var point = FirstTimeSeriesElement()
+            .GetProperty("Period")
+            .GetProperty("Point")
+            .EnumerateArray()
+            .ToList()[0];
+
+        point
+            .GetProperty("position")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedPosition.ToString(CultureInfo.InvariantCulture));
+
+        point
+            .GetProperty("energySum_Quantity.quantity")
+            .GetProperty("value")
+            .GetString()
+            .Should()
+            .Be(expectedQuantity.ToString(CultureInfo.InvariantCulture));
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument SettlementMethodIsNotPresent()
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("marketEvaluationPoint.settlementMethod")
+            .ValueKind.Should()
+            .Be(JsonValueKind.Undefined);
+        // _documentAsserter.IsNotPresent("Series[1]/marketEvaluationPoint.settlementMethod");
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument QualityIsPresentForPosition(
+        int expectedPosition,
+        string expectedQuantityQualityCode)
+    {
+        FirstTimeSeriesElement()
+            .GetProperty("Period")
+            .GetProperty("Point")
+            .EnumerateArray()
+            .ToList()[expectedPosition - 1]
+            .GetProperty("quality")
+            .GetString()
+            .Should()
+            .Be(expectedQuantityQualityCode);
+
+        return this;
+    }
+
+    public IAssertWholesaleCalculationResultDocument SettlementVersionIsNotPresent()
+    {
+        var act = () => _root.GetProperty("Series[1]")
+            .GetProperty("settlement_Series.version");
+
+        act.Should()
+            .ThrowExactly<KeyNotFoundException>()
+            .WithMessage("The given key was not present in the dictionary.");
+
+        // _documentAsserter.IsNotPresent("Series[1]/settlement_Series.version");
+        return this;
+    }
+
+    private JsonElement FirstTimeSeriesElement()
+    {
+        return _root.GetProperty("Series").EnumerateArray().ToList()[0];
+    }
+}
