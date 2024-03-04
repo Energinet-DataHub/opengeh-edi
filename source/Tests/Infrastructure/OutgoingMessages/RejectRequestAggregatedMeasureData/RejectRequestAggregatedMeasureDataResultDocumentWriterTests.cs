5ff282053d2ea3ea51d2cd82bf66f28edf5e2d91
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-03-04 09:27:49 +0100

    clean uå

--- source/Tests/Infrastructure/OutgoingMessages/RejectRequestAggregatedMeasureData/RejectRequestAggregatedMeasureDataResultDocumentWriterTests.cs
+++ source/Tests/Infrastructure/OutgoingMessages/RejectRequestAggregatedMeasureData/RejectRequestAggregatedMeasureDataResultDocumentWriterTests.cs
@@ -17,13 +17,11 @@ using System.Threading.Tasks;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using Energinet.DataHub.EDI.Common.Serialization;
 using Energinet.DataHub.EDI.OutgoingMessages.Application;
-using Energinet.DataHub.EDI.OutgoingMessages.Application.MarketDocuments;
 using Energinet.DataHub.EDI.OutgoingMessages.Application.MarketDocuments.RejectRequestAggregatedMeasureData;
 using Energinet.DataHub.EDI.OutgoingMessages.Domain.OutgoingMessages.Queueing;
 using Energinet.DataHub.EDI.Tests.Factories;
 using Energinet.DataHub.EDI.Tests.Fixtures;
 using Energinet.DataHub.EDI.Tests.Infrastructure.OutgoingMessages.Asserts;
-using FluentAssertions;
 using Xunit;
 
 namespace Energinet.DataHub.EDI.Tests.Infrastructure.OutgoingMessages.RejectRequestAggregatedMeasureData;
