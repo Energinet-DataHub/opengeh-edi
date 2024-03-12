d8d4492054160c65280c78b78ef71f864c941b67
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-03-12 09:26:26 +0100

    not allow null for chargeType type

--- source/ApplyDBMigrationsApp/Scripts/Model/202403111207 Create ChargeTypes Table.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202403111207 Create ChargeTypes Table.sql
@@ -4,7 +4,7 @@
     [ChargeTypeId]                     [uniqueidentifier]   NOT NULL,
     [WholesaleServicesProcessId]       [uniqueidentifier]   NOT NULL,
     [Id]                               [nvarchar](16)       NULL,
-    [Type]                             [nvarchar](3)        NOT NULL,
+    [Type]                             [nvarchar](3)        NULL,
     CONSTRAINT [PK_WholesaleServicesProcessChargeTypes] PRIMARY KEY CLUSTERED ([ChargeTypeId] ASC) ON [PRIMARY],
     CONSTRAINT [FK_WholesaleServicesProcessId] FOREIGN KEY ([WholesaleServicesProcessId]) REFERENCES [dbo].[WholesaleServicesProcesses] ([ProcessId])
 )   
