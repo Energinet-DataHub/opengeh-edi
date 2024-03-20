4c2c3acc00b0b6b5bac6c58ef4726f41a4dbbe93
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-03-20 10:11:13 +0100

    Update 202403201000 Delete commandVersion column from enqueued internal commands cleanup on d002.sql

--- source/ApplyDBMigrationsApp/Scripts/Model/202403201000 Delete commandVersion column from enqueued internal commands cleanup on d002.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202403201000 Delete commandVersion column from enqueued internal commands cleanup on d002.sql
@@ -1,5 +1,2 @@
 ﻿ALTER TABLE [dbo].[QueuedInternalCommands]
 DROP COLUMN CommandVersion
-
-DELETE FROM [dbo].[SchemaVersions]
-WHERE ScriptName = 'Energinet.DataHub.EDI.ApplyDBMigrationsApp.Scripts.Model.202403191100 Add version column to QueuedInternalCommands.sql'
