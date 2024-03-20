0135fa4d26bfd23376b264e01611dd674eb9bf87
Lasse Ringgren Nielsen <sixxth@gmail.com>
2024-03-20 13:31:21 +0100

    wip

---
+++ source/IntegrationEvents/IntegrationEvents.Infrastructure/EventProcessors/MessageDelegationConfiguredEventProcessor.cs
@@ -0,0 +1,46 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Energinet.DataHub.Core.Messaging.Communication;
+using Energinet.DataHub.EDI.MasterData.Interfaces;
+using Energinet.DataHub.EDI.MasterData.Interfaces.Models;
+using Energinet.DataHub.MarketParticipant.Infrastructure.Model.Contracts;
+
+namespace Energinet.DataHub.EDI.IntegrationEvents.Infrastructure.EventProcessors;
+
+public class MessageDelegationConfiguredEventProcessor : IIntegrationEventProcessor
+{
+    private readonly IMasterDataClient _masterDataClient;
+
+    public MessageDelegationConfiguredEventProcessor(IMasterDataClient masterDataClient)
+    {
+        _masterDataClient = masterDataClient;
+    }
+
+    public string EventTypeToHandle => MessageDelegationConfigured.EventName;
+
+    public async Task ProcessAsync(IntegrationEvent integrationEvent, CancellationToken cancellationToken)
+    {
+        ArgumentNullException.ThrowIfNull(integrationEvent);
+
+        var message = (MessageDelegationConfigured)integrationEvent.Message;
+        await _masterDataClient.CreateMessageDelegationConfiguredAsync(
+            new MessageDelegationDto(),
+            cancellationToken).ConfigureAwait(false);
+        throw new System.NotImplementedException();
+    }
+}
