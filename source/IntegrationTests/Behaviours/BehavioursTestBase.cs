a642234342dfc70fd3a7d212c7dbc9b5ef837ed7
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-04-11 13:34:48 +0200

    Build?

--- source/IntegrationTests/Behaviours/BehavioursTestBase.cs
+++ source/IntegrationTests/Behaviours/BehavioursTestBase.cs
@@ -38,7 +38,7 @@ using Energinet.DataHub.EDI.DataAccess.UnitOfWork.Extensions.DependencyInjection
 using Energinet.DataHub.EDI.IncomingMessages.Application.Extensions.DependencyInjection;
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Configuration.DataAccess;
 using Energinet.DataHub.EDI.IncomingMessages.Interfaces;
-using Energinet.DataHub.EDI.IntegrationEvents.Application.Configuration;
+using Energinet.DataHub.EDI.IntegrationEvents.Application.Extensions.DependencyInjection;
 using Energinet.DataHub.EDI.IntegrationTests.EventBuilders;
 using Energinet.DataHub.EDI.IntegrationTests.Fixtures;
 using Energinet.DataHub.EDI.IntegrationTests.Infrastructure.Authentication.MarketActors;
