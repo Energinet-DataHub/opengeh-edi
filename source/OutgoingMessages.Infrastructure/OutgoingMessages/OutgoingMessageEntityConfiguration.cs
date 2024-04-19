d2a29fd0b4816c291399722803cc70a9c4754660
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-04-19 08:58:45 +0200

    updated

--- source/OutgoingMessages.Infrastructure/OutgoingMessages/OutgoingMessageEntityConfiguration.cs
+++ source/OutgoingMessages.Infrastructure/OutgoingMessages/OutgoingMessageEntityConfiguration.cs
@@ -116,6 +116,7 @@ namespace Energinet.DataHub.EDI.OutgoingMessages.Infrastructure.OutgoingMessages
                 });
 
             builder.Property(x => x.CreatedAt);
+
             builder.Property<string>("CreatedBy");
             builder.Property<string?>("ModifiedBy");
             builder.Property<Instant?>("ModifiedAt");
