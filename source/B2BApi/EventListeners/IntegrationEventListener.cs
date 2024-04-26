15ca064c951ea72803a8803fb45ac846025600bd
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-04-26 08:36:09 +0200

    updated

--- source/B2BApi/EventListeners/IntegrationEventListener.cs
+++ source/B2BApi/EventListeners/IntegrationEventListener.cs
@@ -35,7 +35,7 @@ public class IntegrationEventListener
     }
 
     /// <summary>
-    /// Receives messages from the integration event queue and processes them.
+    /// Receives messages from the integration event topic and processes them.
     /// </summary>
     /// <remarks>
     /// Retries are currently handled by the Service Bus to avoid blocking a Azure Function Worker.
