c426658f71c1c5ef600385e47cc4df602c3d411c
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2024-05-14 12:35:28 +0200

    dunno

--- source/Tests/Infrastructure/OutgoingMessages/NotifyWholesaleServices/SampleData.cs
+++ source/Tests/Infrastructure/OutgoingMessages/NotifyWholesaleServices/SampleData.cs
@@ -21,7 +21,7 @@ internal static class SampleData
 {
     #region header attributes
 
-    public static string MessageId => Guid.Parse("11111111-1111-1111-1111-111111111111").ToString("N");
+    public static string MessageId => "11111111-1111-1111-1111-111111111111"[..16]; // Max 16 characters in ebIX
 
     public static ActorNumber SenderId => ActorNumber.Create("5790000000000");
 
