fca23071acecd4e1bbc58760f5ab2be6cd19067b
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-05-16 09:39:04 +0200

    Removed redudant logging registration

--- source/B2BApi/HostFactory.cs
+++ source/B2BApi/HostFactory.cs
@@ -59,8 +59,6 @@ public static class HostFactory
                     services
                         // Logging
                         .AddApplicationInsightsForIsolatedWorker(DomainName)
-                        .ConfigureFunctionsApplicationInsights()
-                        .AddLogging()
 
                         // Health checks
                         .AddHealthChecksForIsolatedWorker()
