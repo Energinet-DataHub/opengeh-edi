64d64c222afc00da9d1833ea90ad57f14ed81dc2
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2024-05-16 08:43:46 +0200

    review comment

--- source/Tests/Infrastructure/OutgoingMessages/NotifyWholesaleServices/NotifyWholesaleServicesDocumentWriterTests.cs
+++ source/Tests/Infrastructure/OutgoingMessages/NotifyWholesaleServices/NotifyWholesaleServicesDocumentWriterTests.cs
@@ -64,7 +64,6 @@ public class NotifyWholesaleServicesDocumentWriterTests : IClassFixture<Document
     [InlineData(nameof(DocumentFormat.Ebix))]
     public async Task Can_create_notifyWholesaleServices_document(string documentFormat)
     {
-        var transactionId = SampleData.TransactionId;
         // Arrange
         var messageBuilder = _wholesaleServicesSeriesBuilder
             .WithMessageId(SampleData.MessageId)
@@ -84,7 +83,7 @@ public class NotifyWholesaleServicesDocumentWriterTests : IClassFixture<Document
             .WithMeasurementUnit(SampleData.MeasurementUnit)
             .WithPriceMeasurementUnit(SampleData.PriceMeasureUnit)
             .WithResolution(SampleData.Resolution)
-            .WithOriginalTransactionIdReference(transactionId)
+            .WithOriginalTransactionIdReference(SampleData.TransactionId)
             .WithPoints(new Collection<WholesaleServicesPoint>() { new(1, 1, 1, SampleData.Quantity, CalculatedQuantityQuality.Calculated) });
 
         // Act
@@ -117,7 +116,7 @@ public class NotifyWholesaleServicesDocumentWriterTests : IClassFixture<Document
             .HasResolution(SampleData.Resolution)
             .HasSumQuantityForPosition(1, SampleData.Quantity)
             .HasProductCode(ProductType.Tariff.Code)
-            .HasOriginalTransactionIdReference(transactionId)
+            .HasOriginalTransactionIdReference(SampleData.TransactionId)
             .SettlementVersionDoesNotExist()
             .DocumentIsValidAsync();
     }
