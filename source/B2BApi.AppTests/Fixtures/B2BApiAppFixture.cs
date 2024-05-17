811baf15e06ac1dfbea1e2ec553727c06fd1fade
Dan Stenrøjl <XDAST@energinet.dk>
2024-05-17 16:29:15 +0200

    Health check tests

---
+++ source/B2BApi.AppTests/Fixtures/B2BApiAppFixture.cs
@@ -0,0 +1,190 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
+using BuildingBlocks.Application.Extensions.Options;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Azurite;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Configuration;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.ServiceBus.ResourceProvider;
+using Energinet.DataHub.Core.TestCommon.Diagnostics;
+using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Configuration.Options;
+using Energinet.DataHub.EDI.IntegrationEvents.Application.Extensions.Options;
+using Energinet.DataHub.EDI.Process.Infrastructure.Configuration.Options;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Energinet.DataHub.EDI.B2BApi.AppTests.Fixtures;
+
+/// <summary>
+/// Support testing B2BApi app.
+/// </summary>
+public class B2BApiAppFixture : IAsyncLifetime
+{
+    public B2BApiAppFixture()
+    {
+        TestLogger = new TestDiagnosticsLogger();
+        IntegrationTestConfiguration = new IntegrationTestConfiguration();
+
+        AzuriteManager = new AzuriteManager(useOAuth: true);
+
+        ServiceBusResourceProvider = new ServiceBusResourceProvider(
+            IntegrationTestConfiguration.ServiceBusConnectionString,
+            TestLogger);
+
+        HostConfigurationBuilder = new FunctionAppHostConfigurationBuilder();
+    }
+
+    public ITestDiagnosticsLogger TestLogger { get; }
+
+    [NotNull]
+    public FunctionAppHostManager? AppHostManager { get; private set; }
+
+    private IntegrationTestConfiguration IntegrationTestConfiguration { get; }
+
+    private AzuriteManager AzuriteManager { get; }
+
+    private ServiceBusResourceProvider ServiceBusResourceProvider { get; }
+
+    private FunctionAppHostConfigurationBuilder HostConfigurationBuilder { get; }
+
+    public async Task InitializeAsync()
+    {
+        // Storage emulator
+        AzuriteManager.StartAzurite();
+
+        // Prepare host settings
+        var port = 8000;
+        var appHostSettings = CreateAppHostSettings("B2BApi", ref port);
+
+        // ServiceBus entities
+        await ServiceBusResourceProvider
+            .BuildTopic("integration-events")
+            .Do(topic => appHostSettings.ProcessEnvironmentVariables
+                .Add($"{IntegrationEventsOptions.SectionName}__{nameof(IntegrationEventsOptions.TopicName)}", topic.Name))
+            .AddSubscription("subscription")
+            .Do(subscription => appHostSettings.ProcessEnvironmentVariables
+                .Add($"{IntegrationEventsOptions.SectionName}__{nameof(IntegrationEventsOptions.SubscriptionName)}", subscription.SubscriptionName))
+            .CreateAsync();
+        await ServiceBusResourceProvider
+            .BuildQueue("edi-inbox")
+            .Do(queue => appHostSettings.ProcessEnvironmentVariables
+                .Add($"{EdiInboxOptions.SectionName}__{nameof(EdiInboxOptions.QueueName)}", queue.Name))
+            .CreateAsync();
+        await ServiceBusResourceProvider
+            .BuildQueue("wholesale-inbox")
+            .Do(queue => appHostSettings.ProcessEnvironmentVariables
+                .Add($"{WholesaleInboxOptions.SectionName}__{nameof(WholesaleInboxOptions.QueueName)}", queue.Name))
+            .CreateAsync();
+        await ServiceBusResourceProvider
+            .BuildQueue("incomming-messages")
+            .Do(queue => appHostSettings.ProcessEnvironmentVariables
+                .Add($"{IncomingMessagesQueueOptions.SectionName}__{nameof(IncomingMessagesQueueOptions.QueueName)}", queue.Name))
+            .CreateAsync();
+
+        // Create and start host
+        AppHostManager = new FunctionAppHostManager(appHostSettings, TestLogger);
+        StartHost(AppHostManager);
+    }
+
+    public async Task DisposeAsync()
+    {
+        AppHostManager.Dispose();
+        AzuriteManager.Dispose();
+        await ServiceBusResourceProvider.DisposeAsync();
+    }
+
+    /// <summary>
+    /// Use this method to attach <paramref name="testOutputHelper"/> to the host logging pipeline.
+    /// While attached, any entries written to host log pipeline will also be logged to xUnit test output.
+    /// It is important that it is only attached while a test i active. Hence, it should be attached in
+    /// the test class constructor; and detached in the test class Dispose method (using 'null').
+    /// </summary>
+    /// <param name="testOutputHelper">If a xUnit test is active, this should be the instance of xUnit's <see cref="ITestOutputHelper"/>;
+    /// otherwise it should be 'null'.</param>
+    public void SetTestOutputHelper(ITestOutputHelper testOutputHelper)
+    {
+        TestLogger.TestOutputHelper = testOutputHelper;
+    }
+
+    private static void StartHost(FunctionAppHostManager hostManager)
+    {
+        IEnumerable<string> hostStartupLog;
+
+        try
+        {
+            hostManager.StartHost();
+        }
+        catch (Exception)
+        {
+            // Function App Host failed during startup.
+            // Exception has already been logged by host manager.
+            hostStartupLog = hostManager.GetHostLogSnapshot();
+
+            if (Debugger.IsAttached)
+                Debugger.Break();
+
+            // Rethrow
+            throw;
+        }
+
+        // Function App Host started.
+        hostStartupLog = hostManager.GetHostLogSnapshot();
+    }
+
+    private static string GetBuildConfiguration()
+    {
+#if DEBUG
+        return "Debug";
+#else
+        return "Release";
+#endif
+    }
+
+    private FunctionAppHostSettings CreateAppHostSettings(string csprojName, ref int port)
+    {
+        var buildConfiguration = GetBuildConfiguration();
+
+        var appHostSettings = HostConfigurationBuilder.CreateFunctionAppHostSettings();
+        appHostSettings.FunctionApplicationPath = $"..\\..\\..\\..\\{csprojName}\\bin\\{buildConfiguration}\\net8.0";
+        appHostSettings.Port = ++port;
+
+        // It seems the host + worker is not ready if we use the default startup log message, so we override it here
+        appHostSettings.HostStartedEvent = "Host lock lease acquired";
+
+        appHostSettings.ProcessEnvironmentVariables.Add(
+            "FUNCTIONS_WORKER_RUNTIME",
+            "dotnet-isolated");
+        appHostSettings.ProcessEnvironmentVariables.Add(
+            "AzureWebJobsStorage",
+            AzuriteManager.FullConnectionString);
+        appHostSettings.ProcessEnvironmentVariables.Add(
+            "APPLICATIONINSIGHTS_CONNECTION_STRING",
+            IntegrationTestConfiguration.ApplicationInsightsConnectionString);
+
+        // ServiceBus connection strings
+        appHostSettings.ProcessEnvironmentVariables.Add(
+            $"{ServiceBusOptions.SectionName}__{nameof(ServiceBusOptions.ManageConnectionString)}",
+            ServiceBusResourceProvider.ConnectionString);
+        appHostSettings.ProcessEnvironmentVariables.Add(
+            $"{ServiceBusOptions.SectionName}__{nameof(ServiceBusOptions.ListenConnectionString)}",
+            ServiceBusResourceProvider.ConnectionString);
+        appHostSettings.ProcessEnvironmentVariables.Add(
+            $"{ServiceBusOptions.SectionName}__{nameof(ServiceBusOptions.SendConnectionString)}",
+            ServiceBusResourceProvider.ConnectionString);
+
+        return appHostSettings;
+    }
+}
