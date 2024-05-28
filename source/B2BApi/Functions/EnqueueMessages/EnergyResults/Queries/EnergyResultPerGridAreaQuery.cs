0e08387140711e36b7f755692c38fcea81b88b59
Dan Stenrøjl <XDAST@energinet.dk>
2024-05-28 13:34:07 +0200

    Query

--- source/B2BApi/Functions/EnqueueMessages/EnergyResults/Queries/EnergyResultPerGridAreaQuery.cs
+++ source/B2BApi/Functions/EnqueueMessages/EnergyResults/Queries/EnergyResultPerGridAreaQuery.cs
@@ -12,28 +12,38 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using Energinet.DataHub.Core.Databricks.SqlStatementExecution;
 
-namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.EnergyResults;
+// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.EnergyResults.Queries;
 
 /// <summary>
-/// The query we must perform on the view.
+/// The query we must perform on the 'Energy Result Per Grid Area' view.
 ///
 /// Keep the code updated with regards to the documentation in confluence in a way
 /// that it is easy to compare (e.g. order of columns).
 /// See confluence: https://energinet.atlassian.net/wiki/spaces/D3/pages/849805314/Calculation+Result+Model#Energy-Result-Points-Per-Grid-Area
 /// </summary>
-public class EnergyResultViewQuery : DatabricksStatement
+public class EnergyResultPerGridAreaQuery(Guid calculationId)
+    : EnergyResultQueryBase(calculationId)
 {
-    private readonly Guid _calculationId;
+    public override string DatabaseName => "wholesale_edi_results";
 
-    public EnergyResultViewQuery(Guid calculationId)
-    {
-        _calculationId = calculationId;
-    }
+    public override string DataObjectName => "energy_result_points_per_ga_v1";
 
-    public static string[] SqlColumnNames { get; } =
-    {
+    public override string[] SqlColumnNames => [
         EnergyResultViewColumnNames.CalculationId,
         EnergyResultViewColumnNames.CalculationType,
         EnergyResultViewColumnNames.CalculationPeriodStart,
@@ -47,20 +57,5 @@ public class EnergyResultViewQuery : DatabricksStatement
         EnergyResultViewColumnNames.Time,
         EnergyResultViewColumnNames.Quantity,
         EnergyResultViewColumnNames.QuantityUnit,
-        EnergyResultViewColumnNames.QuantityQualities,
-    };
-
-    public string ModelName => "wholesale_edi_results";
-
-    public string TableName => "energy_result_points_per_ga_v1";
-
-    protected override string GetSqlStatement()
-    {
-        return $@"
-SELECT {string.Join(", ", SqlColumnNames)}
-FROM {ModelName}.{TableName}
-WHERE {EnergyResultViewColumnNames.CalculationId} = '{_calculationId}'
-ORDER BY {EnergyResultViewColumnNames.ResultId}, {EnergyResultViewColumnNames.Time}
-";
-    }
+        EnergyResultViewColumnNames.QuantityQualities];
 }
