00b8f824516560891d4da1dc9580cc746a2a520a
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-05-30 09:48:23 +0200

    Updated proto

--- source/Process.Infrastructure/Transactions/AggregatedMeasureData/AggregatedMeasureDataRequestFactory.cs
+++ source/Process.Infrastructure/Transactions/AggregatedMeasureData/AggregatedMeasureDataRequestFactory.cs
@@ -12,7 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
 using Azure.Messaging.ServiceBus;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using Energinet.DataHub.EDI.Process.Domain.Transactions.AggregatedMeasureData;
