342f0b7c39bf1b99b634dbd1a21e22b8cb78ccf3
Dan Stenrøjl <XDAST@energinet.dk>
2024-06-12 12:23:35 +0200

    Add EnqueueEnergyResultsForGridAreaOwnersActivity

--- source/B2BApi/Functions/EnqueueMessages/EnqueueMessagesOrchestration.cs
+++ source/B2BApi/Functions/EnqueueMessages/EnqueueMessagesOrchestration.cs
@@ -39,6 +39,10 @@ internal class EnqueueMessagesOrchestration
             nameof(EnqueueEnergyResultsForGridAreaOwnersActivity),
             new EnqueueMessagesInput(input.CalculationId, input.EventId));
 
+        numberOfEnqueuedMessages += await context.CallActivityAsync<int>(
+            nameof(EnqueueEnergyResultsForBalanceResponsiblesActivity),
+            new EnqueueMessagesInput(input.CalculationId, input.EventId));
+
         var messagesWasSuccessfullyEnqueued = numberOfEnqueuedMessages > 0;
 
         await context.CallActivityAsync(
