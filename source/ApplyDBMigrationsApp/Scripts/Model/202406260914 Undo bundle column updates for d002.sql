3189331979c0ece6b7fc9d8c3242fee7feee3131
Ebbe Knudsen <xedkn@energinet.dk>
2024-06-26 16:03:16 +0200

    Update 202406260914 Undo bundle column updates for d002.sql

--- source/ApplyDBMigrationsApp/Scripts/Model/202406260914 Undo bundle column updates for d002.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202406260914 Undo bundle column updates for d002.sql
@@ -1,8 +1,16 @@
 -- Undo bundle column updates for d002 - DO NOT COMMIT TO D001
-ALTER TABLE [dbo].[Bundles] DROP COLUMN IsDequeued;
-ALTER TABLE [dbo].[Bundles] DROP COLUMN IsClosed;
-GO
+DECLARE @ColumnType NVARCHAR(50);
 
-ALTER TABLE [dbo].[Bundles] ADD IsDequeued BIT NOT NULL DEFAULT 0;
-ALTER TABLE [dbo].[Bundles] ADD IsClosed BIT NOT NULL DEFAULT 0;
-GO
+SELECT @ColumnType = [DATA_TYPE] FROM [INFORMATION_SCHEMA].[COLUMNS] WHERE TABLE_NAME = 'Bundles' AND COLUMN_NAME = 'IsDequeued'
+
+IF @ColumnType = 'datetime2'
+BEGIN
+    ALTER TABLE [dbo].[Bundles] DROP COLUMN IsDequeued;
+    ALTER TABLE [dbo].[Bundles] DROP COLUMN IsClosed;
+END
+
+IF @ColumnType = 'datetime2'
+BEGIN
+    ALTER TABLE [dbo].[Bundles] ADD IsDequeued BIT NOT NULL DEFAULT 0;
+    ALTER TABLE [dbo].[Bundles] ADD IsClosed BIT NOT NULL DEFAULT 0;
+END
