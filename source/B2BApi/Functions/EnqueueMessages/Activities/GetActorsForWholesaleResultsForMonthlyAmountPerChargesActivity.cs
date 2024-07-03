f24fb6d3c50d3a37e970faf95e5251d6dd96ef78
Lasse Ringgren Nielsen <sixxth@gmail.com>
2024-07-03 22:09:15 +0200

    wip

--- source/B2BApi/Functions/EnqueueMessages/Activities/GetActorsForWholesaleResultsForMonthlyAmountPerChargesActivity.cs
+++ source/B2BApi/Functions/EnqueueMessages/Activities/GetActorsForWholesaleResultsForMonthlyAmountPerChargesActivity.cs
@@ -24,7 +24,7 @@ using EventId = Energinet.DataHub.EDI.BuildingBlocks.Domain.Models.EventId;
 namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.Activities;
 
 /// <summary>
-/// Enqueue wholesale results for Amount Per Charge to Energy Supplier and ChargeOwner as outgoing messages for the given calculation id.
+/// Enqueue wholesale results for Monthly Amount Per Charge to Energy Supplier and ChargeOwner as outgoing messages for the given calculation id.
 /// </summary>
 public class GetActorsForWholesaleResultsForMonthlyAmountPerChargesActivity(
     ILogger<GetActorsForWholesaleResultsForMonthlyAmountPerChargesActivity> logger,
