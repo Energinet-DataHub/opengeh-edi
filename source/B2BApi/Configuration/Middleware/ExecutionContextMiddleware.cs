906604dd9feb150c494bc9f3c93e4ef75ed534a6
Dan Stenrøjl <XDAST@energinet.dk>
2024-08-23 11:51:12 +0200

    Comment

--- source/B2BApi/Configuration/Middleware/ExecutionContextMiddleware.cs
+++ source/B2BApi/Configuration/Middleware/ExecutionContextMiddleware.cs
@@ -24,6 +24,9 @@ public class ExecutionContextMiddleware : IFunctionsWorkerMiddleware
 {
     private readonly ILogger<ExecutionContextMiddleware> _logger;
 
+    // DO NOT inject scoped services in the middleware constructor.
+    // DO use scoped services in middleware by retrieving them from 'FunctionContext.InstanceServices'
+    // DO NOT store scoped services in fields or properties of the middleware object. See https://github.com/Azure/azure-functions-dotnet-worker/issues/1327#issuecomment-1434408603
     public ExecutionContextMiddleware(ILogger<ExecutionContextMiddleware> logger)
     {
         _logger = logger;
