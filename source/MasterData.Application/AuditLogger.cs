44aa695335545e400db163d9aca487eb4dda2860
Ebbe Knudsen <xedkn@energinet.dk>
2024-08-26 16:24:37 +0200

    Remove unused usings

--- source/MasterData.Application/AuditLogger.cs
+++ source/MasterData.Application/AuditLogger.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System.Buffers.Text;
-using System.Runtime.Serialization.Formatters.Binary;
 using System.Text;
 using System.Text.Json;
 using Energinet.DataHub.EDI.MasterData.Interfaces;
