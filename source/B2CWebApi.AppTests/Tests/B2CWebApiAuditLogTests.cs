b978e1690de8b76297e5cc3500f9346506aef6eb
Ebbe Knudsen <xedkn@energinet.dk>
2024-08-29 10:53:47 +0200

    Update test to verify expected properties

--- source/B2CWebApi.AppTests/Tests/B2CWebApiAuditLogTests.cs
+++ source/B2CWebApi.AppTests/Tests/B2CWebApiAuditLogTests.cs
@@ -14,11 +14,14 @@
 
 using System.Net;
 using System.Text.Json;
+using Energinet.DataHub.EDI.AuditLog;
 using Energinet.DataHub.EDI.AuditLog.AuditLogClient;
 using Energinet.DataHub.EDI.B2CWebApi.AppTests.Fixture;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using NodaTime;
+using NodaTime.Text;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -39,13 +42,13 @@ public class B2CWebApiAuditLogTests : IAsyncLifetime
     {
         return
         [
-            [B2CWebApiRequests.CreateArchivedMessageGetDocumentRequest(), "DataHubAdministrator"],
-            [B2CWebApiRequests.CreateArchivedMessageSearchRequest(), "DataHubAdministrator"],
-            [B2CWebApiRequests.CreateOrchestrationsRequest(), "DataHubAdministrator"],
-            [B2CWebApiRequests.CreateOrchestrationRequest(), "DataHubAdministrator"],
-            [B2CWebApiRequests.CreateOrchestrationTerminateRequest(), "DataHubAdministrator"],
-            [B2CWebApiRequests.CreateRequestAggregatedMeasureDataRequest(), ActorRole.EnergySupplier.Name],
-            [B2CWebApiRequests.CreateRequestWholesaleSettlementRequest(), ActorRole.EnergySupplier.Name],
+            [B2CWebApiRequests.CreateArchivedMessageGetDocumentRequest(), "DataHubAdministrator", AuditLogActivity.ArchivedMessagesGet],
+            [B2CWebApiRequests.CreateArchivedMessageSearchRequest(), "DataHubAdministrator", AuditLogActivity.ArchivedMessagesSearch],
+            [B2CWebApiRequests.CreateOrchestrationsRequest(), "DataHubAdministrator", AuditLogActivity.OrchestrationsSearch],
+            [B2CWebApiRequests.CreateOrchestrationRequest(), "DataHubAdministrator", AuditLogActivity.OrchestrationsGet],
+            [B2CWebApiRequests.CreateOrchestrationTerminateRequest(), "DataHubAdministrator", AuditLogActivity.OrchestrationsTerminate],
+            [B2CWebApiRequests.CreateRequestAggregatedMeasureDataRequest(), ActorRole.EnergySupplier.Name, AuditLogActivity.RequestEnergyResults],
+            [B2CWebApiRequests.CreateRequestWholesaleSettlementRequest(), ActorRole.EnergySupplier.Name, AuditLogActivity.RequestWholesaleResults],
         ];
     }
 
@@ -61,20 +64,35 @@ public class B2CWebApiAuditLogTests : IAsyncLifetime
         return Task.CompletedTask;
     }
 
+    /// <summary>
+    /// Validate that the correct audit log request is sent when a B2C Web API request is performed, according to
+    /// the audit log documentation at: https://github.com/Energinet-DataHub/opengeh-revision-log/blob/main/docs/documentation-for-submitting-audit-logs.md
+    /// </summary>
     [Theory]
     [MemberData(nameof(GetB2CWebApiRequests))]
-    public async Task B2CWebApiRequest_WhenRequestPerformed_AuditLogRequestIsSent(HttpRequestMessage request, string actorRole)
+    public async Task B2CWebApiRequest_WhenRequestPerformed_CorrectAuditLogRequestIsSent(
+        HttpRequestMessage request,
+        string actorRole,
+        AuditLogActivity expectedActivity)
     {
         // Arrange
+        var expectedUserId = Guid.NewGuid();
+        var expectedActorId = Guid.NewGuid();
+        string[] permissions =
+        [
+            "request-wholesale-settlement:view",
+            "request-aggregated-measured-data:view",
+            "calculations:manage",
+        ];
+        var expectedPermissions = string.Join(", ", permissions);
+
         request.Headers.Authorization = await _fixture
             .OpenIdJwtManager
             .JwtProvider
             .CreateInternalTokenAuthenticationHeaderAsync(
-                roles: [
-                    "request-wholesale-settlement:view",
-                    "request-aggregated-measured-data:view",
-                    "calculations:manage",
-                ],
+                userId: expectedUserId.ToString(),
+                actorId: expectedActorId.ToString(),
+                roles: permissions,
                 extraClaims: [
                     new("actornumber", "1234567890123"),
                     new("marketroles", actorRole),
@@ -90,19 +108,35 @@ public class B2CWebApiAuditLogTests : IAsyncLifetime
             .ContainSingle()
             .Subject;
 
-        using var assertionScope = new AssertionScope();
+        AuditLogRequestBody? deserializedBody;
+        using (new AssertionScope())
+        {
+            // => Ensure that the audit log request was successful
+            auditLogCall.Response.StatusCode.Should().Be((int)HttpStatusCode.OK);
 
-        // => Ensure that the audit log request was successful
-        auditLogCall.Response.StatusCode.Should().Be((int)HttpStatusCode.OK);
+            // => Ensure that the audit log request contains a body
+            auditLogCall.Request.Body.Should().NotBeNull();
 
-        // => Ensure that the audit log request contains a body
-        auditLogCall.Request.Body.Should().NotBeNull();
+            // => Ensure that the audit log request body can be deserialized to an instance of AuditLogRequestBody
+            var deserializeBody = () =>
+                JsonSerializer.Deserialize<AuditLogRequestBody>(auditLogCall.Request.Body ?? string.Empty);
 
-        // => Ensure that the audit log request body can be deserialized to an instance of AuditLogRequestBody
-        var deserializeBody = () =>
-            JsonSerializer.Deserialize<AuditLogRequestBody>(auditLogCall.Request.Body ?? string.Empty);
+            deserializedBody = deserializeBody.Should().NotThrow().Subject;
+            deserializedBody.Should().NotBeNull();
+        }
 
-        deserializeBody.Should().NotThrow()
-            .Subject.Should().NotBeNull();
+        deserializedBody!.LogId.Should().NotBeEmpty();
+        deserializedBody.UserId.Should().Be(expectedUserId);
+        deserializedBody.ActorId.Should().Be(expectedActorId);
+        deserializedBody.SystemId.Should().Be(Guid.Parse("688b2dca-7231-490f-a731-d7869d33fe5e")); // EDI subsystem id
+        deserializedBody.Permissions.Should().Be(expectedPermissions);
+        InstantPattern.General.Parse(deserializedBody.OccurredOn).Success.Should().BeTrue($"because {deserializedBody.OccurredOn} should be a valid Instant");
+        deserializedBody.Activity.Should().Be(expectedActivity.Identifier);
+        deserializedBody.Origin.Should().Be(request.RequestUri?.AbsoluteUri);
+        deserializedBody.Payload.Should().NotBeNull();
+        deserializedBody.AffectedEntityType.Should().NotBeNullOrWhiteSpace();
+        deserializedBody.AffectedEntityKey.Should().NotBeNull();
+
+        request.Dispose();
     }
 }
