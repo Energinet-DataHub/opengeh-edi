a82b61ed25bbd76a1fcfca03816ac88c308455b6
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-09-03 14:32:44 +0200

    Removed unsed usings and a bundle delete method

--- source/OutgoingMessages.Domain/DocumentWriters/NotifyWholesaleServices/NotifyWholesaleServicesCimJsonDocumentWriter.cs
+++ source/OutgoingMessages.Domain/DocumentWriters/NotifyWholesaleServices/NotifyWholesaleServicesCimJsonDocumentWriter.cs
@@ -12,13 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Globalization;
-using System.Linq;
 using System.Text.Json;
-using System.Threading.Tasks;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using Energinet.DataHub.EDI.OutgoingMessages.Domain.DocumentWriters.Formats.CIM;
 using Energinet.DataHub.EDI.OutgoingMessages.Domain.DocumentWriters.Formats.CIM.Json;
