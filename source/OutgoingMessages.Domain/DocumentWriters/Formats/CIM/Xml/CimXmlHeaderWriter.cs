d709c166cdb4b622d06fb54cb77579acfc1c7370
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2024-09-04 09:12:25 +0200

    chore: Removed unused usings and a bundle delete method (#1199)

--- source/OutgoingMessages.Domain/DocumentWriters/Formats/CIM/Xml/CimXmlHeaderWriter.cs
+++ source/OutgoingMessages.Domain/DocumentWriters/Formats/CIM/Xml/CimXmlHeaderWriter.cs
@@ -12,8 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Threading.Tasks;
 using System.Xml;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using Energinet.DataHub.EDI.OutgoingMessages.Domain.Models.OutgoingMessages;
--- source/OutgoingMessages.Domain/DocumentWriters/NotifyAggregatedMeasureData/NotifyAggregatedMeasureDataEbixDocumentWriter.cs
+++ source/OutgoingMessages.Domain/DocumentWriters/NotifyAggregatedMeasureData/NotifyAggregatedMeasureDataEbixDocumentWriter.cs
@@ -12,11 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Xml;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using Energinet.DataHub.EDI.OutgoingMessages.Domain.DocumentWriters.Formats;
