77389d117ca924029a2d3dbfc769daf2d1851ced
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-09-23 16:40:22 +0200

    updated

--- source/ApplyDBMigrationsApp/Scripts/Model/202409231616 Ensure all tables has a clustered index on RecordId.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202409231616 Ensure all tables has a clustered index on RecordId.sql
@@ -1,104 +1,99 @@
 -- Actor
 CREATE CLUSTERED INDEX IX_RecordId ON Actor(RecordId)
+GO
 
-go
 -- ActorCertificate
 ALTER TABLE ActorCertificate
-    ADD RecordId int not null;
+    ADD RecordId INT IDENTITY(1,1);
 
 CREATE CLUSTERED INDEX IX_RecordId ON ActorCertificate(RecordId)
-go
+GO
 
 -- ActorMessageQueues
 CREATE CLUSTERED INDEX IX_RecordId ON ActorMessageQueues(RecordId)
-go
+GO
 
 -- AggregatedMeasureDataProcesses
 ALTER TABLE AggregatedMeasureDataProcessGridAreas DROP CONSTRAINT FK_AggregatedMeasureDataProcessGridAreas_AggregatedMeasureDataProcessId
 ALTER TABLE AggregatedMeasureDataProcesses DROP CONSTRAINT PK_AggregatedMeasureDataProcesses
-    go
+    GO
+
 ALTER TABLE AggregatedMeasureDataProcesses ADD CONSTRAINT PK_AggregatedMeasureDataProcesses
     PRIMARY KEY NONCLUSTERED (ProcessId)
 
 ALTER TABLE AggregatedMeasureDataProcessGridAreas  WITH CHECK ADD  CONSTRAINT FK_AggregatedMeasureDataProcessGridAreas_AggregatedMeasureDataProcessId FOREIGN KEY(AggregatedMeasureDataProcessId)
     REFERENCES AggregatedMeasureDataProcesses (ProcessId)
-    go
+    GO
 
 CREATE CLUSTERED INDEX IX_RecordId ON AggregatedMeasureDataProcesses(RecordId)
-go
+GO
 
 -- AggregatedMeasureDataProcessGridAreas
 -- LGTM
 
 -- ArchivedMessages
 ALTER TABLE ArchivedMessages DROP CONSTRAINT PK_ArchivedMessages_Id
-    go
+    GO
+
 ALTER TABLE ArchivedMessages ADD CONSTRAINT PK_ArchivedMessages_Id
     PRIMARY KEY NONCLUSTERED ([Id])
 
 CREATE CLUSTERED INDEX IX_RecordId ON ArchivedMessages(RecordId)
+GO
 
 -- Bundles
 CREATE CLUSTERED INDEX IX_RecordId ON Bundles(RecordId)
+GO
 
 -- GridAreaOwner
-ALTER TABLE GridAreaOwner ADD RecordId int null;
-go
-WITH cte AS (
-    SELECT ROW_NUMBER() OVER (order by SequenceNumber desc) AS rn, Id FROM GridAreaOwner 
-)
-UPDATE GridAreaOwner
-SET RecordId = cte.rn
-    FROM cte
-WHERE GridAreaOwner.Id = cte.Id;
-go
+ALTER TABLE GridAreaOwner ADD RecordId INT IDENTITY(1,1);
+GO
 
 CREATE CLUSTERED INDEX IX_RecordId ON GridAreaOwner(RecordId)
-go 
+GO
 
 -- MarketDocuments
 CREATE CLUSTERED INDEX IX_RecordId ON MarketDocuments(RecordId)
 
 -- MarketEvaluationPoints
 CREATE CLUSTERED INDEX IX_RecordId ON MarketEvaluationPoints(RecordId)
+GO
 
 -- MessageRegistry
 CREATE CLUSTERED INDEX IX_RecordId ON MessageRegistry(RecordId)
-
--- Outbox
--- LGTM
+GO
 
 -- OutgoingMessages
 CREATE CLUSTERED INDEX IX_RecordId ON OutgoingMessages(RecordId)
-
--- ProcessDelegation
--- LGTM
-
--- QueuedInternalCommands
--- LGTM
+GO
 
 -- ReceivedInboxEvents 
 CREATE CLUSTERED INDEX IX_RecordId ON ReceivedInboxEvents(RecordId)
+GO
 
 -- ReceivedIntegrationEvents
 CREATE CLUSTERED INDEX IX_RecordId ON ReceivedIntegrationEvents(RecordId)
+GO
 
 -- TransactionRegistry
 CREATE CLUSTERED INDEX IX_RecordId ON TransactionRegistry(RecordId)
+GO
 
 -- WholesaleServicesProcessChargeTypes
 ALTER TABLE WholesaleServicesProcessChargeTypes DROP CONSTRAINT PK_WholesaleServicesProcessChargeTypes
-    go
+    GO
+
 ALTER TABLE WholesaleServicesProcessChargeTypes ADD CONSTRAINT PK_WholesaleServicesProcessChargeTypes
     PRIMARY KEY NONCLUSTERED (ChargeTypeId)
 
 CREATE CLUSTERED INDEX IX_RecordId ON WholesaleServicesProcessChargeTypes(RecordId)
+GO
 
 -- WholesaleServicesProcesses
 ALTER TABLE WholesaleServicesProcessChargeTypes DROP CONSTRAINT FK_WholesaleServicesProcessId
 ALTER TABLE WholesaleServicesProcessGridAreas DROP CONSTRAINT FK_WholesaleServicesProcessGridAreas_WholesaleServicesProcessId
 ALTER TABLE WholesaleServicesProcesses DROP CONSTRAINT PK_WholesaleServicesProcesses
-    go
+    GO
 
 ALTER TABLE WholesaleServicesProcesses ADD CONSTRAINT PK_WholesaleServicesProcesses
     PRIMARY KEY NONCLUSTERED (ProcessId)
@@ -106,9 +101,6 @@ ALTER TABLE WholesaleServicesProcessGridAreas  WITH CHECK ADD  CONSTRAINT [FK_Wh
     REFERENCES WholesaleServicesProcesses (ProcessId)
 ALTER TABLE WholesaleServicesProcessChargeTypes  WITH CHECK ADD  CONSTRAINT [FK_WholesaleServicesProcessId] FOREIGN KEY(WholesaleServicesProcessId)
     REFERENCES WholesaleServicesProcesses (ProcessId)
-    go
+    GO
 
 CREATE CLUSTERED INDEX IX_RecordId ON WholesaleServicesProcesses(RecordId) -- should delete old clustered index and create new ones
-
--- WholesaleServicesProcessGridAreas
--- LGTM
