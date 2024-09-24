0395ec402ef17ea1b21588b389712d360b7c1033
Lasse Ringgren Nielsen <sixxth@gmail.com>
2024-09-24 13:08:17 +0200

    Added Databasemanager and Fixture to Masterdata integrationtest

---
+++ source/MasterData.Integrationtests/Fixture/MasterDataFixture.cs
@@ -0,0 +1,172 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Azure.Storage.Blobs;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.Azurite;
+using Energinet.DataHub.Core.Messaging.Communication.Extensions.Options;
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Authentication;
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
+using Energinet.DataHub.EDI.MasterData.Application.Extensions.DependencyInjection;
+using Energinet.DataHub.EDI.MasterData.IntegrationTests.Fixture.Database;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace Energinet.DataHub.EDI.MasterData.IntegrationTests.Fixture;
+
+public class MasterDataFixture : IDisposable, IAsyncLifetime
+{
+    private bool _disposed;
+
+    public AzuriteManager AzuriteManager { get; } = new(true);
+
+    public EdiDatabaseManager DatabaseManager { get; set; } = new();
+
+    public ServiceProvider ServiceProvider { get; private set; } = null!;
+
+    protected AuthenticatedActor AuthenticatedActor { get; set; } = null!;
+
+    public void CleanupDatabase()
+    {
+        DatabaseManager.CleanupDatabase();
+    }
+
+    public void CleanupFileStorage(bool disposing = false)
+    {
+        var blobServiceClient = new BlobServiceClient(AzuriteManager.BlobStorageConnectionString);
+
+        var containers = blobServiceClient.GetBlobContainers();
+
+        foreach (var containerToDelete in containers)
+            blobServiceClient.DeleteBlobContainer(containerToDelete.Name);
+
+        if (disposing)
+        {
+            // Cleanup actual Azurite "database" files
+            if (Directory.Exists("__blobstorage__"))
+                Directory.Delete("__blobstorage__", true);
+
+            if (Directory.Exists("__queuestorage__"))
+                Directory.Delete("__queuestorage__", true);
+
+            if (Directory.Exists("__tablestorage__"))
+                Directory.Delete("__tablestorage__", true);
+
+            if (File.Exists("__azurite_db_blob__.json"))
+                File.Delete("__azurite_db_blob__.json");
+
+            if (File.Exists("__azurite_db_blob_extent__.json"))
+                File.Delete("__azurite_db_blob_extent__.json");
+
+            if (File.Exists("__azurite_db_queue__.json"))
+                File.Delete("__azurite_db_queue__.json");
+
+            if (File.Exists("__azurite_db_queue_extent__.json"))
+                File.Delete("__azurite_db_queue_extent__.json");
+
+            if (File.Exists("__azurite_db_table__.json"))
+                File.Delete("__azurite_db_table__.json");
+
+            if (File.Exists("__azurite_db_table_extent__.json"))
+                File.Delete("__azurite_db_table_extent__.json");
+        }
+        else
+        {
+            CreateRequiredContainers();
+        }
+    }
+
+    public async Task InitializeAsync()
+    {
+        await DatabaseManager.CreateDatabaseAsync();
+        AzuriteManager.StartAzurite();
+        CleanupFileStorage();
+        BuildService();
+
+        AuthenticatedActor = ServiceProvider.GetRequiredService<AuthenticatedActor>();
+        AuthenticatedActor.SetAuthenticatedActor(
+            new ActorIdentity(
+                ActorNumber.Create("1234512345888"),
+                restriction: Restriction.None,
+                ActorRole.MeteredDataAdministrator));
+    }
+
+    public async Task DisposeAsync()
+    {
+        Dispose();
+        await Task.CompletedTask;
+    }
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    public void BuildService()
+    {
+        var builder = new ConfigurationBuilder();
+        builder.AddInMemoryCollection(new Dictionary<string, string?>
+        {
+            ["DB_CONNECTION_STRING"] = DatabaseManager.ConnectionString,
+            ["AZURE_STORAGE_ACCOUNT_CONNECTION_STRING"] = AzuriteManager.BlobStorageConnectionString,
+            // TODO: fix this
+            // Archived messages does not depend on ServiceBus, but the dependency injection in building blocks require it :(
+            [$"{ServiceBusNamespaceOptions.SectionName}:{nameof(ServiceBusNamespaceOptions.FullyQualifiedNamespace)}"] = "Fake",
+        });
+
+        var services = new ServiceCollection();
+        var configuration = builder.Build();
+
+        services
+            .AddScoped<AuthenticatedActor>()
+            .AddMasterDataModule(configuration);
+
+        ServiceProvider = services.BuildServiceProvider();
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (_disposed)
+        {
+            return;
+        }
+
+        if (disposing)
+        {
+            CleanupFileStorage(true);
+            DatabaseManager.DeleteDatabase();
+            AzuriteManager.Dispose();
+        }
+
+        _disposed = true;
+    }
+
+    private void CreateRequiredContainers()
+    {
+        List<FileStorageCategory> containerCategories = [
+            FileStorageCategory.ArchivedMessage(),
+        ];
+
+        var blobServiceClient = new BlobServiceClient(AzuriteManager.BlobStorageConnectionString);
+        foreach (var fileStorageCategory in containerCategories)
+        {
+            var container = blobServiceClient.GetBlobContainerClient(fileStorageCategory.Value);
+            var containerExists = container.Exists();
+
+            if (!containerExists)
+                container.Create();
+        }
+    }
+}
