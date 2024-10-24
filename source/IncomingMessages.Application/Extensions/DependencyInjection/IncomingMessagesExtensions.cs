c43a77b56095ba99101f37f4cbc3cfb17f0e3247
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-10-24 14:57:45 +0200

    feat: impl metered data for measurement point ebix parser

--- source/IncomingMessages.Application/Extensions/DependencyInjection/IncomingMessagesExtensions.cs
+++ source/IncomingMessages.Application/Extensions/DependencyInjection/IncomingMessagesExtensions.cs
@@ -27,7 +27,7 @@ using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Configuration.DataAc
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Configuration.Options;
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.MessageParsers;
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.MessageParsers.AggregatedMeasureDataRequestMessageParsers;
-using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.MessageParsers.MeteredDateForMeasurementPointParsers;
+using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.MessageParsers.MeteredDateForMeasurementPointParsers.Ebix;
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.MessageParsers.WholesaleSettlementMessageParsers;
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Repositories.MessageId;
 using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Repositories.TransactionId;
---
+++ source/IncomingMessages.Infrastructure/MessageParsers/BaseParsers/Ebix/MessageHeaderExtractor.cs
@@ -0,0 +1,64 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Xml.Linq;
+
+namespace Energinet.DataHub.EDI.IncomingMessages.Infrastructure.MessageParsers.BaseParsers.Ebix;
+
+internal static class MessageHeaderExtractor
+{
+    private const string Identification = "Identification";
+    private const string DocumentType = "DocumentType";
+    private const string Creation = "Creation";
+    private const string SenderEnergyParty = "SenderEnergyParty";
+    private const string RecipientEnergyParty = "RecipientEnergyParty";
+    private const string EnergyBusinessProcess = "EnergyBusinessProcess";
+    private const string EnergyBusinessProcessRole = "EnergyBusinessProcessRole";
+    private const string EnergyIndustryClassification = "EnergyIndustryClassification";
+
+    public static MessageHeader Extract(
+        XDocument document,
+        XNamespace ns,
+        string headerElementName,
+        string energyContextElementName)
+    {
+        var headerElement = document.Descendants(ns + headerElementName).FirstOrDefault();
+        if (headerElement == null) throw new InvalidOperationException("Header element not found");
+
+        var messageId = headerElement.Element(ns + Identification)?.Value ?? string.Empty;
+        var messageType = headerElement.Element(ns + DocumentType)?.Value ?? string.Empty;
+        var createdAt = headerElement.Element(ns + Creation)?.Value ?? string.Empty;
+        var senderId = headerElement.Element(ns + SenderEnergyParty)?.Element(ns + Identification)?.Value ?? string.Empty;
+        var receiverId = headerElement.Element(ns + RecipientEnergyParty)?.Element(ns + Identification)?.Value ?? string.Empty;
+
+        var energyContextElement = document.Descendants(ns + energyContextElementName).FirstOrDefault();
+        if (energyContextElement == null) throw new InvalidOperationException("Energy Context element not found");
+
+        var businessReason = energyContextElement.Element(ns + EnergyBusinessProcess)?.Value ?? string.Empty;
+        var senderRole = energyContextElement.Element(ns + EnergyBusinessProcessRole)?.Value ?? string.Empty;
+        var businessType = energyContextElement.Element(ns + EnergyIndustryClassification)?.Value;
+
+        return new MessageHeader(
+            messageId,
+            messageType,
+            businessReason,
+            senderId,
+            senderRole,
+            receiverId,
+            // ReceiverRole is not specified in incoming Ebix documents
+            string.Empty,
+            createdAt,
+            businessType);
+    }
+}
