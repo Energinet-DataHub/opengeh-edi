42e0292eb475a42a738cbb880f1e5678f824f920
Mads Magnus Due <madsmagnusdue@hotmail.com>
2024-10-28 15:30:59 +0100

    feat: new ebix response factory in incoming messages

---
+++ source/Tests/CimMessageAdapter/Response/EbixResponseFactoryTests.cs
@@ -0,0 +1,83 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using System.Xml.Linq;
+using Energinet.DataHub.EDI.IncomingMessages.Domain.Validation;
+using Energinet.DataHub.EDI.IncomingMessages.Domain.Validation.ValidationErrors;
+using Energinet.DataHub.EDI.IncomingMessages.Infrastructure.Response;
+using Energinet.DataHub.EDI.IncomingMessages.Interfaces.Models;
+using Xunit;
+using Xunit.Categories;
+
+namespace Energinet.DataHub.EDI.Tests.CimMessageAdapter.Response;
+
+[UnitTest]
+public class EbixResponseFactoryTests
+{
+    private readonly EbixResponseFactory _responseFactory;
+
+    public EbixResponseFactoryTests()
+    {
+        _responseFactory = new EbixResponseFactory();
+    }
+
+    [Fact]
+    public void Generates_single_error_response()
+    {
+        var duplicateMessageIdError = new DuplicateMessageIdDetected("Duplicate message id");
+        var result = Result.Failure(duplicateMessageIdError);
+
+        var response = CreateResponse(result);
+
+        Assert.True(response.IsErrorResponse);
+        AssertHasValue(response, "faultcode", duplicateMessageIdError.Code);
+        AssertHasValue(response, "faultstring", duplicateMessageIdError.Message);
+    }
+
+    [Fact]
+    public void Generates_multiple_errors_response()
+    {
+        var duplicateMessageIdError = new DuplicateMessageIdDetected("Duplicate message id");
+        var duplicateTransactionIdError = new DuplicateTransactionIdDetected("Fake transaction id");
+        var result = Result.Failure(duplicateMessageIdError, duplicateTransactionIdError);
+
+        var response = CreateResponse(result);
+
+        Assert.True(response.IsErrorResponse);
+        AssertHasValue(response, "faultcode", "BadRequest");
+        AssertHasValue(response, "faultstring", "Multiple errors in message");
+        AssertContainsError(response, duplicateMessageIdError);
+        AssertContainsError(response, duplicateTransactionIdError);
+    }
+
+    private static void AssertHasValue(ResponseMessage responseMessage, string elementName, string expectedValue)
+    {
+        var document = XDocument.Parse(responseMessage.MessageBody);
+        Assert.Equal(expectedValue, document?.Element("Error")?.Element(elementName)?.Value);
+    }
+
+    private static void AssertContainsError(ResponseMessage responseMessage, ValidationError validationError)
+    {
+        var document = XDocument.Parse(responseMessage.MessageBody);
+        var errors = document.Element("Error")?.Elements("detail")?.ToList();
+
+        Assert.Contains(errors!, error => error.Element("fault")?.Element("ErrorCode")?.Value == validationError.Code);
+        Assert.Contains(errors!, error => error.Element("fault")?.Element("ErrorText")?.Value == validationError.Message);
+    }
+
+    private ResponseMessage CreateResponse(Result result)
+    {
+        return _responseFactory.From(result);
+    }
+}
