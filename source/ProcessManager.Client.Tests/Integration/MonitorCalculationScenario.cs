cd7a8fbbedb479f0bbc0d2c92a46790b51e5aab7
Dan Stenrøjl <XDAST@energinet.dk>
2024-11-02 13:43:12 +0100

    Cleanup usings

--- source/ProcessManager.Client.Tests/Integration/MonitorCalculationScenario.cs
+++ source/ProcessManager.Client.Tests/Integration/MonitorCalculationScenario.cs
@@ -12,9 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-using AutoFixture;
 using Energinet.DataHub.ProcessManager.Client.Tests.Fixtures;
-using Energinet.DataHub.ProcessManager.Tests.Fixtures;
 using Xunit.Abstractions;
 
 namespace Energinet.DataHub.ProcessManager.Client.Tests.Integration;
