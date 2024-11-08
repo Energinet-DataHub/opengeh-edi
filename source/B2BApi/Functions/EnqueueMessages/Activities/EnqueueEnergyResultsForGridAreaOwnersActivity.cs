aec79c4d8466b6d2de661ee4fab27401b5e1ab0d
Mathias Ormstrup Bjerregaard <mwo@energinet.dk>
2024-11-08 13:00:09 +0100

    fixup! rewritten to new activity (tests do not compile yet)

--- source/B2BApi/Functions/EnqueueMessages/Activities/EnqueueEnergyResultsForGridAreaOwnersActivity.cs
+++ source/B2BApi/Functions/EnqueueMessages/Activities/EnqueueEnergyResultsForGridAreaOwnersActivity.cs
@@ -30,12 +30,10 @@ namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.Activities;
 public class EnqueueEnergyResultsForGridAreaOwnersActivity(
     ILogger<EnqueueEnergyResultsForGridAreaOwnersActivity> logger,
     IServiceScopeFactory serviceScopeFactory,
-    IMasterDataClient masterDataClient,
     EnergyResultEnumerator energyResultEnumerator)
     : EnqueueEnergyResultsBaseActivity(logger, serviceScopeFactory, energyResultEnumerator)
 {
     private readonly ILogger<EnqueueEnergyResultsForGridAreaOwnersActivity> _logger = logger;
-    private readonly IMasterDataClient _masterDataClient = masterDataClient;
     private readonly EnergyResultEnumerator _energyResultEnumerator = energyResultEnumerator;
 
     [Function(nameof(EnqueueEnergyResultsForGridAreaOwnersActivity))]
