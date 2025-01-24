2bbb20f4760946e7eb647cf23d7bebb1d85d6e5b
Lasse Ringgren Nielsen <sixxth@gmail.com>
2025-01-24 11:38:41 +0100

    Update DataLakeFileStorageClientTests.cs

--- source/Tests/BuildingBlocks/FileStorageClient/DataLakeFileStorageClientTests.cs
+++ source/Tests/BuildingBlocks/FileStorageClient/DataLakeFileStorageClientTests.cs
@@ -29,57 +29,53 @@ namespace Energinet.DataHub.EDI.Tests.BuildingBlocks.FileStorageClient;
 public class DataLakeFileStorageClientTests
 {
     private readonly DataLakeFileStorageClient _sut;
-    private readonly Mock<BlobContainerClient> _blobContainerClientMock;
-    private readonly Mock<BlobContainerClient> _blobContainerClientObsoletedMock;
-    private readonly Mock<BlobClient> _blobClientMock;
+    private Mock<BlobContainerClient> _blobContainerClientMock;
+    private Mock<BlobContainerClient> _blobContainerClientObsoletedMock;
+    private Mock<BlobClient> _blobClientMock;
 
+#pragma warning disable CS8618, CS9264
     public DataLakeFileStorageClientTests()
+#pragma warning restore CS8618, CS9264
     {
-        Mock<IAzureClientFactory<BlobServiceClient>> clientFactoryMock = new();
-        Mock<BlobServiceClient> blobServiceClientMock = new();
-        _blobContainerClientMock = new();
-        _blobClientMock = new();
-
-        var options = Options.Create(
-            new BlobServiceClientConnectionOptions
-            {
-                ClientName = "ClientName",
-                ClientNameObsoleted = "ClientNameObsoleted",
-            });
-
-        clientFactoryMock
-            .Setup(x => x.CreateClient(options.Value.ClientName))
-            .Returns(blobServiceClientMock.Object);
-
-        blobServiceClientMock
-            .Setup(x => x.GetBlobContainerClient(It.IsAny<string>()))
-            .Returns(_blobContainerClientMock.Object);
-
-        _blobContainerClientMock
-            .Setup(x => x.GetBlobClient(It.IsAny<string>()))
-            .Returns(_blobClientMock.Object);
-
-        // Obsoleted client
-        Mock<BlobServiceClient> blobServiceClientObsoletedMock = new();
-        _blobContainerClientObsoletedMock = new();
-        clientFactoryMock
-            .Setup(x => x.CreateClient(options.Value.ClientNameObsoleted))
-            .Returns(blobServiceClientObsoletedMock.Object);
-
-        blobServiceClientObsoletedMock
-            .Setup(x => x.GetBlobContainerClient(It.IsAny<string>()))
-            .Returns(_blobContainerClientObsoletedMock.Object);
-
-        _blobContainerClientObsoletedMock
-            .Setup(x => x.GetBlobClient(It.IsAny<string>()))
-            .Returns(_blobClientMock.Object);
-        // End: Obsoleted client
-
+        var options = GetOptions();
+        var clientFactoryMock = GetClientFactoryMock(options);
         Mock<IFeatureFlagManager> featureFlagManager = new();
         featureFlagManager
             .Setup(x => x.UseStandardBlobServiceClientAsync())
             .ReturnsAsync(true);
         _sut = new DataLakeFileStorageClient(clientFactoryMock.Object, options, featureFlagManager.Object);
+    }
+
+    [Fact]
+    public async Task Given_UploadAsync_When_UseStandardBlobServiceClientAsyncIsFalse_Then_ObsoleteBlobClientIsCalled()
+    {
+        // Arrange
+        var reference = new FileStorageReference(FileStorageCategory.ArchivedMessage(), "path");
+        var content = "test content";
+        var stream = new MemoryStream();
+        var writer = new StreamWriter(stream);
+        await writer.WriteAsync(content);
+        await writer.FlushAsync();
+        stream.Position = 0;
+
+        Mock<IFeatureFlagManager> featureFlagManager = new();
+        featureFlagManager
+            .Setup(x => x.UseStandardBlobServiceClientAsync())
+            .ReturnsAsync(false);
+
+        var sut = new DataLakeFileStorageClient(GetClientFactoryMock(GetOptions()).Object, GetOptions(), featureFlagManager.Object);
+
+        // Act
+        await sut.UploadAsync(reference, stream);
+
+        // Assert
+        _blobContainerClientObsoletedMock.Verify(
+            x => x.UploadBlobAsync(reference.Path, stream, It.IsAny<CancellationToken>()),
+            Times.Once);
+
+        _blobContainerClientMock.Verify(
+            x => x.UploadBlobAsync(reference.Path, stream, It.IsAny<CancellationToken>()),
+            Times.Never);
     }
 
     [Fact]
@@ -98,6 +94,10 @@ public class DataLakeFileStorageClientTests
         await _sut.UploadAsync(reference, stream);
 
         // Assert
+        _blobContainerClientObsoletedMock.Verify(
+            x => x.UploadBlobAsync(reference.Path, stream, It.IsAny<CancellationToken>()),
+            Times.Never);
+
         _blobContainerClientMock.Verify(
             x => x.UploadBlobAsync(reference.Path, stream, It.IsAny<CancellationToken>()),
             Times.Once);
@@ -146,5 +146,52 @@ public class DataLakeFileStorageClientTests
         _blobContainerClientObsoletedMock.Verify(
             x => x.GetBlobClient(reference.Path),
             Times.Never);
+    }
+
+    private static IOptions<BlobServiceClientConnectionOptions> GetOptions()
+    {
+        var options = Options.Create(
+            new BlobServiceClientConnectionOptions
+            {
+                ClientName = "ClientName",
+                ClientNameObsoleted = "ClientNameObsoleted",
+            });
+        return options;
+    }
+
+    private Mock<IAzureClientFactory<BlobServiceClient>> GetClientFactoryMock(IOptions<BlobServiceClientConnectionOptions> options)
+    {
+        Mock<IAzureClientFactory<BlobServiceClient>> clientFactoryMock = new();
+        Mock<BlobServiceClient> blobServiceClientMock = new();
+        _blobContainerClientMock = new();
+        _blobClientMock = new();
+
+        clientFactoryMock
+            .Setup(x => x.CreateClient(options.Value.ClientName))
+            .Returns(blobServiceClientMock.Object);
+
+        blobServiceClientMock
+            .Setup(x => x.GetBlobContainerClient(It.IsAny<string>()))
+            .Returns(_blobContainerClientMock.Object);
+
+        _blobContainerClientMock
+            .Setup(x => x.GetBlobClient(It.IsAny<string>()))
+            .Returns(_blobClientMock.Object);
+
+        // Obsoleted client
+        Mock<BlobServiceClient> blobServiceClientObsoletedMock = new();
+        _blobContainerClientObsoletedMock = new();
+        clientFactoryMock
+            .Setup(x => x.CreateClient(options.Value.ClientNameObsoleted))
+            .Returns(blobServiceClientObsoletedMock.Object);
+
+        blobServiceClientObsoletedMock
+            .Setup(x => x.GetBlobContainerClient(It.IsAny<string>()))
+            .Returns(_blobContainerClientObsoletedMock.Object);
+
+        _blobContainerClientObsoletedMock
+            .Setup(x => x.GetBlobClient(It.IsAny<string>()))
+            .Returns(_blobClientMock.Object);
+        return clientFactoryMock;
     }
 }
