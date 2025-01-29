abb60eb7fb9b24b2548ce6a9af423d784750f835
Lasse Ringgren Nielsen <sixxth@gmail.com>
2025-01-29 12:40:59 +0100

    fix

--- source/Tests/Infrastructure/IncomingMessages/RequestProcessOrchestrationStarterTests.cs
+++ source/Tests/Infrastructure/IncomingMessages/RequestProcessOrchestrationStarterTests.cs
@@ -164,7 +164,6 @@ public class RequestProcessOrchestrationStarterTests
         var expectedBusinessReason = BusinessReason.BalanceFixing;
         var messageId = "9b6184af-2f05-40b9-d783-08dc814df95a";
         var transactionId = "85f00b2e-cbfa-4b17-86e0-b9004d683f9f";
-        var messageId = "996D9155-DB5E-4B4C-817D-8B880B59E611";
         var expectedStart = "2023-04-30T22:00:00Z";
         var expectedSettlementVersion = settlementVersionCode is not null
             ? SettlementVersion.FromCode(settlementVersionCode)
