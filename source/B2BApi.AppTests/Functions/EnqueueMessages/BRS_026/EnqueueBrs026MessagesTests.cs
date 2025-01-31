a367e996e6c579332fad36f1fadfe9fd64ef068a
Ebbe Knudsen <xedkn@energinet.dk>
2025-01-31 13:55:50 +0100

    Update EnqueueBrs026MessagesTests.cs

--- source/B2BApi.AppTests/Functions/EnqueueMessages/BRS_026/EnqueueBrs026MessagesTests.cs
+++ source/B2BApi.AppTests/Functions/EnqueueMessages/BRS_026/EnqueueBrs026MessagesTests.cs
@@ -180,7 +180,7 @@ public class EnqueueBrs026MessagesTests : IAsyncLifetime
         using var assertionScope = new AssertionScope();
         actualOutgoingMessage!.BusinessReason.Should().Be(businessReason.Name);
         actualOutgoingMessage.RelatedToMessageId.Should().NotBeNull();
-        actualOutgoingMessage.RelatedToMessageId!.Value.Should().Be(enqueueMessagesData.OriginalMessageId);
+        actualOutgoingMessage.RelatedToMessageId!.Value.Value.Should().Be(enqueueMessagesData.OriginalMessageId);
         actualOutgoingMessage.Receiver.Number.Value.Should().Be(requestedForActorNumber.Value);
         actualOutgoingMessage.Receiver.ActorRole.Name.Should().Be(requestedForActorRole.Name);
     }
