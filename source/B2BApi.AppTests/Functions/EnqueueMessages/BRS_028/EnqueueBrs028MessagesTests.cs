2c2cb169eb9bf8815108d07d0a5d7ca083d88d00
Mads Magnus Due <madsmagnusdue@hotmail.com>
2025-02-03 14:13:29 +0100

    feat: Enqueue BRS-028 reject messages received from Process Manager

--- source/B2BApi.AppTests/Functions/EnqueueMessages/BRS_028/EnqueueBrs028MessagesTests.cs
+++ source/B2BApi.AppTests/Functions/EnqueueMessages/BRS_028/EnqueueBrs028MessagesTests.cs
@@ -13,25 +13,36 @@
 // limitations under the License.
 
 using Energinet.DataHub.Core.FunctionApp.TestCommon.FunctionAppHost;
+using Energinet.DataHub.Core.FunctionApp.TestCommon.ServiceBus.ListenerMock;
 using Energinet.DataHub.Core.TestCommon;
 using Energinet.DataHub.EDI.B2BApi.AppTests.Fixtures;
 using Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.BRS_028;
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
+using Energinet.DataHub.EDI.OutgoingMessages.Infrastructure.DataAccess;
+using Energinet.DataHub.ProcessManager.Abstractions.Components.BusinessValidation;
 using Energinet.DataHub.ProcessManager.Abstractions.Contracts;
-using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Components.Datahub.ValueObjects;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_023_027.V1.Model;
 using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_026_028.BRS_028;
 using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_026_028.BRS_028.V1.Model;
 using Energinet.DataHub.ProcessManager.Shared.Extensions;
 using FluentAssertions;
 using FluentAssertions.Execution;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 using NodaTime;
 using Xunit;
 using Xunit.Abstractions;
+using ActorNumber = Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Components.Datahub.ValueObjects.ActorNumber;
+using ActorRole = Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Components.Datahub.ValueObjects.ActorRole;
+using BusinessReason = Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Components.Datahub.ValueObjects.BusinessReason;
 
 namespace Energinet.DataHub.EDI.B2BApi.AppTests.Functions.EnqueueMessages.BRS_028;
 
 [Collection(nameof(B2BApiAppCollectionFixture))]
 public class EnqueueBrs028MessagesTests : IAsyncLifetime
 {
+    // This string must match the subject defined in the "ProcessManagerMessageClient" from the process manager
+    private const string NotifyOrchestrationInstanceSubject = "NotifyOrchestration";
     private readonly B2BApiAppFixture _fixture;
 
     public EnqueueBrs028MessagesTests(
@@ -105,5 +116,97 @@ public class EnqueueBrs028MessagesTests : IAsyncLifetime
         didFinish.Should().BeTrue($"because the {nameof(EnqueueTrigger_Brs_028)} should have been executed");
         hostLog.Should().ContainMatch($"*Executed 'Functions.{nameof(EnqueueTrigger_Brs_028)}' (Succeeded,*");
         hostLog.Should().ContainMatch("*Received enqueue accepted message(s) for BRS 028*");
+    }
+
+    [Fact]
+    public async Task Given_EnqueueRejectBrs028Message_When_MessageIsReceived_Then_RejectedMessagesIsEnqueued()
+    {
+        // => Given enqueue rejected BRS-028 service bus message
+        var eventId = EventId.From(Guid.NewGuid());
+        var actorId = Guid.NewGuid().ToString();
+        var requestedForActorNumber = ActorNumber.Create("1111111111111");
+        var requestedForActorRole = ActorRole.EnergySupplier;
+        var businessReason = BusinessReason.BalanceFixing;
+        var orchestrationInstanceId = Guid.NewGuid().ToString();
+        var enqueueMessagesData = new RequestCalculatedWholesaleServicesRejectedV1(
+            OriginalMessageId: Guid.NewGuid().ToString(),
+            OriginalTransactionId: Guid.NewGuid().ToString(),
+            BusinessReason: businessReason,
+            RequestedForActorNumber: requestedForActorNumber,
+            RequestedForActorRole: requestedForActorRole,
+            RequestedByActorNumber: requestedForActorNumber,
+            RequestedByActorRole: requestedForActorRole,
+            ValidationErrors: [
+                new ValidationErrorDto(
+                    ErrorCode: "T01",
+                    Message: "Test error message 1"),
+                new ValidationErrorDto(
+                    ErrorCode: "T02",
+                    Message: "Test error message 2"),
+            ]);
+        var enqueueActorMessages = new EnqueueActorMessagesV1
+        {
+            OrchestrationName = Brs_028.Name,
+            OrchestrationVersion = 1,
+            OrchestrationStartedByActorId = actorId,
+            OrchestrationInstanceId = orchestrationInstanceId,
+        };
+        enqueueActorMessages.SetData(enqueueMessagesData);
+
+        var serviceBusMessage = enqueueActorMessages.ToServiceBusMessage(
+            subject: EnqueueActorMessagesV1.BuildServiceBusMessageSubject(enqueueActorMessages.OrchestrationName),
+            idempotencyKey: eventId.Value);
+
+        // => When message is received
+        await _fixture.EdiTopicResource.SenderClient.SendMessageAsync(serviceBusMessage);
+
+        // => Then rejected message is enqueued
+        var didFinish = await Awaiter.TryWaitUntilConditionAsync(
+            () => _fixture.AppHostManager.CheckIfFunctionWasExecuted($"Functions.{nameof(EnqueueTrigger_Brs_028)}"),
+            timeLimit: TimeSpan.FromSeconds(30));
+        var hostLog = _fixture.AppHostManager.GetHostLogSnapshot();
+
+        using (new AssertionScope())
+        {
+            didFinish.Should().BeTrue($"because the {nameof(EnqueueTrigger_Brs_028)} should have been executed");
+            hostLog.Should().ContainMatch($"*Executed 'Functions.{nameof(EnqueueTrigger_Brs_028)}' (Succeeded,*");
+            hostLog.Should().ContainMatch("*Received enqueue rejected message(s) for BRS 028*");
+        }
+
+        await using var dbContext = _fixture.DatabaseManager.CreateDbContext<ActorMessageQueueContext>();
+        var actualOutgoingMessage = await dbContext.OutgoingMessages
+            .SingleOrDefaultAsync(om => om.EventId == eventId);
+
+        actualOutgoingMessage.Should().NotBeNull();
+
+        using var assertionScope = new AssertionScope();
+        actualOutgoingMessage!.DocumentType.Should().Be(DocumentType.RejectRequestWholesaleSettlement);
+        actualOutgoingMessage.BusinessReason.Should().Be(businessReason.Name);
+        actualOutgoingMessage.RelatedToMessageId.Should().NotBeNull();
+        actualOutgoingMessage.RelatedToMessageId!.Value.Value.Should().Be(enqueueMessagesData.OriginalMessageId);
+        actualOutgoingMessage.Receiver.Number.Value.Should().Be(requestedForActorNumber.Value);
+        actualOutgoingMessage.Receiver.ActorRole.Name.Should().Be(requestedForActorRole.Name);
+
+        // => Verify that the expected message was sent on the ServiceBus
+        var verifyServiceBusMessages = await _fixture.ServiceBusListenerMock
+            .When(msg =>
+            {
+                if (msg.Subject != NotifyOrchestrationInstanceSubject)
+                {
+                    return false;
+                }
+
+                var parsedNotification = NotifyOrchestrationInstanceV1.Parser.ParseJson(
+                    msg.Body.ToString());
+
+                var matchingOrchestrationId = parsedNotification.OrchestrationInstanceId == orchestrationInstanceId;
+                var matchingEvent = parsedNotification.EventName == RequestCalculatedWholesaleServicesNotifyEventsV1.EnqueueActorMessagesCompleted;
+
+                return matchingOrchestrationId && matchingEvent;
+            })
+            .VerifyCountAsync(1);
+
+        var wait = verifyServiceBusMessages.Wait(TimeSpan.FromSeconds(10));
+        wait.Should().BeTrue("ActorMessagesEnqueuedV1 service bus message should be sent");
     }
 }
--- source/B2BApi/Functions/EnqueueMessages/BRS_028/EnqueueHandler_Brs_028_V1.cs
+++ source/B2BApi/Functions/EnqueueMessages/BRS_028/EnqueueHandler_Brs_028_V1.cs
@@ -12,10 +12,14 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
+using Energinet.DataHub.EDI.OutgoingMessages.Interfaces;
+using Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models.WholesaleResultMessages.Request;
 using Energinet.DataHub.ProcessManager.Abstractions.Api.Model;
 using Energinet.DataHub.ProcessManager.Client;
 using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_026_028.BRS_028.V1.Model;
 using Microsoft.Extensions.Logging;
+using EventId = Energinet.DataHub.EDI.BuildingBlocks.Domain.Models.EventId;
 
 namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.BRS_028;
 
@@ -25,10 +29,12 @@ namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.BRS_028;
 /// <param name="logger"></param>
 public class EnqueueHandler_Brs_028_V1(
     ILogger<EnqueueHandler_Brs_028_V1> logger,
+    IActorRequestsClient actorRequestsClient,
     IProcessManagerMessageClient processManagerMessageClient)
     : EnqueueActorMessagesValidatedHandlerBase<RequestCalculatedWholesaleServicesAcceptedV1, RequestCalculatedWholesaleServicesRejectedV1>(logger)
 {
     private readonly ILogger _logger = logger;
+    private readonly IActorRequestsClient _actorRequestsClient = actorRequestsClient;
     private readonly IProcessManagerMessageClient _processManagerMessageClient = processManagerMessageClient;
 
     protected override async Task EnqueueAcceptedMessagesAsync(
@@ -61,7 +67,30 @@ public class EnqueueHandler_Brs_028_V1(
             "Received enqueue rejected message(s) for BRS 028. Data: {0}",
             rejectedData);
 
-        // TODO: Call actual logic that enqueues rejected message
+        var rejectReasons = rejectedData.ValidationErrors.Select(
+                e => new RejectedWholesaleServicesMessageRejectReason(
+                    ErrorCode: e.ErrorCode,
+                    ErrorMessage: e.Message))
+            .ToList();
+
+        var rejectedTimeSeries = new RejectedWholesaleServicesMessageSeries(
+            TransactionId: TransactionId.New(),
+            RejectReasons: rejectReasons,
+            OriginalTransactionIdReference: TransactionId.From(rejectedData.OriginalTransactionId));
+
+        var rejectedMessageDto = new RejectedWholesaleServicesMessageDto(
+            relatedToMessageId: MessageId.Create(rejectedData.OriginalMessageId),
+            receiverNumber: ActorNumber.Create(rejectedData.RequestedByActorNumber.Value),
+            receiverRole: ActorRole.FromName(rejectedData.RequestedByActorRole.Name),
+            documentReceiverNumber: ActorNumber.Create(rejectedData.RequestedForActorNumber.Value),
+            documentReceiverRole: ActorRole.FromName(rejectedData.RequestedForActorRole.Name),
+            processId: orchestrationInstanceId,
+            eventId: EventId.From(serviceBusMessageId),
+            businessReason: BusinessReason.FromName(rejectedData.BusinessReason.Name).Name,
+            series: rejectedTimeSeries);
+
+        await _actorRequestsClient.EnqueueRejectWholesaleServicesRequestAsync(rejectedMessageDto, cancellationToken)
+            .ConfigureAwait(false);
 
         await _processManagerMessageClient.NotifyOrchestrationInstanceAsync(
                 new NotifyOrchestrationInstanceEvent(
