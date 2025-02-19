f61e0de4f4d16c068f56ce5480a9619f1e9e082e
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2025-02-19 13:34:54 +0100

    Update EnqueueHandler_Brs_028_V1.cs

--- source/B2BApi/Functions/EnqueueMessages/BRS_028/EnqueueHandler_Brs_028_V1.cs
+++ source/B2BApi/Functions/EnqueueMessages/BRS_028/EnqueueHandler_Brs_028_V1.cs
@@ -12,9 +12,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
 using Energinet.DataHub.EDI.BuildingBlocks.Interfaces;
 using Energinet.DataHub.EDI.OutgoingMessages.Interfaces;
-using Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models.CalculationResults;
 using Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models.CalculationResults.WholesaleResults;
 using Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models.WholesaleResultMessages.Request;
 using Energinet.DataHub.ProcessManager.Abstractions.Api.Model;
@@ -23,6 +23,7 @@ using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS
 using Microsoft.Extensions.Logging;
 using NodaTime.Extensions;
 using ActorRole = Energinet.DataHub.ProcessManager.Abstractions.Core.ValueObjects.ActorRole;
+using Period = Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models.CalculationResults.Period;
 using Resolution = Energinet.DataHub.ProcessManager.Components.Abstractions.ValueObjects.Resolution;
 
 namespace Energinet.DataHub.EDI.B2BApi.Functions.EnqueueMessages.BRS_028;
@@ -53,7 +54,7 @@ public class EnqueueHandler_Brs_028_V1(
             acceptedData);
 
         var settlementVersion = acceptedData.SettlementVersion is not null
-            ? BuildingBlocks.Domain.Models.SettlementVersion.FromName(acceptedData.SettlementVersion.Name)
+            ? SettlementVersion.FromName(acceptedData.SettlementVersion.Name)
             : null;
         var queryParams = new WholesaleServicesQueryParameters(
             AmountType: AmountType.AmountPerCharge, // Default to AmountPerCharge, will be replaced in foreach loop
@@ -63,9 +64,9 @@ public class EnqueueHandler_Brs_028_V1(
             ChargeTypes: acceptedData.ChargeTypes.Select(
                     ct => (
                         ct.ChargeCode,
-                        ct.ChargeType == null ? null : BuildingBlocks.Domain.Models.ChargeType.FromName(ct.ChargeType.Name)))
+                        ct.ChargeType == null ? null : ChargeType.FromName(ct.ChargeType.Name)))
                 .ToList(),
-            BusinessReason: BuildingBlocks.Domain.Models.BusinessReason.FromName(acceptedData.BusinessReason.Name),
+            BusinessReason: BusinessReason.FromName(acceptedData.BusinessReason.Name),
             SettlementVersion: settlementVersion,
             Period: new Period(
                 acceptedData.PeriodStart.ToInstant(),
@@ -85,14 +86,14 @@ public class EnqueueHandler_Brs_028_V1(
 
             var enqueuedCount = await _actorRequestsClient.EnqueueWholesaleServicesAsync(
                     wholesaleServicesQueryParameters: queryParamsForAmountType,
-                    requestedByActorNumber: BuildingBlocks.Domain.Models.ActorNumber.Create(acceptedData.RequestedByActorNumber),
+                    requestedByActorNumber: ActorNumber.Create(acceptedData.RequestedByActorNumber),
                     requestedByActorRole: BuildingBlocks.Domain.Models.ActorRole.Create(acceptedData.RequestedByActorRole),
-                    requestedForActorNumber: BuildingBlocks.Domain.Models.ActorNumber.Create(acceptedData.RequestedForActorNumber),
+                    requestedForActorNumber: ActorNumber.Create(acceptedData.RequestedForActorNumber),
                     requestedForActorRole: BuildingBlocks.Domain.Models.ActorRole.Create(acceptedData.RequestedForActorRole),
                     orchestrationInstanceId: orchestrationInstanceId,
                     eventId: BuildingBlocks.Domain.Models.EventId.From(serviceBusMessageId),
-                    originalMessageId: BuildingBlocks.Domain.Models.MessageId.Create(acceptedData.OriginalActorMessageId),
-                    originalTransactionId: BuildingBlocks.Domain.Models.TransactionId.From(acceptedData.OriginalTransactionId),
+                    originalMessageId: MessageId.Create(acceptedData.OriginalActorMessageId),
+                    originalTransactionId: TransactionId.From(acceptedData.OriginalTransactionId),
                     cancellationToken: cancellationToken)
                 .ConfigureAwait(false);
 
@@ -104,15 +105,15 @@ public class EnqueueHandler_Brs_028_V1(
             _logger.LogInformation("No wholesale services messages enqueued for accepted BRS-028, enqueuing rejected message. Data: {0}", acceptedData);
             await _actorRequestsClient.EnqueueRejectWholesaleServicesRequestWithNoDataAsync(
                     queryParams,
-                    requestedByActorNumber: BuildingBlocks.Domain.Models.ActorNumber.Create(acceptedData.RequestedByActorNumber.Value),
+                    requestedByActorNumber: ActorNumber.Create(acceptedData.RequestedByActorNumber.Value),
                     requestedByActorRole: BuildingBlocks.Domain.Models.ActorRole.FromName(acceptedData.RequestedByActorRole.Name),
-                    requestedForActorNumber: BuildingBlocks.Domain.Models.ActorNumber.Create(acceptedData.RequestedForActorNumber.Value),
+                    requestedForActorNumber: ActorNumber.Create(acceptedData.RequestedForActorNumber.Value),
                     requestedForActorRole: BuildingBlocks.Domain.Models.ActorRole.FromName(acceptedData.RequestedForActorRole.Name),
                     orchestrationInstanceId: orchestrationInstanceId,
                     eventId: BuildingBlocks.Domain.Models.EventId.From(serviceBusMessageId),
-                    originalMessageId: BuildingBlocks.Domain.Models.MessageId.Create(acceptedData.OriginalActorMessageId),
-                    originalTransactionId: BuildingBlocks.Domain.Models.TransactionId.From(acceptedData.OriginalTransactionId),
-                    businessReason: BuildingBlocks.Domain.Models.BusinessReason.FromName(acceptedData.BusinessReason.Name),
+                    originalMessageId: MessageId.Create(acceptedData.OriginalActorMessageId),
+                    originalTransactionId: TransactionId.From(acceptedData.OriginalTransactionId),
+                    businessReason: BusinessReason.FromName(acceptedData.BusinessReason.Name),
                     cancellationToken: cancellationToken)
                 .ConfigureAwait(false);
         }
@@ -144,19 +145,19 @@ public class EnqueueHandler_Brs_028_V1(
             .ToList();
 
         var rejectedTimeSeries = new RejectedWholesaleServicesMessageSeries(
-            TransactionId: BuildingBlocks.Domain.Models.TransactionId.New(),
+            TransactionId: TransactionId.New(),
             RejectReasons: rejectReasons,
-            OriginalTransactionIdReference: BuildingBlocks.Domain.Models.TransactionId.From(rejectedData.OriginalTransactionId));
+            OriginalTransactionIdReference: TransactionId.From(rejectedData.OriginalTransactionId));
 
         var rejectedMessageDto = new RejectedWholesaleServicesMessageDto(
-            relatedToMessageId: BuildingBlocks.Domain.Models.MessageId.Create(rejectedData.OriginalMessageId),
-            receiverNumber: BuildingBlocks.Domain.Models.ActorNumber.Create(rejectedData.RequestedByActorNumber.Value),
+            relatedToMessageId: MessageId.Create(rejectedData.OriginalMessageId),
+            receiverNumber: ActorNumber.Create(rejectedData.RequestedByActorNumber.Value),
             receiverRole: BuildingBlocks.Domain.Models.ActorRole.FromName(rejectedData.RequestedByActorRole.Name),
-            documentReceiverNumber: BuildingBlocks.Domain.Models.ActorNumber.Create(rejectedData.RequestedForActorNumber.Value),
+            documentReceiverNumber: ActorNumber.Create(rejectedData.RequestedForActorNumber.Value),
             documentReceiverRole: BuildingBlocks.Domain.Models.ActorRole.FromName(rejectedData.RequestedForActorRole.Name),
             processId: orchestrationInstanceId,
             eventId: BuildingBlocks.Domain.Models.EventId.From(serviceBusMessageId),
-            businessReason: BuildingBlocks.Domain.Models.BusinessReason.FromName(rejectedData.BusinessReason.Name).Name,
+            businessReason: BusinessReason.FromName(rejectedData.BusinessReason.Name).Name,
             series: rejectedTimeSeries);
 
         await _actorRequestsClient.EnqueueRejectWholesaleServicesRequestAsync(rejectedMessageDto, cancellationToken)
