2a82b96613e02a82f41fb57216ca1c12c1ee1f5e
Mathias Ormstrup Bjerregaard <152613120+MWO1024@users.noreply.github.com>
2025-02-21 10:03:46 +0100

    feat: Remove PM from EDI (#1510)

--- source/ProcessManager.DatabaseMigration/Scripts/202411192300 Add operating identity columns.sql
+++
@@ -1,8 +0,0 @@
-﻿ALTER TABLE [pm].[OrchestrationInstance]
-    ADD [Lifecycle_CreatedBy_IdentityType]  NVARCHAR(255) NULL,
-        [Lifecycle_CreatedBy_ActorId]       UNIQUEIDENTIFIER NULL,
-        [Lifecycle_CreatedBy_UserId]        UNIQUEIDENTIFIER NULL,
-        [Lifecycle_CanceledBy_IdentityType] NVARCHAR(255) NULL,
-        [Lifecycle_CanceledBy_ActorId]      UNIQUEIDENTIFIER NULL,
-        [Lifecycle_CanceledBy_UserId]       UNIQUEIDENTIFIER NULL
-GO
