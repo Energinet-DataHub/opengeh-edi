53aab5c9177750f671baf58545ae382807343d42
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2025-02-25 09:29:55 +0100

    rewrote metering point type mapper to switch

--- source/OutgoingMessages.Infrastructure/Databricks/CalculationResults/Mappers/WholesaleResults/MeteringPointTypeMapper.cs
+++ source/OutgoingMessages.Infrastructure/Databricks/CalculationResults/Mappers/WholesaleResults/MeteringPointTypeMapper.cs
@@ -20,36 +20,28 @@ public static class MeteringPointTypeMapper
 {
     public static BuildingBlocks.Domain.Models.MeteringPointType? FromDeltaTableValue(string? meteringPointType)
     {
-        if (meteringPointType == null)
-            return null;
-
-        Dictionary<string, BuildingBlocks.Domain.Models.MeteringPointType> meteringPointTypeMap = new()
+        return meteringPointType switch
         {
-            { DeltaTableMeteringPointType.Consumption, BuildingBlocks.Domain.Models.MeteringPointType.Consumption },
-            { DeltaTableMeteringPointType.Production, BuildingBlocks.Domain.Models.MeteringPointType.Production },
-            { DeltaTableMeteringPointType.Exchange, BuildingBlocks.Domain.Models.MeteringPointType.Exchange },
-            { DeltaTableMeteringPointType.VeProduction, BuildingBlocks.Domain.Models.MeteringPointType.VeProduction },
-            { DeltaTableMeteringPointType.NetProduction, BuildingBlocks.Domain.Models.MeteringPointType.NetProduction },
-            { DeltaTableMeteringPointType.SupplyToGrid, BuildingBlocks.Domain.Models.MeteringPointType.SupplyToGrid },
-            { DeltaTableMeteringPointType.ConsumptionFromGrid, BuildingBlocks.Domain.Models.MeteringPointType.ConsumptionFromGrid },
-            { DeltaTableMeteringPointType.WholesaleServicesInformation, BuildingBlocks.Domain.Models.MeteringPointType.WholesaleServicesInformation },
-            { DeltaTableMeteringPointType.OwnProduction, BuildingBlocks.Domain.Models.MeteringPointType.OwnProduction },
-            { DeltaTableMeteringPointType.NetFromGrid, BuildingBlocks.Domain.Models.MeteringPointType.NetFromGrid },
-            { DeltaTableMeteringPointType.NetToGrid, BuildingBlocks.Domain.Models.MeteringPointType.NetToGrid },
-            { DeltaTableMeteringPointType.TotalConsumption, BuildingBlocks.Domain.Models.MeteringPointType.TotalConsumption },
-            { DeltaTableMeteringPointType.ElectricalHeating, BuildingBlocks.Domain.Models.MeteringPointType.ElectricalHeating },
-            { DeltaTableMeteringPointType.NetConsumption, BuildingBlocks.Domain.Models.MeteringPointType.NetConsumption },
-            { DeltaTableMeteringPointType.CapacitySettlement, BuildingBlocks.Domain.Models.MeteringPointType.CapacitySettlement },
-        };
-
-        if (!meteringPointTypeMap.TryGetValue(meteringPointType, out var value))
-        {
-            throw new ArgumentOutOfRangeException(
+            DeltaTableMeteringPointType.Consumption => BuildingBlocks.Domain.Models.MeteringPointType.Consumption,
+            DeltaTableMeteringPointType.Production => BuildingBlocks.Domain.Models.MeteringPointType.Production,
+            DeltaTableMeteringPointType.Exchange => BuildingBlocks.Domain.Models.MeteringPointType.Exchange,
+            DeltaTableMeteringPointType.VeProduction => BuildingBlocks.Domain.Models.MeteringPointType.VeProduction,
+            DeltaTableMeteringPointType.NetProduction => BuildingBlocks.Domain.Models.MeteringPointType.NetProduction,
+            DeltaTableMeteringPointType.SupplyToGrid => BuildingBlocks.Domain.Models.MeteringPointType.SupplyToGrid,
+            DeltaTableMeteringPointType.ConsumptionFromGrid => BuildingBlocks.Domain.Models.MeteringPointType.ConsumptionFromGrid,
+            DeltaTableMeteringPointType.WholesaleServicesInformation => BuildingBlocks.Domain.Models.MeteringPointType.WholesaleServicesInformation,
+            DeltaTableMeteringPointType.OwnProduction => BuildingBlocks.Domain.Models.MeteringPointType.OwnProduction,
+            DeltaTableMeteringPointType.NetFromGrid => BuildingBlocks.Domain.Models.MeteringPointType.NetFromGrid,
+            DeltaTableMeteringPointType.NetToGrid => BuildingBlocks.Domain.Models.MeteringPointType.NetToGrid,
+            DeltaTableMeteringPointType.TotalConsumption => BuildingBlocks.Domain.Models.MeteringPointType.TotalConsumption,
+            DeltaTableMeteringPointType.ElectricalHeating => BuildingBlocks.Domain.Models.MeteringPointType.ElectricalHeating,
+            DeltaTableMeteringPointType.NetConsumption => BuildingBlocks.Domain.Models.MeteringPointType.NetConsumption,
+            DeltaTableMeteringPointType.CapacitySettlement => BuildingBlocks.Domain.Models.MeteringPointType.CapacitySettlement,
+            null => null,
+            _ => throw new ArgumentOutOfRangeException(
                 nameof(meteringPointType),
                 actualValue: meteringPointType,
-                "Value does not contain a valid string representation of a metering point type.");
-        }
-
-        return value;
+                "Value does not contain a valid string representation of a metering point type."),
+        };
     }
 }
