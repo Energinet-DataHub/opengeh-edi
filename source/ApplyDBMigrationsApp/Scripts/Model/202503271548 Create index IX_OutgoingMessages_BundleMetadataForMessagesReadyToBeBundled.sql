6c6d83a99962a770e8dcfa1ebc6bbb772963ebd0
Ebbe Knudsen <xedkn@energinet.dk>
2025-03-28 09:38:12 +0100

    Add missing index

--- source/ApplyDBMigrationsApp/Scripts/Model/202503271548 Create index IX_OutgoingMessages_BundleMetadataForMessagesReadyToBeBundled.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202503271548 Create index IX_OutgoingMessages_BundleMetadataForMessagesReadyToBeBundled.sql
@@ -1,9 +1,8 @@
 -- Should match the query in OutgoingMessageRepository.GetBundleMetadataForMessagesReadyToBeBundledAsync() (BundleMetadataDto)
+-- AssignedBundleId does not need to be included as a column since the index is filtered on AssignedBundleId IS NULL
 CREATE INDEX IX_OutgoingMessages_BundleMetadataForMessagesReadyToBeBundled
     ON [dbo].[OutgoingMessages] (
-        AssignedBundleId,
-        CreatedAt)
-    INCLUDE (
+        CreatedAt,
         ReceiverNumber,
         ReceiverRole,
         BusinessReason,
