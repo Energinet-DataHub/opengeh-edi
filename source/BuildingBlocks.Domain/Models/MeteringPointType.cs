7e9f3af37d98a08f28e58afac085879c634d1599
Mads Magnus Due <xmadu@energinet.dk>
2025-04-02 08:17:23 +0200

    feat: ensure all metering point is supported for incoming rsm012 (#1562)

--- source/BuildingBlocks.Domain/Models/MeteringPointType.cs
+++ source/BuildingBlocks.Domain/Models/MeteringPointType.cs
@@ -26,18 +26,26 @@ public class MeteringPointType : DataHubType<MeteringPointType>
     public static readonly MeteringPointType Exchange = new(PMTypes.MeteringPointType.Exchange.Name, "E20");
 
     // Child metering point types
-    public static readonly MeteringPointType VeProduction = new("VeProduction", "D01");
-    public static readonly MeteringPointType NetProduction = new("NetProduction", "D05");
-    public static readonly MeteringPointType SupplyToGrid = new("SupplyToGrid", "D06");
-    public static readonly MeteringPointType ConsumptionFromGrid = new("ConsumptionFromGrid", "D07");
-    public static readonly MeteringPointType WholesaleServicesInformation = new("WholesaleServicesInformation", "D08");
-    public static readonly MeteringPointType OwnProduction = new("OwnProduction", "D09");
-    public static readonly MeteringPointType NetFromGrid = new("NetFromGrid", "D10");
-    public static readonly MeteringPointType NetToGrid = new("NetToGrid", "D11");
-    public static readonly MeteringPointType TotalConsumption = new("TotalConsumption", "D12");
-    public static readonly MeteringPointType ElectricalHeating = new("ElectricalHeating", "D14");
-    public static readonly MeteringPointType NetConsumption = new("NetConsumption", "D15");
-    public static readonly MeteringPointType CapacitySettlement = new("CapacitySettlement", "D19");
+    public static readonly MeteringPointType VeProduction = new(PMTypes.MeteringPointType.VeProduction.Name, "D01");
+    public static readonly MeteringPointType Analysis = new(PMTypes.MeteringPointType.Analysis.Name, "D02");
+    public static readonly MeteringPointType SurplusProductionGroup6 = new(PMTypes.MeteringPointType.SurplusProductionGroup6.Name, "D04");
+    public static readonly MeteringPointType NetProduction = new(PMTypes.MeteringPointType.NetProduction.Name, "D05");
+    public static readonly MeteringPointType SupplyToGrid = new(PMTypes.MeteringPointType.SupplyToGrid.Name, "D06");
+    public static readonly MeteringPointType ConsumptionFromGrid = new(PMTypes.MeteringPointType.ConsumptionFromGrid.Name, "D07");
+    public static readonly MeteringPointType WholesaleServicesInformation = new(PMTypes.MeteringPointType.WholesaleServicesInformation.Name, "D08");
+    public static readonly MeteringPointType OwnProduction = new(PMTypes.MeteringPointType.OwnProduction.Name, "D09");
+    public static readonly MeteringPointType NetFromGrid = new(PMTypes.MeteringPointType.NetFromGrid.Name, "D10");
+    public static readonly MeteringPointType NetToGrid = new(PMTypes.MeteringPointType.NetToGrid.Name, "D11");
+    public static readonly MeteringPointType TotalConsumption = new(PMTypes.MeteringPointType.TotalConsumption.Name, "D12");
+    public static readonly MeteringPointType ElectricalHeating = new(PMTypes.MeteringPointType.ElectricalHeating.Name, "D14");
+    public static readonly MeteringPointType NetConsumption = new(PMTypes.MeteringPointType.NetConsumption.Name, "D15");
+    public static readonly MeteringPointType OtherConsumption = new(PMTypes.MeteringPointType.OtherConsumption.Name, "D17");
+    public static readonly MeteringPointType OtherProduction = new(PMTypes.MeteringPointType.OtherProduction.Name, "D18");
+    public static readonly MeteringPointType CapacitySettlement = new(PMTypes.MeteringPointType.CapacitySettlement.Name, "D19");
+    public static readonly MeteringPointType ExchangeReactiveEnergy = new(PMTypes.MeteringPointType.ExchangeReactiveEnergy.Name, "D20");
+    public static readonly MeteringPointType CollectiveNetProduction = new(PMTypes.MeteringPointType.CollectiveNetProduction.Name, "D21");
+    public static readonly MeteringPointType CollectiveNetConsumption = new(PMTypes.MeteringPointType.CollectiveNetConsumption.Name, "D22");
+    public static readonly MeteringPointType InternalUse = new(PMTypes.MeteringPointType.InternalUse.Name, "D99");
 
     [JsonConstructor]
     private MeteringPointType(string name, string code)
