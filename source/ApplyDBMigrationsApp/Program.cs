b414eaf0767d55af48e9546d6e53b18ff06d3c68
Mads Magnus Due <madsmagnusdue@hotmail.com>
2025-04-14 20:54:00 +0200

    clean up

--- source/ApplyDBMigrationsApp/Program.cs
+++ source/ApplyDBMigrationsApp/Program.cs
@@ -30,6 +30,6 @@ public static class Program
 
     private static string ParseConnectionStringFrom(string[] args)
     {
-        return "Data Source=(LocalDB)\\MSSQLLocalDB;Integrated Security=true;Database=ArchivedMessages;Trust Server Certificate=True;";
+        return args.First();
     }
 }
