698b189531faf3349dd997c1e29e76faa2cef226
Ebbe Knudsen <xedkn@energinet.dk>
2025-05-15 11:00:49 +0200

    Update 202505141542 Create ExternalId_NEW as VARCHAR on OutgoingMessages table.sql

--- source/ApplyDBMigrationsApp/Scripts/Model/202505141542 Create ExternalId_NEW as VARCHAR on OutgoingMessages table.sql
+++ source/ApplyDBMigrationsApp/Scripts/Model/202505141542 Create ExternalId_NEW as VARCHAR on OutgoingMessages table.sql
@@ -5,7 +5,7 @@ BEGIN TRANSACTION
     GO
 
     UPDATE [dbo].[OutgoingMessages]
-        SET [ExternalId_NEW] = LOWER(CONVERT(VARCHAR(36), [ExternalId]))
+        SET [ExternalId_NEW] = LOWER(CONVERT(VARCHAR(36), [ExternalId])) -- We must use LOWER() to match Guid.ToString() C# method.
 
     ALTER TABLE [dbo].[OutgoingMessages]
         ALTER COLUMN [ExternalId_NEW] VARCHAR(36) NOT NULL
