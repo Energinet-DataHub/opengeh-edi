485e617aa87c4a3c23ca0169228a9857995f5fe1
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2025-05-16 11:04:12 +0200

    Correct business reasons

--- source/BuildingBlocks.Domain/Models/BusinessReason.cs
+++ source/BuildingBlocks.Domain/Models/BusinessReason.cs
@@ -26,7 +26,7 @@ public sealed class BusinessReason : DataHubType<BusinessReason>
     public static readonly BusinessReason Correction = new(PMTypes.BusinessReason.Correction.Name, "D32", 5);
     public static readonly BusinessReason PeriodicMetering = new(PMTypes.BusinessReason.PeriodicMetering.Name, "E23", 6);
     public static readonly BusinessReason PeriodicFlexMetering = new(PMTypes.BusinessReason.PeriodicFlexMetering.Name, "D42", 7);
-    public static readonly BusinessReason ReminderOfMissingMeasurementLog = new(nameof(ReminderOfMissingMeasurementLog), "D26", 8);
+    public static readonly BusinessReason ReminderOfMissingMeasurementLog = new(nameof(ReminderOfMissingMeasurementLog), "D24", 8);
 
     /// <summary>
     /// Represents the business reason in the Edi system.
