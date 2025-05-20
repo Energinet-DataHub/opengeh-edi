4e5789d3aa44c24bedcfccdc13cfc85fd8426d01
RasmusGraabaek <rasmus.graabaek@hotmail.com>
2025-05-20 10:37:43 +0200

    Test of isEic and isGln

--- source/BuildingBlocks.Domain/Models/ActorNumber.cs
+++ source/BuildingBlocks.Domain/Models/ActorNumber.cs
@@ -52,6 +52,8 @@ public class ActorNumber : ValueObject
 
     public static bool IsEic(string actorNumber)
     {
+        // There is more to a valid EIC than just the length. https://en.wikipedia.org/wiki/Energy_Identification_Code
+        // The responsible for validating that is not for EDI.
         ArgumentNullException.ThrowIfNull(actorNumber);
         return actorNumber.Length == 16;
     }
