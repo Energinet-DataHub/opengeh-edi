618d9bd0a15a6d6bfe9b0a58c3f6685205b57212
Mads Magnus Due <madsmagnusdue@hotmail.com>
2025-05-28 12:53:53 +0200

    test: complete brs024 accepted behavior test

--- source/IntegrationTests/Behaviours/IncomingRequests/GivenRequestMeasurementsTests.cs
+++ source/IntegrationTests/Behaviours/IncomingRequests/GivenRequestMeasurementsTests.cs
@@ -12,11 +12,21 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+using System.Globalization;
 using Energinet.DataHub.EDI.BuildingBlocks.Domain.Models;
-using Energinet.DataHub.EDI.BuildingBlocks.Tests.TestDoubles;
+using Energinet.DataHub.EDI.IntegrationTests.EventBuilders;
 using Energinet.DataHub.EDI.IntegrationTests.Fixtures;
+using Energinet.DataHub.EDI.OutgoingMessages.Application.Extensions.Options;
+using Energinet.DataHub.EDI.OutgoingMessages.IntegrationTests.DocumentAsserters;
+using Energinet.DataHub.EDI.OutgoingMessages.Interfaces.Models.Peek;
+using Energinet.DataHub.EDI.OutgoingMessages.UnitTests.Domain.RSM012;
 using Energinet.DataHub.ProcessManager.Abstractions.Client;
+using Energinet.DataHub.ProcessManager.Orchestrations.Abstractions.Processes.BRS_024.V1.Model;
+using FluentAssertions;
+using FluentAssertions.Execution;
+using Microsoft.Extensions.Options;
 using NodaTime;
+using NodaTime.Text;
 using Xunit;
 using Xunit.Abstractions;
 using Period = NodaTime.Period;
@@ -35,6 +45,8 @@ public class GivenRequestMeasurementsTests(
         // DocumentFormat.Ebix
     ];
 
+    protected BundlingOptions BundlingOptions => GetService<IOptions<BundlingOptions>>().Value;
+
     [Theory]
     [MemberData(nameof(SupportedDocumentFormats))]
     public async Task
@@ -46,14 +58,18 @@ public class GivenRequestMeasurementsTests(
 
         // Arrange
         var senderSpy = CreateServiceBusSenderSpy(StartSenderClientNames.ProcessManagerStartSender);
+        var senderSpyNotify = CreateServiceBusSenderSpy(NotifySenderClientNames.ProcessManagerNotifySender);
+        const string notifyEventName = RequestYearlyMeasurementsNotifyEventV1.OrchestrationInstanceEventName;
         var senderActor = new Actor(ActorNumber.Create("5799999933318"), ActorRole.EnergySupplier);
+        var receiverActor = senderActor;
+        var orchestrationInstanceId = Guid.NewGuid();
 
-        var now = new LocalDate(2025, 5, 23);
-        var periodEnd = CreateDateInstant(now.Year, now.Month, now.Day);
-        var oneYearAgo = now.Minus(Period.FromYears(1));
+        var localNow = new LocalDate(2025, 5, 23);
+        var now = CreateDateInstant(localNow.Year, localNow.Month, localNow.Day);
+        var oneYearAgo = localNow.Minus(Period.FromYears(1));
         var periodStart = CreateDateInstant(oneYearAgo.Year, oneYearAgo.Month, oneYearAgo.Day);
 
-        GivenNowIs(periodEnd);
+        GivenNowIs(now);
         GivenAuthenticatedActorIs(senderActor.ActorNumber, senderActor.ActorRole);
 
         var transactionId = TransactionId.From("12356478912356478912356478912356478");
@@ -67,7 +83,7 @@ public class GivenRequestMeasurementsTests(
             [
                 (TransactionId: transactionId,
                     PeriodStart: periodStart,
-                    PeriodEnd: periodEnd,
+                    PeriodEnd: now,
                     MeteringPointId: meteringPointId),
             ]);
 
@@ -80,11 +96,101 @@ public class GivenRequestMeasurementsTests(
                 BusinessReason: BusinessReason.PeriodicMetering,
                 TransactionId: transactionId,
                 MeteringPointId: meteringPointId,
-                ReceivedAt: periodEnd));
+                ReceivedAt: now));
 
         /*
          *  --- PART 2: Receive data from Process Manager and create RSM document ---
          */
-        // TODO: Implement this part when the Process Manager is ready to send data to the EDI, issue #700
+        // Arrange
+        var startDate = Instant.FromUtc(2024, 11, 28, 13, 51);
+        var endDate = Instant.FromUtc(2024, 11, 29, 9, 15);
+
+        var expectedEnergyObservations = new List<(int Position, string QualityName, decimal Quantity)>
+        {
+            (1, "A04", 1), (2, "A04", 2), (3, "A04", 3), (4, "A04", 4), (5, "A04", 5), (6, "A04", 6), (7, "A04", 7),
+            (8, "A04", 8), (9, "A04", 9), (10, "A04", 10), (11, "A04", 11), (12, "A04", 12), (13, "A04", 13),
+            (14, "A04", 14), (15, "A04", 15), (16, "A04", 16), (17, "A04", 17), (18, "A04", 18), (19, "A04", 19),
+            (20, "A04", 20),
+        };
+
+        var requestYearlyMeasurementsInputV1 = message.ParseInput<RequestYearlyMeasurementsInputV1>();
+        var requestMeasurementsAcceptedServiceBusMessage = RequestMeasurementsResponseBuilder
+            .GenerateAcceptedFrom(
+                requestYearlyMeasurementsInputV1,
+                receiverActor,
+                startDate,
+                endDate,
+                orchestrationInstanceId,
+                expectedEnergyObservations);
+
+        await GivenRequestMeasurementsAcceptedIsReceived(requestMeasurementsAcceptedServiceBusMessage);
+
+        AssertCorrectProcessManagerNotification(
+            senderSpyNotify.LatestMessage!,
+            new NotifyOrchestrationInstanceEventV1AssertionInput(
+                orchestrationInstanceId,
+                notifyEventName));
+
+        // Act
+        var whenBundleShouldBeClosed = now.Plus(Duration.FromSeconds(BundlingOptions.BundleMessagesOlderThanSeconds));
+        GivenNowIs(whenBundleShouldBeClosed);
+        await GivenBundleMessagesHasBeenTriggered();
+
+        var peekResults = await WhenActorPeeksAllMessages(
+            receiverActor.ActorNumber,
+            receiverActor.ActorRole,
+            documentFormat);
+
+        // Assert
+        PeekResultDto peekResult;
+        using (new AssertionScope())
+        {
+            peekResult = peekResults
+                .Should()
+                .ContainSingle()
+                .Subject;
+        }
+
+        await ThenNotifyValidatedMeasureDataDocumentIsCorrect(
+                peekResultDocumentStream: peekResult.Bundle,
+                documentFormat: documentFormat,
+                assertionInput: new NotifyValidatedMeasureDataDocumentAssertionInput(
+                    RequiredHeaderDocumentFields: new RequiredHeaderDocumentFields(
+                        BusinessReasonCode: "E23",
+                        ReceiverId: receiverActor.ActorNumber.Value,
+                        ReceiverScheme: "A10",
+                        SenderId: "5790001330552",
+                        SenderScheme: "A10",
+                        SenderRole: "DGL",
+                        ReceiverRole: "DDQ",
+                        Timestamp: InstantPattern.General.Format(whenBundleShouldBeClosed)),
+                    OptionalHeaderDocumentFields: new OptionalHeaderDocumentFields(
+                        BusinessSectorType: "23",
+                        AssertSeriesDocumentFieldsInput: [
+                            new AssertSeriesDocumentFieldsInput(
+                                1,
+                                RequiredSeriesFields: new RequiredSeriesFields(
+                                    MeteringPointNumber: meteringPointId.Value,
+                                    MeteringPointScheme: "A10",
+                                    MeteringPointType: MeteringPointType.FromCode("E17"),
+                                    QuantityMeasureUnit: "KWH",
+                                    RequiredPeriodDocumentFields: new RequiredPeriodDocumentFields(
+                                        Resolution: Resolution.QuarterHourly.Code,
+                                        StartedDateTime: startDate.ToString("yyyy-MM-dd'T'HH:mm'Z'", CultureInfo.InvariantCulture),
+                                        EndedDateTime: endDate.ToString("yyyy-MM-dd'T'HH:mm'Z'", CultureInfo.InvariantCulture),
+                                        Points: expectedEnergyObservations
+                                            .Select(eo => new AssertPointDocumentFieldsInput(
+                                                new RequiredPointDocumentFields(eo.Position),
+                                                new OptionalPointDocumentFields(
+                                                    Quantity: eo.Quantity,
+                                                    Quality: Quality.FromCode(eo.QualityName!))))
+                                            .ToList())),
+                                OptionalSeriesFields: new OptionalSeriesFields(
+                                    OriginalTransactionIdReferenceId: transactionId.Value,
+                                    RegistrationDateTime: endDate.ToString(),
+                                    InDomain: null,
+                                    OutDomain: null,
+                                    Product: "8716867000030")),
+                        ])));
     }
 }
