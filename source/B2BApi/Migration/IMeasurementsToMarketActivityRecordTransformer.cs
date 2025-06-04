301f3543be73dbf47a6664e2cc048219867a2c54
Henrik Sommer <54191286+HenrikSommer@users.noreply.github.com>
2025-06-04 20:07:48 +0200

    feat: Convert metered data times series from DataHub 2 to ebiX (#1651)
    Co-authored-by: Lasse Ringgren Nielsen <sixxth@gmail.com>

---
+++ source/B2BApi/Migration/IMeasurementsToMarketActivityRecordTransformer.cs
@@ -0,0 +1,29 @@
+﻿// Copyright 2020 Energinet DataHub A/S
+//
+// Licensed under the Apache License, Version 2.0 (the "License2");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+using Energinet.DataHub.EDI.OutgoingMessages.Domain.DocumentWriters.RSM012;
+using NodaTime;
+
+namespace Energinet.DataHub.EDI.B2BApi.Migration;
+
+public interface IMeasurementsToMarketActivityRecordTransformer
+{
+    /// <summary>
+    /// Transform imported JSON TimeSeries message to a list of MeteredDataForMeteringPointMarketActivityRecord
+    /// </summary>
+    /// <param name="creationTime"></param>
+    /// <param name="timeSeries">List containing all time series in message.</param>
+    /// <returns>List MeteredDataForMeteringPointMarketActivityRecords containing all quantity observations.</returns>
+    List<MeteredDataForMeteringPointMarketActivityRecord> Transform(Instant creationTime, List<TimeSeries> timeSeries);
+}
